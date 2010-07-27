a402ac968cdca1c1e5c200310b5a281671ecd4b7
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-07-27 04:32:26 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-12/BioLinkUtilities/Extensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.ComponentModel;
+
+namespace BioLink.Client.Utilities {
+
+    public static class Extensions {
+
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action) {
+            foreach (T item in enumeration) {
+                action(item);
+            }
+        }
+
+        public static void InvokeIfRequired(this DispatcherObject control, Action action) {
+            if (control.Dispatcher.Thread != Thread.CurrentThread) {
+                control.Dispatcher.Invoke(action);
+            } else {
+                action();
+            }
+        }
+
+        public static bool IsDesignTime(this Control control) {
+            return DesignerProperties.GetIsInDesignMode(control);
+        }
+
+        public static string _R(this Control control, string messageKey, params object[] args) {
+            string message = null;
+            control.InvokeIfRequired(() => {
+
+                try {
+                    message = control.FindResource(messageKey) as string;
+                } catch (Exception) {
+                }
+
+                if (message == null) {
+                    Logger.Warn("Failed for find message for key '{0}' - using key instead", messageKey);
+                    message = messageKey;
+                }
+
+                if (args != null) {
+                    message = String.Format(message, args);
+                }
+            });
+
+            return message;
+        }
+
+
+    }
+
+}
