99d32a96d818d910479b98aa8a1fc05e3a786c46
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-08-03 06:54:31 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-24/BioLink.Client.Extensibility/BioLinkPluginBase.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Reflection;
+using System.Xml;
+using System.Xaml;
+using System.IO;
+using System.Windows.Resources;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+
+namespace BioLink.Client.Extensibility {
+
+    public abstract class BiolinkPluginBase : IBioLinkPlugin {
+
+        private ResourceDictionary _resourceDictionary;
+
+        public User User { get; set; }
+        public PluginManager PluginManager { get; set; }
+
+        public BiolinkPluginBase(User user, PluginManager pluginManager ) {
+
+            this.User = user;
+            this.PluginManager = pluginManager;
+
+            string assemblyName = this.GetType().Assembly.GetName().Name;
+            string packUri = String.Format("pack://application:,,,/{0};component/StringResources.xaml", assemblyName);
+            Logger.Debug("Attempting resource discovery for {0} ({1})", assemblyName, packUri);
+            try {
+                Uri uri = new Uri(packUri, UriKind.Absolute);
+                StreamResourceInfo info = Application.GetResourceStream(uri);
+                if (info != null) {
+                    _resourceDictionary = XamlServices.Load(info.Stream) as ResourceDictionary;
+                    Logger.Debug("{0} resource keys loaded (Ok).", _resourceDictionary.Count);
+                } else {
+                    Logger.Debug("No resource stream found for assembly {0} - message keys will be used instead", assemblyName);
+                    _resourceDictionary = new ResourceDictionary();
+                }
+            } catch (Exception ex) {
+                Logger.Debug("Failed to read resources for {0} : {1}", assemblyName, ex.ToString());
+                _resourceDictionary = new ResourceDictionary();
+            }
+        }
+
+        protected String _R(string key, params object[] args) {
+            Object res = _resourceDictionary[key];
+            if (res == null) {
+                return key;
+            } else {
+                if (args != null) {
+                    return String.Format(res.ToString(), args);
+                } else {
+                    return res.ToString();
+                }
+            }            
+        }
+
+        public string GetCaption(string key, params object[] args) {
+            return _R(key, args);
+        }
+
+        public ResourceDictionary ResourceDictionary {
+            get { return _resourceDictionary; }
+        }
+
+        public abstract string Name { get; }
+
+        public abstract List<IWorkspaceContribution> Contributions { get; }
+
+        public abstract bool RequestShutdown();
+
+        public virtual void Dispose() {            
+        }
+
+    }
+}
---
+++ tags/build-24/BioLinkDAL/Mapper.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using BioLink.Data.Model;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Reflection;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Data {
+
+    public abstract class MapperBase {
+
+        private static Regex PREFIX_REGEX = new Regex("^([a-z]+)[A-Za-z]+$");
+
+        private static string KNOWN_TYPE_PREFIXES = "chr,vchr,bit,int,txt,flt,tint,dt";
+
+        public static void ReflectMap(object dest, DbDataReader reader, params ConvertingMapper[] columnOverrides) {
+            PropertyInfo[] props = dest.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            Dictionary<string, PropertyInfo> propMap = new Dictionary<string, PropertyInfo>();
+            foreach (PropertyInfo propInfo in props) {
+                if (propInfo.CanWrite) {
+                    propMap.Add(propInfo.Name, propInfo);
+                }
+            }
+
+            var overrides = new Dictionary<string, ConvertingMapper>();
+            foreach (ConvertingMapper mapper in columnOverrides) {
+                overrides.Add(mapper.ColumnName, mapper);
+            }
+
+            for (int i = 0; i < reader.FieldCount; ++i) {
+                string name = reader.GetName(i);
+
+                PropertyInfo target = null;
+
+                if (propMap.ContainsKey(name)) {
+                    target = propMap[name];
+                } else {
+                    // It may be that the column names have been prefixed with their database type.
+                    // Look for common prefixes, and if so, strip the prefix off, and try that
+                    Match m = PREFIX_REGEX.Match(name);
+                    if (m.Success) {
+                        string prefix = m.Groups[1].Value;
+
+                        if (KNOWN_TYPE_PREFIXES.IndexOf(prefix) >= 0) {
+                            string shortened = name.Substring(prefix.Length);
+                            if (propMap.ContainsKey(shortened)) {
+                                target = propMap[shortened];
+                            }
+                        }
+                    }
+                }
+
+                if (target != null) {
+                    object val = reader[i];
+                    if (overrides.ContainsKey(name)) {
+                        val = overrides[name].Converter(val);
+                    } else {
+                        if (val is DBNull) {
+                            val = null;
+                        } else if (val is string) {
+                            val = (val as string).TrimEnd();
+                        }
+                    }
+
+                    target.SetValue(dest, val, null);
+                } else {
+                    Logger.Debug("Could not map field '{0}' to object of type {1}", name, dest.GetType().Name);
+                }
+            }
+        }
+
+        public static ConvertingMapper ToNull(string columnName) {
+            return new ConvertingMapper(columnName, (@in) => null);
+        }
+
+    }
+
+    public class TaxonMapper : MapperBase {
+
+        public static Taxon MapTaxon(SqlDataReader reader, params ConvertingMapper[] overrides) {
+            Taxon t = new Taxon();
+            ReflectMap(t, reader, overrides);
+            return t;
+        }
+
+        public static TaxonSearchResult MapTaxonSearchResult(SqlDataReader reader) {
+            TaxonSearchResult t = new TaxonSearchResult();
+            ReflectMap(t, reader);
+            return t;
+        }
+
+        public static TaxonRank MapTaxonRank(SqlDataReader reader) {
+            TaxonRank tr = new TaxonRank();
+            ReflectMap(tr, reader);
+            return tr;
+        }
+
+    }
+
+    public class ConvertingMapper {
+
+        public ConvertingMapper(string column, Converter<object, object> converter) {
+            this.ColumnName = column;
+            this.Converter = converter;
+        }
+
+        public string ColumnName { get; private set; }
+        public Converter<object, object> Converter { get; set; }
+    }
+
+}
