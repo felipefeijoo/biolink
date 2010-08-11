f73c7aa9fc074f1f634f71119b7dc64cdf473251
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-08-11 23:11:58 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-37/BioLink.Client.Extensibility/ITabularDataExporter.cs
@@ -0,0 +1,20 @@
+﻿using BioLink.Client.Utilities;
+using BioLink.Data;
+using System.Windows;
+
+namespace BioLink.Client.Extensibility {
+
+    public interface ITabularDataExporter<TOptionType> : IBioLinkExtension {
+
+        TOptionType GetOptions(Window parentWindow);
+
+        void Export(DataMatrix matrix, TOptionType options, IProgressObserver progress);
+
+        #region Properties
+
+        string Description { get; }
+
+        #endregion
+
+    }
+}
---
+++ tags/build-37/BioLink.Taxa/StatisticsReport.cs
@@ -0,0 +1,27 @@
+﻿using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Taxa {
+
+    public class TaxonStatisticsReport : TaxonReportBase {
+
+        public TaxonStatisticsReport(TaxaService service, TaxonViewModel taxon) : base(service, taxon) {
+            RegisterViewer(new TabularDataViewerSource());
+            DefineColumn("Category");
+            DefineColumn("Count");
+        }
+
+        public override DataMatrix ExtractReportData(IProgressObserver progress) {
+            DataMatrix table = Service.GetStatistics(Taxon.TaxaID.Value);
+            return table;
+        }
+
+        public override string Name {
+            get { return string.Format("Statistics for '{0}'", Taxon.DisplayLabel); }
+        }
+
+    }
+
+}
+
