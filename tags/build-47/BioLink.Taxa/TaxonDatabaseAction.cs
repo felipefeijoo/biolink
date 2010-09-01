acf080e47f196703c74a3797b43be2a20e42bba0
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-09-01 08:49:54 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-47/BioLink.Taxa/TaxonDatabaseAction.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Taxa {
+
+    public abstract class TaxonDatabaseAction : DatabaseAction<TaxaService> {
+    }
+
+    public class MoveTaxonDatabaseAction : TaxonDatabaseAction {
+
+        public MoveTaxonDatabaseAction(TaxonViewModel taxon, TaxonViewModel newParent) {
+            this.Taxon = taxon;
+            this.NewParent = newParent;
+        }
+
+        public TaxonViewModel Taxon { get; private set; }
+        public TaxonViewModel NewParent { get; private set; }
+
+        protected override void ProcessImpl(TaxaService service) {
+            service.MoveTaxon(Taxon.TaxaID.Value, NewParent.TaxaID.Value);
+        }
+
+        public override string ToString() {
+            return String.Format("Move: {0} to {1}", Taxon, NewParent);
+        }
+    }
+
+    public class UpdateTaxonDatabaseAction : TaxonDatabaseAction {
+
+        public UpdateTaxonDatabaseAction(Taxon taxon) {
+            this.Taxon = taxon;
+        }
+
+        public Taxon Taxon { get; private set; }
+
+        protected override void ProcessImpl(TaxaService service) {
+            service.UpdateTaxon(Taxon);
+        }
+
+        public override string ToString() {
+            return String.Format("Update: {0}", Taxon);
+        }
+
+
+    }
+
+    public class MergeTaxonDatabaseAction : TaxonDatabaseAction {
+
+        public MergeTaxonDatabaseAction(TaxonViewModel source, TaxonViewModel target, bool createNewIDRecord) {
+            this.Source = source;
+            this.Target = target;
+            this.CreateNewIDRecord = createNewIDRecord;
+        }
+
+        public TaxonViewModel Source { get; private set; }
+        public TaxonViewModel Target { get; private set; }
+        public bool CreateNewIDRecord { get; private set; }
+
+        protected override void ProcessImpl(TaxaService service) {
+            service.MergeTaxon(Source.TaxaID.Value, Target.TaxaID.Value, CreateNewIDRecord);
+            service.DeleteTaxon(Source.TaxaID.Value);
+        }
+
+        public override string ToString() {
+            return String.Format("Merging: {0} with {1}", Source, Target);
+        }
+
+
+    }
+
+    public class DeleteTaxonDatabaseAction : TaxonDatabaseAction {
+        
+        public DeleteTaxonDatabaseAction(TaxonViewModel taxon) {
+            this.Taxon = taxon;
+        }
+
+        public TaxonViewModel Taxon { get; private set; }
+
+        protected override void ProcessImpl(TaxaService service) {
+            service.DeleteTaxon(Taxon.TaxaID.Value);
+        }
+
+        public override string ToString() {
+            return String.Format("Deleting: {0} ", Taxon);
+        }
+
+    }
+
+    public class InsertTaxonDatabaseAction : TaxonDatabaseAction {
+
+        public InsertTaxonDatabaseAction(TaxonViewModel taxon) {
+            this.Taxon = taxon;
+        }
+
+        public TaxonViewModel Taxon { get; private set; }
+
+        protected override void ProcessImpl(TaxaService service) {                           
+            service.InsertTaxon(Taxon.Taxon);
+            // The service will have updated the new taxon with its database identity.
+            // If this taxon has any children we can update their identity too.
+            foreach (HierarchicalViewModelBase child in Taxon.Children) {
+                TaxonViewModel tvm = child as TaxonViewModel;
+                tvm.TaxaParentID = Taxon.Taxon.TaxaID;
+            }
+        }
+
+        public override string ToString() {
+            return String.Format("Inserting: {0}", Taxon);
+        }
+
+
+    }
+
+    
+}
