f7fddc1345c5cc0f49310cff5813817bd741c48a
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-09-03 08:29:33 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-52/BioLink.Taxa/TaxonMenuFactory.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using System.Windows.Input;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Taxa {
+
+    internal class TaxonMenuFactory {
+
+        private MenuItemBuilder _builder;
+
+        public TaxonMenuFactory(TaxonViewModel taxon, TaxonExplorer explorer, MessageFormatterFunc formatter) {
+            this.Taxon = taxon;
+            this.Explorer = explorer;
+            this.FormatterFunc = formatter;
+            _builder = new MenuItemBuilder(formatter);
+        }
+
+        public ContextMenu BuildExplorerMenu() {
+            ContextMenu menu = new ContextMenu();
+
+            if (Explorer.IsUnlocked) {
+                if (!Taxon.IsRootNode) {                    
+                    menu.Items.Add(_builder.New("TaxonExplorer.menu.Delete", Taxon.DisplayLabel).Handler(() => { Explorer.DeleteTaxon(Taxon); }).MenuItem);
+                    menu.Items.Add(_builder.New("TaxonExplorer.menu.Rename", Taxon.DisplayLabel).Handler(() => { Explorer.RenameTaxon(Taxon); }).MenuItem);
+                }
+
+                MenuItem addMenu = BuildAddMenuItems();
+                if (addMenu != null && addMenu.Items.Count > 0) {
+                    menu.Items.Add(new Separator());
+                    menu.Items.Add(addMenu);
+                }
+
+            } else {
+                menu.Items.Add(_builder.New("TaxonExplorer.menu.Unlock").Handler(() => { Explorer.btnLock.IsChecked = true; }).MenuItem);
+            }
+
+            if (menu.HasItems) {
+                menu.Items.Add(new Separator());
+            }
+
+            menu.Items.Add(_builder.New("TaxonExplorer.menu.ExpandAll").Handler(() => {
+                JobExecutor.QueueJob(() => {
+                    Explorer.tvwAllTaxa.InvokeIfRequired(() => {
+                        Explorer.tvwAllTaxa.Cursor = Cursors.Wait;
+                        Explorer.ExpandChildren(Taxon);
+                        Explorer.tvwAllTaxa.Cursor = Cursors.Arrow;
+                    });
+                });
+            }).MenuItem);
+            
+
+            MenuItem sortMenu = BuildSortMenuItems();
+            if (sortMenu != null && sortMenu.HasItems) {
+                menu.Items.Add(new Separator());
+                menu.Items.Add(sortMenu);
+            }
+
+            if (!Explorer.IsUnlocked) {
+                menu.Items.Add(new Separator());
+                menu.Items.Add(_builder.New("TaxonExplorer.menu.Refresh").Handler(() => Explorer.Refresh()).MenuItem);
+            }
+
+            if (!Taxon.IsRootNode) {
+                MenuItem reports = CreateReportMenuItems();
+                if (reports != null && reports.HasItems) {
+                    if (menu.HasItems) {
+                        menu.Items.Add(new Separator());
+                    }
+                    menu.Items.Add(reports);
+                }
+
+                if (menu.HasItems) {
+                    menu.Items.Add(new Separator());
+                }
+                menu.Items.Add(_builder.New("_Edit Name...").Handler(() => { Explorer.EditTaxonName(Taxon); }).MenuItem);
+                menu.Items.Add(_builder.New("_Edit Details...").Handler(() => { Explorer.ShowTaxonDetails(Taxon); }).MenuItem);
+            }
+
+            return menu;
+        
+        }
+
+        private MenuItem CreateReportMenuItems() {
+            MenuItem reports = _builder.New("Reports").MenuItem;
+
+            List<IBioLinkReport> list = new List<IBioLinkReport>();
+
+            list.Add(new TaxonStatisticsReport(Explorer.User, Taxon));
+            list.Add(new MaterialForTaxonReport(Explorer.User, Taxon));
+
+            foreach (IBioLinkReport report in list) {
+                IBioLinkReport reportToExecute = report;
+                reports.Items.Add(_builder.New(report.Name).Handler(() => { Explorer.RunReport(reportToExecute); }).MenuItem);
+            }
+
+            return reports;
+        }
+
+        private MenuItem BuildAddMenuItems() {
+
+            MenuItem addMenu = _builder.New("TaxonExplorer.menu.Add").MenuItem;
+
+            if (Taxon.AvailableName.GetValueOrDefault(false) || Taxon.LiteratureName.GetValueOrDefault(false)) {
+                return null;
+            }
+
+            if (Taxon.TaxaParentID == -1) {
+                TaxonRank rank = Explorer.Service.GetRankByOrder(1);
+                if (rank != null) {
+                    addMenu.Items.Add(_builder.New(rank.LongName).Handler(() => { Explorer.AddNewTaxon(Taxon, rank); }).MenuItem);
+                    addMenu.Items.Add(_builder.New("TaxonExplorer.menu.Add.AllRanks").Handler(() => { Explorer.AddNewTaxonAllRanks(Taxon); }).MenuItem);
+                }
+            } else {
+                switch (Taxon.ElemType) {
+                    case "":
+                        addMenu.Items.Add(_builder.New("Unranked Valid").Handler(() => { Explorer.AddUnrankedValid(Taxon); }).MenuItem);
+                        break;
+                    case TaxonRank.INCERTAE_SEDIS:
+                    case TaxonRank.SPECIES_INQUIRENDA:
+                        AddSpecialNameMenuItems(addMenu, true, false, false, false);
+                        break;
+                    default:
+
+                        TaxonRank rank = Explorer.Service.GetTaxonRank(Taxon.ElemType);
+                        if (rank != null) {
+                            List<TaxonRank> validChildRanks = Explorer.Service.GetChildRanks(rank);
+                            if (validChildRanks != null && validChildRanks.Count > 0) {
+                                foreach (TaxonRank childRank in validChildRanks) {
+                                    // The for loop variable is outside of the scope of the closure, so we need to create a local...
+                                    TaxonRank closureRank = Explorer.Service.GetTaxonRank(childRank.Code);
+                                    addMenu.Items.Add(_builder.New(childRank.LongName).Handler(() => {
+                                        Explorer.AddNewTaxon(Taxon, closureRank);
+                                    }).MenuItem);
+                                }
+                                addMenu.Items.Add(new Separator());
+                                addMenu.Items.Add(_builder.New("Unranked Valid").Handler(() => { Explorer.AddUnrankedValid(Taxon); }).MenuItem);
+                                addMenu.Items.Add(new Separator());
+                                AddSpecialNameMenuItems(addMenu, rank.AvailableNameAllowed, rank.LituratueNameAllowed, rank.AvailableNameAllowed, rank.AvailableNameAllowed);
+                                addMenu.Items.Add(new Separator());
+                                foreach (TaxonRank childRank in validChildRanks) {
+                                    // The for loop variable is outside of the scope of the closure, so we need to create a local...
+                                    TaxonRank closureRank = Explorer.Service.GetTaxonRank(childRank.Code);
+                                    if (childRank.UnplacedAllowed.ValueOrFalse()) {
+                                        addMenu.Items.Add(_builder.New("Unplaced " + childRank.LongName).Handler(() => { Explorer.AddNewTaxon(Taxon, closureRank, true); }).MenuItem);
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
+            return addMenu;
+        }
+
+        private void AddSpecialNameMenuItems(MenuItem parentMenu, bool? availEnabled = true, bool? litEnabled = true, bool? ISEnabled = true, bool? SIEnabled = true) {            
+            parentMenu.Items.Add(_builder.New("TaxonExplorer.menu.Add.AvailableName").Handler(() => { Explorer.AddAvailableName(Taxon); }).Enabled(availEnabled.ValueOrFalse()).MenuItem);
+            parentMenu.Items.Add(_builder.New("TaxonExplorer.menu.Add.LiteratureName").Handler(() => { Explorer.AddLiteratureName(Taxon); }).Enabled(litEnabled.ValueOrFalse()).MenuItem);
+            parentMenu.Items.Add(_builder.New("TaxonExplorer.menu.Add.IncertaeSedis").Handler(() => { Explorer.AddIncertaeSedis(Taxon); }).Enabled(ISEnabled.ValueOrFalse()).MenuItem);
+            parentMenu.Items.Add(_builder.New("TaxonExplorer.menu.Add.SpeciesInquirenda").Handler(() => { Explorer.AddSpeciesInquirenda(Taxon); }).Enabled(SIEnabled.ValueOrFalse()).MenuItem);
+        }
+
+
+        private MenuItem BuildSortMenuItems() {
+
+            MenuItem sort = _builder.New("Sort").MenuItem;
+            sort.Items.Add(_builder.New("By Name").Handler(() => { Explorer.SetManualSortMode(false); }).Checked(!TaxonExplorer.IsManualSort).MenuItem);
+            sort.Items.Add(_builder.New("Manual").Handler(() => { Explorer.SetManualSortMode(true); }).Checked(TaxonExplorer.IsManualSort).MenuItem);
+            sort.Items.Add(new Separator());
+            sort.Items.Add(_builder.New("Shift Up").Handler(() => { Explorer.ShiftTaxonUp(Taxon); }).Enabled(TaxonExplorer.IsManualSort && Explorer.IsUnlocked).MenuItem);
+            sort.Items.Add(_builder.New("Shift Down").Handler(() => { Explorer.ShiftTaxonDown(Taxon); }).Enabled(TaxonExplorer.IsManualSort && Explorer.IsUnlocked).MenuItem);
+
+            return sort;
+        }
+
+        internal ContextMenu BuildFindResultsMenu() {
+            ContextMenu menu = new ContextMenu();
+
+            menu.Items.Add(_builder.New("TaxonExplorer.menu.ShowInContents").Handler(() => { Explorer.ShowInExplorer(Taxon); }).MenuItem);            
+            MenuItem reports = CreateReportMenuItems();
+            if (reports != null && reports.HasItems) {
+                if (menu.HasItems) {
+                    menu.Items.Add(new Separator());
+                }
+                menu.Items.Add(reports);
+            }
+
+            menu.Items.Add(new Separator());
+            menu.Items.Add(_builder.New("_Edit Name...").Handler(() => { Explorer.EditTaxonName(Taxon); }).MenuItem);
+            menu.Items.Add(_builder.New("_Edit Details...").Handler(() => { Explorer.ShowTaxonDetails(Taxon); }).MenuItem);
+
+            return menu;
+        }
+
+        #region properties
+
+        protected TaxonViewModel Taxon { get; private set; }
+        protected TaxonExplorer Explorer { get; private set; }
+        protected MessageFormatterFunc FormatterFunc { get; private set; }
+
+        #endregion
+
+    }
+}
---
+++ tags/build-52/BioLinkDAL/SupportService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data.Model;
+using System.Data.SqlClient;
+
+namespace BioLink.Data {
+
+    public class SupportService : BioLinkService {
+
+        public SupportService(User user)
+            : base(user) {
+        }
+
+        public List<PhraseCategory> GetPhraseCategories() {
+            var mapper = new GenericMapperBuilder<PhraseCategory>().build();
+            return StoredProcToList<PhraseCategory>("spPhraseCategoryList", mapper);
+        }
+
+        public int GetPhraseCategoryId(string categoryName, bool @fixed) {
+            return StoredProcReturnVal<int>("spPhraseCategoryGetID", new SqlParameter("vchrCategory", categoryName), new SqlParameter("bitFixed", @fixed));
+        }
+
+        public List<Phrase> GetPhrases(int categoryId) {
+            var mapper = new GenericMapperBuilder<Phrase>().Map("Phrase", "PhraseText").build();
+            return StoredProcToList<Phrase>("spPhraseList", mapper, new SqlParameter("intCategory", categoryId));
+        }
+
+        public void AddPhrase(Phrase phrase) {
+            // Obviously a copy-pasta error in the Stored Proc, as the return value is called NewRegionID...oh well...
+            SqlParameter retval = ReturnParam("NewRegionID", System.Data.SqlDbType.Int);
+            StoredProcUpdate("spPhraseInsert", _P("intPhraseCatID", phrase.PhraseCatID), _P("vchrPhrase", phrase.PhraseText), retval);
+            if (retval != null && retval.Value != null) {
+                phrase.PhraseID = (Int32)retval.Value;
+            }
+        }
+
+        public void RenamePhrase(int phraseId, string phrase) {
+            StoredProcUpdate("spPhraseRename", _P("intPhraseID", phraseId), _P("vchrPhrase", phrase)); 
+        }
+
+        public void DeletePhrase(int phraseId) {
+            StoredProcUpdate("spPhraseDelete", _P("intPhraseID", phraseId));
+        }
+
+        public void DeletePhraseCategory(int categoryId) {
+            StoredProcUpdate("spPhraseCategoryDelete", _P("intCatID", categoryId));
+        }
+    }
+
+}
