e767ab4c3756a1844ce76da581b72930f1eaf8f9
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-09-03 12:49:43 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-53/BioLink.Client.Extensibility/ControlHostWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for ControlHostWindow.xaml
+    /// </summary>
+    public partial class ControlHostWindow : Window {
+
+        #region DesignerConstructor
+        public ControlHostWindow() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public ControlHostWindow(UIElement element) {
+            InitializeComponent();
+            this.Control = element;
+            grid.Children.Add(element);
+        }
+
+        public UIElement Control { get; private set; }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            if (Control != null && Control is IClosable) {
+                var closable = Control as IClosable;
+                if (!closable.RequestClose()) {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+
+}
