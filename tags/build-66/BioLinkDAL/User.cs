36a2296adfc79fb10e59f5421066f0c2f25e154b
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-09-13 08:07:53 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-66/BioLinkDAL/User.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using BioLink.Client.Utilities;
+using BioLink.Data.Model;
+
+namespace BioLink.Data {
+
+    public class User {
+
+        private string _username;
+
+        public String Username {
+            get {
+                if (ConnectionProfile.IntegratedSecurity || String.IsNullOrEmpty(_username)) {
+                    return Environment.UserName;
+                } else {
+                    return _username;
+                }
+            }
+            private set {
+                _username = value;
+            }
+        }
+
+        private String Password;
+        public ConnectionProfile ConnectionProfile { get; private set; }
+
+        public User(string username, string password, ConnectionProfile profile) {
+            this.Username = username;
+            this.Password = password;
+            this.ConnectionProfile = profile;
+        }
+
+        public bool Authenticate(out String message) {
+
+                SqlConnection connection = null;
+                try {
+                    Logger.Debug("Attemping to connect to {0} (Database {1}) with username '{2}'...", ConnectionProfile.Server, ConnectionProfile.Database, Username);
+                    using (GetConnection()) {
+                        message = "Ok";
+                        return true;
+                    }                    
+                } catch (SqlException sqlex) {
+                    message = sqlex.Message;
+                    Logger.Warn("SQL Exception: {0}", sqlex.Message);                    
+                } catch (Exception ex) {
+                    message = ex.Message;
+                    Logger.Error("Unexpected error connecting to database!", ex);                                        
+                } finally {
+                    if (connection != null) {
+                        connection.Close();
+                        connection.Dispose();
+                    }
+                }
+
+            return false;
+        }
+
+        private string ConnectionString {
+            get {
+                StringBuilder s = new StringBuilder();
+                if (ConnectionProfile.IntegratedSecurity) {
+                    s.Append(String.Format("Data Source={0};Initial Catalog={1};Integrated Security=SSPI;", ConnectionProfile.Server, ConnectionProfile.Database));
+                } else {
+                    s.Append(String.Format("Data Source={0};Initial Catalog={1};User Id={2};Password={3}", ConnectionProfile.Server, ConnectionProfile.Database, Username, Password));
+                }
+
+                return s.ToString();
+
+            }
+
+        }
+
+        public SqlConnection GetConnection() {
+            Logger.Debug("Opening connection to {0} (Database {1}) with username '{2}'...", ConnectionProfile.Server, ConnectionProfile.Database, Username);
+            SqlConnection connection = new SqlConnection(this.ConnectionString);
+            connection.Open();            
+            return connection;
+        }
+
+    }
+
+}
