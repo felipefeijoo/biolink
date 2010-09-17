a675e821aac05051e061205f3f9e67440f4dded3
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-09-17 08:45:28 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-71/BioLink.Client.Extensibility/rtf/BindableRichTextBox.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using BioLink.Client.Utilities;
+using System.Text;
+using System.IO;
+
+namespace BioLink.Client.Extensibility {
+
+    public class BindableRichTextBox : RichTextBox {
+
+        private bool _selfSetting;
+
+        public BindableRichTextBox() {
+            this.TextChanged += new TextChangedEventHandler(txt_TextChanged);
+        }
+
+        void txt_TextChanged(object sender, TextChangedEventArgs e) {
+
+            var range = new TextRange(Document.ContentStart, Document.ContentEnd);
+            string rtf;
+
+            using (var stream = new MemoryStream()) {
+                range.Save(stream, DataFormats.Rtf);
+                stream.Seek(0, SeekOrigin.Begin);
+                using (var reader = new StreamReader(stream)) {
+                    rtf = reader.ReadToEnd();
+                }
+            }
+            _selfSetting = true;
+            RTF = rtf;
+            _selfSetting = false;
+        }
+
+        public static readonly DependencyProperty RTFProperty = DependencyProperty.Register("RTF", typeof(string), typeof(BindableRichTextBox), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnRTFChanged)));
+
+        private static void OnRTFChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args) {
+            var control = (BindableRichTextBox)obj;
+            if (!control._selfSetting) {
+                var rtf = args.NewValue as string;
+                var doc = control.Document;
+                if (string.IsNullOrEmpty(rtf)) {
+                    doc.Blocks.Clear();
+                } else {                    
+                    using (var stream = new MemoryStream((new ASCIIEncoding()).GetBytes(rtf))) {
+                        var text = new TextRange(doc.ContentStart, doc.ContentEnd);
+                        text.Load(stream, DataFormats.Rtf);
+                    }
+                }
+            }
+        }
+
+        public String RTF {
+            get { return (string)GetValue(RTFProperty); }
+            set { SetValue(RTFProperty, value); }
+        }
+
+    }
+}
---
+++ tags/build-71/BioLinkDAL/Model/BiolinkDataObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public abstract class GUIDObject {
+        public Nullable<Guid> GUID { get; set; }
+    }
+
+    public abstract class BiolinkDataObject : GUIDObject {
+
+        public DateTime DateCreated { get; set; }
+        public string WhoCreated { get; set; }
+        public DateTime DateLastUpdated { get; set; }
+        public string WhoLastUpdated { get; set; }
+
+    }
+
+}
