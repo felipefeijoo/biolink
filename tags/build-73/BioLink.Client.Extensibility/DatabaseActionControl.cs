e182bc20524a3e234cdd614020de4e85a7f640e6
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-09-21 02:11:51 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-73/BioLink.Client.Extensibility/DatabaseActionControl.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using BioLink.Data;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Extensibility {
+
+    public class DatabaseActionControl : UserControl, IIdentifiableContent, IChangeContainerObserver {
+
+        #region Designer Constructor
+        public DatabaseActionControl() : base() {
+        }
+        #endregion
+
+        public DatabaseActionControl(User user, string contentId)
+            : base() {
+
+            this.User = user;
+            this.ContentIdentifier = contentId;
+        }
+
+        public string ContentIdentifier { get; private set; }
+
+        public bool CompareContentHash(string other) {
+            return ContentIdentifier == other;
+        }
+
+        public void RegisterPendingChange(DatabaseAction action) {
+            WithChangeContainer(window => {
+                window.RegisterPendingChange(action, this);
+            });
+        }
+
+        public bool RegisterUniquePendingChange(DatabaseAction action) {
+            bool ret = false;
+            WithChangeContainer(window => { ret = window.RegisterUniquePendingChange(action, this); });
+            return ret;
+        }
+
+        public void RegisterPendingChanges(List<DatabaseAction> actions) {
+            WithChangeContainer(window =>  window.RegisterPendingChanges(actions, this));
+        }
+
+        protected void CommitPendingChanges(Action successAction = null) {
+            WithChangeContainer(window => window.CommitPendingChanges(successAction));            
+        }
+
+        public void ClearPendingChanges() {
+            WithChangeContainer(window => window.ClearPendingChanges());
+        }
+
+        private void WithChangeContainer(Action<IChangeContainer> action) {
+            var window = this.FindParentWindow() as IChangeContainer;
+            if (window != null) {
+                action(window);
+            } else {
+                throw new Exception("Parent window could not be found, or it is not an IChangeContainer");
+            }
+
+        }
+
+        public void OnChangesCommitted() {
+            if (ChangesCommitted != null) {
+                ChangesCommitted(this);
+            }
+        }
+
+
+        public virtual void Dispose() {        
+        }
+
+        public User User { get; private set; }
+
+        public event PendingChangesCommittedHandler ChangesCommitted;
+
+    }
+
+    public interface IIdentifiableContent {
+        string ContentIdentifier { get; }        
+    }
+    
+}
---
+++ tags/build-73/BioLink.Taxa/TaxonDetails.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Data.Model;
+using System.Reflection;
+using BioLink.Data;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Taxa {
+    /// <summary>
+    /// Interaction logic for TaxonDetails.xaml
+    /// </summary>
+    public partial class TaxonDetails : DatabaseActionControl {
+
+        #region designer constructor
+        public TaxonDetails() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public TaxonDetails(TaxonViewModel taxon, User user) : base(user, "TaxonDetails::" + taxon.TaxaID.Value) {
+            InitializeComponent();           
+
+            AddTabItem("General", new TaxonNameDetails(taxon.TaxaID, User));
+
+            if (taxon.IsAvailableOrLiteratureName) {
+                TaxonRank rank = Service.GetTaxonRank(taxon.ElemType);
+                switch (rank.Category.ToLower()) {
+                    case "g":
+                        AddTabItem("Available Name", new GenusAvailableNameControl(taxon, user));
+                        break;
+                    case "s":
+                        AddTabItem("Available Name", new SpeciesAvailableNameControl(taxon, user));
+                        break;
+                    default:
+                        AddTabItem("Available Name", new AvailableNameControl(taxon, user));
+                        break;
+                }
+            } else {
+                AddTabItem("Common Names", new CommonNamesControl(taxon, user));
+            }
+
+            AddTabItem("References", new ReferencesControl(user, TraitCategoryType.Taxon, taxon.TaxaID));
+
+            AddTabItem("Traits", new TraitControl(User, TraitCategoryType.Taxon, taxon.TaxaID));
+            var mmc = new MultimediaControl(User, TraitCategoryType.Taxon, taxon.TaxaID);
+
+            AddTabItem("Multimedia", mmc, () => {
+                if (!mmc.IsPopulated) {
+                    mmc.PopulateControl();
+                }
+            });
+            AddTabItem("Ownership", new OwnershipDetails(taxon.Taxon));
+
+            this.Taxon = taxon;
+
+        }
+
+        private TabItem AddTabItem(string title, UIElement content, Action bringIntoViewAction = null) {            
+            TabItem tabItem = new TabItem();
+            tabItem.Header = title;
+            tabItem.Content = content;            
+            tabControl.Items.Add(tabItem);
+            if (bringIntoViewAction != null) {
+                tabItem.RequestBringIntoView += new RequestBringIntoViewEventHandler((s, e) => {
+                    bringIntoViewAction();
+                });
+            }
+                 
+            return tabItem;
+        }
+
+        public override void Dispose() {
+            foreach (TabItem item in tabControl.Items) {
+                if (item.Content is IDisposable) {
+                    (item.Content as IDisposable).Dispose();
+                }
+            }
+
+            base.Dispose();
+        }
+
+        #region properties
+
+        public TaxonViewModel Taxon { get; private set; }
+
+        public TaxaService Service { get { return new TaxaService(User); } }
+
+        #endregion
+
+    }
+
+
+
+}
