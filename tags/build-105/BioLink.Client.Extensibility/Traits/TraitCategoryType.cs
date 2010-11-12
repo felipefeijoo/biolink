ca23602fb5052a24c7a0e7bd274c1ca3c37f729b
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-11-12 07:04:35 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-105/BioLink.Client.Extensibility/Traits/TraitCategoryType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Client.Extensibility {
+
+    public enum TraitCategoryType {
+        Material,
+        Taxon,
+        Site,
+        Trap,
+        SiteVisit,
+        Multimedia
+    }
+
+}
