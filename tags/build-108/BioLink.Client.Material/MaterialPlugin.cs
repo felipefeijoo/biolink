a90166a981b3791c4f97cab11e4aaa33e92affb3
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-11-18 06:11:01 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-108/BioLink.Client.Material/MaterialPlugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+
+namespace BioLink.Client.Material {
+
+    public class MaterialPlugin : BiolinkPluginBase {
+
+        private MaterialExplorer _explorer;
+
+        public MaterialPlugin() {            
+        }
+
+        public override string Name {
+            get { return "Material"; }
+        }
+
+        public override List<IWorkspaceContribution> GetContributions() {
+            
+            List<IWorkspaceContribution> contrib = new List<IWorkspaceContribution>();
+
+            contrib.Add(new MenuWorkspaceContribution(this, "ShowExplorer", (obj, e) => { PluginManager.EnsureVisible(this, "MaterialExplorer"); },
+                String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("Material.Menu.View")),
+                String.Format("{{'Name':'ShowMaterialExplorer', 'Header':'{0}'}}", _R("Material.Menu.ShowExplorer"))
+            ));
+
+            _explorer = new MaterialExplorer(this);
+            contrib.Add(new ExplorerWorkspaceContribution<MaterialExplorer>(this, "MaterialExplorer", _explorer, _R("MaterialExplorer.Title"), (explorer) => {
+                explorer.InitializeMaterialExplorer();
+                // initializer
+            }));
+
+            return contrib;
+        }
+
+        public override bool RequestShutdown() {
+            return true;
+        }
+
+
+        public override List<Command> GetCommandsForObject(ViewModelBase obj) {
+            return null;
+        }
+    }
+}
