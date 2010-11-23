7b17bd7ad2c0629059f8645dacfd7c6701e713d0
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-11-23 07:24:47 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-111/BioLink.Client.Extensibility/Traits/TraitCategoryType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Client.Extensibility {
+
+    public enum TraitCategoryType {
+        Taxon,
+        Region,
+        Site,        
+        SiteVisit,
+        Trap,
+        Material,                
+        Multimedia
+    }
+
+}
---
+++ tags/build-111/BioLinkDAL/MaterialService.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data.Model;
+
+namespace BioLink.Data {
+
+    public class MaterialService : BioLinkService {
+
+        public MaterialService(User user)
+            : base(user) {
+        }
+
+        #region Site Explorer
+
+        public List<SiteExplorerNode> GetTopLevelExplorerItems() {
+            return GetExplorerElementsForParent(0, SiteExplorerNodeType.Region);
+        }
+
+        public List<SiteExplorerNode> GetExplorerElementsForParent(int parentID, SiteExplorerNodeType parentElemType) {
+            return GetExplorerElementsForParent(parentID, parentElemType.ToString());
+        }
+
+        public List<SiteExplorerNode> GetExplorerElementsForParent(int parentID, string parentElemType) {
+            var mapper = new GenericMapperBuilder<SiteExplorerNode>().build();
+            return StoredProcToList<SiteExplorerNode>("spSiteExplorerList", 
+                mapper, 
+                _P("intParentID", parentID), 
+                _P("vchrParentType", parentElemType)
+            );
+        }
+
+        #endregion
+
+        #region Region
+
+        public Region GetRegion(int regionID) {
+            var mapper = new GenericMapperBuilder<Region>().build();
+            Region result = null;
+            StoredProcReaderFirst("spRegionGet", (reader) => {
+                result = mapper.Map(reader);
+            }, _P("intRegionID", regionID));
+            return result;
+        }
+
+        public void UpdateRegion(int regionID, string name, string rank) {
+            StoredProcUpdate("spRegionUpdate",
+                _P("intRegionID", regionID),
+                _P("vchrName", name),
+                _P("vchrRank", rank)
+            );
+        }
+
+        public void DeleteRegion(int regionID) {
+            StoredProcUpdate("spRegionDelete", _P("intRegionID", regionID));
+        }
+
+        public void RenameRegion(int regionID, string name) {
+            StoredProcUpdate("spRegionRename",
+                _P("intRegionID", regionID),
+                _P("vchrName", name)
+            );
+        }
+
+        public int InsertRegion(string name, int parentID) {
+            var retval = ReturnParam("NewRegionID", System.Data.SqlDbType.Int);
+            StoredProcUpdate("spRegionInsert",
+                _P("vchrName", name),
+                _P("intParentID", parentID),
+                retval
+            );
+            return (int) retval.Value;
+        }
+
+        #endregion
+
+        #region Site Group
+
+        public void RenameSiteGroup(int siteGroupID, string name) {
+            StoredProcUpdate("spSiteGroupRename", _P("intSiteGroupID",siteGroupID), _P("vchrSiteGroupName", name));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">The name of the new Site Group</param>
+        /// <param name="parentType">The "type" of the parent: 1 = Region, 2 = Site Group</param>
+        /// <param name="parentID">The parent ID</param>
+        /// <param name="regionID">The region id underwhich this group is placed. Note that even if this is a child of another site group it still needs a region id</param>
+        /// <returns></returns>
+        public int InsertSiteGroup(string name, int parentType, int parentID, int regionID) {
+            var retval = ReturnParam("NewRegionID");
+
+            StoredProcUpdate("spSiteGroupInsert",
+                _P("vchrName", name),
+                _P("sintParentType", parentType),
+                _P("intParentID", parentID),
+                _P("intPoliticalRegionID", regionID),
+                retval
+            );
+
+            return (int)retval.Value;
+        }
+
+        public void DeleteSiteGroup(int siteGroupID) {
+            StoredProcUpdate("spSiteGroupDelete", _P("intSiteGroupID", siteGroupID));
+        }
+
+        public void MergeSiteGroups(int sourceID, int targetID) {
+            StoredProcUpdate("spSiteGroupMerge",
+                _P("intOldSiteGroupID", sourceID),
+                _P("intNewSiteGroupID", targetID)
+            );
+        }
+
+        public void MoveSiteGroup(int siteGroupID, int newParentType, int newParentID, int newRegionID) {
+            StoredProcUpdate("spSiteGroupMove",
+                _P("intSiteGroupID", siteGroupID),
+                _P("sintParentType", newParentType),
+                _P("intParentID", newParentID),
+                _P("intPoliticalRegionID", newRegionID)
+            );
+        }
+
+        public List<int> GetSiteGroupSiteIDList(int siteGroupID) {            
+            var results = new List<int>();
+            StoredProcReaderForEach("spSiteGroupGetSiteIDList",
+                (reader) => { results.Add(reader.GetInt32(0)); },
+                _P("intSiteGroupID", siteGroupID)
+            );
+
+            return results;
+        }
+
+        #endregion
+
+        #region Site
+
+        public Site GetSite(int siteID) {
+            Site result = null;
+            var mapper = new GenericMapperBuilder<Site>().build();
+            StoredProcReaderFirst("spSiteGet", (reader) => {
+                result = mapper.Map(reader);
+            }, _P("intSiteID", siteID));
+            return result;
+        }
+
+        public void RenameSite(int siteID, string name) {
+            StoredProcUpdate("spSiteUpdateName",
+                _P("intSiteID", siteID),
+                _P("vchrSiteName", name)
+            );
+        }
+
+        #endregion
+
+        #region Site Visit
+
+        public void RenameSiteVisit(int siteVisitID, string name) {
+            StoredProcUpdate("spSiteVisitUpdateName", _P("intSiteVisitID", siteVisitID), _P("vchrName", name));
+        }
+
+        #endregion
+
+        #region Trap
+
+        public void RenameTrap(int trapID, string name) {
+            StoredProcUpdate("spTrapUpdateName", _P("intTrapID", trapID), _P("vchrTrapName", name));
+        }
+
+        #endregion
+
+        #region Material
+
+        public void RenameMaterial(int materialID, string name) {
+            StoredProcUpdate("spMaterialUpdateName", _P("intMaterialID", materialID), _P("vchrName", name));
+        }
+
+        #endregion
+    }
+}
