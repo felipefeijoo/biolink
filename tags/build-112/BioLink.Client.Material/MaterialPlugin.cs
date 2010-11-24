4f35ef6cefa6f5da0fea7cb6a514e64ff766cfbc
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-11-24 06:28:33 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-112/BioLink.Client.Material/MaterialPlugin.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+
+    public class MaterialPlugin : BiolinkPluginBase {
+
+        private MaterialExplorer _explorer;
+
+        public MaterialPlugin() {            
+        }
+
+        public override string Name {
+            get { return "MATERIAL"; }
+        }
+
+        public override List<IWorkspaceContribution> GetContributions() {
+            
+            List<IWorkspaceContribution> contrib = new List<IWorkspaceContribution>();
+
+            contrib.Add(new MenuWorkspaceContribution(this, "ShowExplorer", (obj, e) => { PluginManager.EnsureVisible(this, "MaterialExplorer"); },
+                String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("MATERIAL.Menu.View")),
+                String.Format("{{'Name':'ShowMaterialExplorer', 'Header':'{0}'}}", _R("MATERIAL.Menu.ShowExplorer"))
+            ));
+
+            _explorer = new MaterialExplorer(this);
+            contrib.Add(new ExplorerWorkspaceContribution<MaterialExplorer>(this, "MaterialExplorer", _explorer, _R("MaterialExplorer.Title"), (explorer) => {
+                explorer.InitializeMaterialExplorer();
+                // initializer
+            }));
+
+            return contrib;
+        }
+
+        public override bool RequestShutdown() {
+            return true;
+        }
+
+        public override void Dispose() {
+            base.Dispose();
+            if (_explorer != null && _explorer.Content != null) {
+
+                if (Config.GetGlobal<bool>("Material.RememberExpandedNodes", true)) {
+                    List<string> expandedElements = _explorer.GetExpandedParentages(_explorer.RegionsModel);
+                    if (expandedElements != null) {
+                        Config.SetProfile(User, "Material.Explorer.ExpandedNodes.Region", expandedElements);
+                    }
+                }
+
+            }
+        }
+
+        public override bool CanSelect(Type t) {
+            return typeof(Region).IsAssignableFrom(t);
+        }
+
+        public override void Select(Type t, Action<SelectionResult> success) {
+            if (typeof(Region).IsAssignableFrom(t)) {
+                _explorer.BringIntoView();                
+                _explorer.BindSelectCallback(success);
+            } else {
+                throw new Exception("Unhandled Selection Type: " + t.Name);
+            }
+
+        }
+
+        public override List<Command> GetCommandsForObject(ViewModelBase obj) {
+            return null;
+        }
+    }
+}
---
+++ tags/build-112/BioLink.Client.Material/SiteDetails.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for SiteDetails.xaml
+    /// </summary>
+    public partial class SiteDetails : DatabaseActionControl {
+
+        private SiteViewModel _viewModel;
+
+        #region Designer Constructor
+        public SiteDetails() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public SiteDetails(User user, int siteID) : base(user, "Site:" + siteID) {
+            InitializeComponent();
+            this.SiteID = siteID;
+
+            var service = new MaterialService(user);
+            var model = service.GetSite(siteID);
+            _viewModel = new SiteViewModel(model);
+            this.DataContext = _viewModel;
+
+            tabSite.AddTabItem("Traits", new TraitControl(user, TraitCategoryType.Site, siteID));
+            tabSite.AddTabItem("Notes", new NotesControl(user, TraitCategoryType.Site, siteID));
+            tabSite.AddTabItem("Multimedia", new MultimediaControl(user, TraitCategoryType.Site, siteID));
+            tabSite.AddTabItem("Ownership", new OwnershipDetails(_viewModel.Model));
+
+            _viewModel.DataChanged += new DataChangedHandler(_viewModel_DataChanged);
+
+            txtPoliticalRegion.BindUser(user, LookupType.Region);
+
+            UpdateMiniMap(model.PosY1, model.PosX1);
+        }
+
+        private void UpdateMiniMap(double latitude, double longitude) {
+
+            double meridian = imgMap.Width / 2.0;
+            double equator = imgMap.Height / 2.0;
+            double x = meridian + ((longitude / 180) * meridian);
+            double y = equator - ((latitude / 90) * equator);
+
+            string assemblyName = this.GetType().Assembly.GetName().Name;
+            var img = ImageCache.GetPackedImage(@"images\World.png", assemblyName);
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int) img.Width, (int) img.Height, 96, 96, PixelFormats.Pbgra32);
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext dc = drawingVisual.RenderOpen();
+
+            dc.DrawImage(img, new Rect(0, 0, (int)imgMap.Width, (int) imgMap.Height));
+            var brush = new SolidColorBrush(Colors.Red);
+            var pen = new Pen(brush, 1);
+
+            dc.DrawEllipse(brush, pen, new Point(x, y), 2, 2);
+
+            dc.Close();
+            bmp.Render(drawingVisual);
+
+            imgMap.Source = bmp;
+        }
+
+        void _viewModel_DataChanged(ChangeableModelBase viewmodel) {
+            RegisterUniquePendingChange(new UpdateSiteAction(_viewModel));
+        }
+
+        #region Properties
+
+        public int SiteID { get; private set; }
+
+        #endregion
+    
+    }
+}
