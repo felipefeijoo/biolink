0a96c4a8658b1228dc18548ba4d7d18eb9c80d5f
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-11-30 04:34:00 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-115/BioLink.Client.Material/SiteDetails.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for SiteDetails.xaml
+    /// </summary>
+    public partial class SiteDetails : DatabaseActionControl {
+
+        private SiteViewModel _viewModel;
+
+        #region Designer Constructor
+        public SiteDetails() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public SiteDetails(User user, int siteID) : base(user, "Site:" + siteID) {
+            InitializeComponent();
+            this.SiteID = siteID;
+
+            // Radio button checked event handlers
+            optNearestPlace.Checked += new RoutedEventHandler((s, e) => {
+                txtLocality.IsEnabled = false;
+                txtDirectionFrom.IsEnabled = true;
+                txtDistanceFrom.IsEnabled = true;
+                txtFrom.IsEnabled = true;
+            });
+
+            optLocality.Checked += new RoutedEventHandler((s, e) => {
+                txtLocality.IsEnabled = true;
+                txtDirectionFrom.IsEnabled = false;
+                txtDistanceFrom.IsEnabled = false;
+                txtFrom.IsEnabled = false;
+            });
+
+            var service = new MaterialService(user);
+            var model = service.GetSite(siteID);
+            _viewModel = new SiteViewModel(model);
+            this.DataContext = _viewModel;
+
+            tabSite.AddTabItem("Traits", new TraitControl(user, TraitCategoryType.Site, siteID));
+            tabSite.AddTabItem("Notes", new NotesControl(user, TraitCategoryType.Site, siteID));
+            tabSite.AddTabItem("Multimedia", new MultimediaControl(user, TraitCategoryType.Site, siteID));
+            tabSite.AddTabItem("Ownership", new OwnershipDetails(_viewModel.Model));
+
+            txtPosSource.BindUser(User, PickListType.Phrase, "Source", TraitCategoryType.Site);
+            txtPosWho.BindUser(User, "tblSite", "vchrPosWho");
+            txtPosOriginal.BindUser(User, PickListType.Phrase, "OriginalDetermination", TraitCategoryType.Site);
+
+            txtElevUnits.BindUser(User, PickListType.Phrase, "Units", TraitCategoryType.Site);
+            txtElevSource.BindUser(User, PickListType.Phrase, "Source", TraitCategoryType.Site);
+
+            txtGeoEra.BindUser(User, PickListType.Phrase, "Geological Era", TraitCategoryType.Site);
+            txtGeoPlate.BindUser(User, PickListType.Phrase, "Geological Plate", TraitCategoryType.Site);
+            txtGeoStage.BindUser(User, PickListType.Phrase, "Geological State", TraitCategoryType.Site);
+
+            txtGeoFormation.BindUser(User, PickListType.Phrase, "Geological Formation", TraitCategoryType.Site);
+            txtGeoMember.BindUser(User, PickListType.Phrase, "Geological Member", TraitCategoryType.Site);
+            txtGeoBed.BindUser(User, PickListType.Phrase, "Geological Bed", TraitCategoryType.Site);
+
+            this.ctlX1.CoordinateValueChanged += new CoordinateValueChangedHandler((s, v) => { UpdateMiniMap(ctlY1.Value, ctlX1.Value); });
+            this.ctlY1.CoordinateValueChanged += new CoordinateValueChangedHandler((s, v) => { UpdateMiniMap(ctlY1.Value, ctlX1.Value); });
+
+
+            _viewModel.DataChanged += new DataChangedHandler(_viewModel_DataChanged);
+
+            txtPoliticalRegion.BindUser(user, LookupType.Region);
+
+            UpdateMiniMap(model.PosY1, model.PosX1);
+        }
+
+        private void UpdateMiniMap(double latitude, double longitude) {
+            UpdateMiniMap(latitude, longitude, imgMap);
+            UpdateMiniMap(latitude, longitude, imgMiniMap2);
+        }
+
+        private void UpdateMiniMap(double latitude, double longitude, Image imgControl) {
+
+            if (imgControl.Width == 0 || imgControl.Height == 0) {
+                return;
+            }
+
+            double meridian = imgControl.Width / 2.0;
+            double equator = imgControl.Height / 2.0;
+            double x = meridian + ((longitude / 180) * meridian);
+            double y = equator - ((latitude / 90) * equator);
+
+            string assemblyName = this.GetType().Assembly.GetName().Name;
+            var img = ImageCache.GetPackedImage(@"images\World.png", assemblyName);
+
+            RenderTargetBitmap bmp = new RenderTargetBitmap((int) img.Width, (int) img.Height, 96, 96, PixelFormats.Pbgra32);
+            DrawingVisual drawingVisual = new DrawingVisual();
+            DrawingContext dc = drawingVisual.RenderOpen();
+
+            dc.DrawImage(img, new Rect(0, 0, (int)imgControl.Width, (int)imgControl.Height));
+            var brush = new SolidColorBrush(Colors.Red);
+            var pen = new Pen(brush, 1);
+
+            dc.DrawEllipse(brush, pen, new Point(x, y), 2, 2);
+
+            dc.Close();
+            bmp.Render(drawingVisual);
+
+            imgControl.Source = bmp;
+        }
+
+        void _viewModel_DataChanged(ChangeableModelBase viewmodel) {
+            RegisterUniquePendingChange(new UpdateSiteAction(_viewModel));
+        }
+
+        #region Properties
+
+        public int SiteID { get; private set; }
+
+        #endregion
+    
+    }
+
+}
---
+++ tags/build-115/BioLink.Client.Material/SiteExplorerMenuBuilder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Controls;
+using BioLink.Client.Extensibility;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+
+    public class SiteExplorerMenuBuilder {
+
+        private static SiteExplorerNodeType NodeType(string elemType) {
+            return (SiteExplorerNodeType)Enum.Parse(typeof(SiteExplorerNodeType), elemType);
+        }
+
+        private static SiteExplorerNodeType NodeType(SiteExplorerNodeViewModel model) {
+            return NodeType(model.ElemType);
+        }
+
+        public static ContextMenu Build(SiteExplorerNodeViewModel node, MaterialExplorer explorer) {
+
+            if (node == null) {
+                return null;
+            }
+
+            ContextMenuBuilder builder = new ContextMenuBuilder(null);
+
+            builder.New("Refresh").Handler(() => { explorer.Refresh(); }).End();
+
+            builder.Separator();
+
+            builder.New("Rename").Handler(() => { node.IsRenaming = true; }).End();
+
+            var addMenu = CreateAddMenu(node, explorer);
+            builder.AddMenuItem(addMenu);
+
+            builder.Separator();
+
+            var type = NodeType(node);
+
+            builder.New("Delete").Handler(() => {
+                switch (type) {
+                    case SiteExplorerNodeType.Region: explorer.DeleteRegion(node);
+                        break;
+                    case SiteExplorerNodeType.SiteGroup: explorer.DeleteSiteGroup(node);
+                        break;
+                    default:
+                        throw new Exception("[Delete] Unhandled site explorer element type: " + node.ElemType);
+                }
+            });
+
+            var pinnable = explorer.CreatePinnable(node);
+            if (pinnable != null) {
+                builder.Separator();
+                builder.New("_Pin to pin board").Handler(() => { PluginManager.Instance.PinObject(pinnable); });
+            }
+
+            builder.Separator();
+
+            builder.New("Details...").Handler(() => {
+                switch (type) {
+                    case SiteExplorerNodeType.Region: explorer.EditRegion(node);
+                        break;
+                    case SiteExplorerNodeType.Site: explorer.EditSite(node);
+                        break;
+                    default:
+                        throw new Exception("[Details] Unhandled site explorer element type: " + node.ElemType);
+                }
+            }).End();
+
+            return builder.ContextMenu;
+        }
+
+        private static MenuItem CreateAddMenu(SiteExplorerNodeViewModel viewModel, MaterialExplorer explorer) {
+            var addMenu = new MenuItem();
+            addMenu.Header = "Add";
+
+            var builder = new MenuItemBuilder();
+
+            switch (NodeType(viewModel)) {
+                case SiteExplorerNodeType.Region:
+                    addMenu.Items.Add(builder.New("New Region").Handler(() => { explorer.AddRegion(viewModel); }).MenuItem);
+                    addMenu.Items.Add(builder.New("New Site Group").Handler(() => { explorer.AddSiteGroup(viewModel); }).MenuItem);
+                    break;
+                case SiteExplorerNodeType.SiteGroup:
+                    addMenu.Items.Add(builder.New("New Site Group").Handler(() => { explorer.AddSiteGroup(viewModel); }).MenuItem);
+                    break;
+                default:
+                    break;
+            }
+
+
+            return addMenu;
+        }
+    }
+
+}
