ba77d47d2584c80fe71914fbb2c442b00162d19b
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-05 11:00:11 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-118/BioLink.Client.Material/SiteDatabaseAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Material {
+
+    public class RenameSiteAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public RenameSiteAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.RenameSite(Model.ElemID, Model.Name);
+        }
+    }
+
+    public class UpdateSiteAction : GenericDatabaseAction<SiteViewModel> {
+
+        public UpdateSiteAction(SiteViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.UpdateSite(Model.Model);
+        }
+
+    }
+
+}
