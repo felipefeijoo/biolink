ceabf741872a62c2ff54a6ed4baa010becce4de2
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-05 10:52:59 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-117/BioLinkUtilities/GraphicsUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace BioLink.Client.Utilities {
+    public static class GraphicsUtils {
+
+        public static BitmapSource SystemDrawingImageToBitmapSource(System.Drawing.Image image) {
+            using (System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(image)) {
+                IntPtr hBitmap = bitmap.GetHbitmap();
+                System.Windows.Media.Imaging.BitmapSource bitmapSource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    System.Windows.Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                SystemUtils.DeleteObject(hBitmap);
+                return bitmapSource;
+            }
+        }
+
+        public static System.Drawing.Drawing2D.HatchStyle? GetHatchStyleFromBrush(System.Drawing.Brush brush) {
+            if (brush is System.Drawing.Drawing2D.HatchBrush) {
+                return (brush as System.Drawing.Drawing2D.HatchBrush).HatchStyle;
+            }
+            return null;
+        }
+
+        public static System.Drawing.Color GetColorFromBrush(System.Drawing.Brush brush) {
+
+            if (brush is System.Drawing.SolidBrush) {
+                return (brush as System.Drawing.SolidBrush).Color;
+            }
+
+            if (brush is System.Drawing.Drawing2D.HatchBrush) {
+                return (brush as System.Drawing.Drawing2D.HatchBrush).ForegroundColor;
+            }
+
+            throw new Exception("Unhandled brush type! Could not extract brush color");
+        }
+
+        public static System.Drawing.Brush CreateBrush(System.Drawing.Color color, System.Drawing.Drawing2D.HatchStyle? hatchStyle) {
+            if (hatchStyle != null && hatchStyle.HasValue) {
+                return new System.Drawing.Drawing2D.HatchBrush(hatchStyle.Value, color, System.Drawing.Color.FromArgb(0,0,0,0));
+            } else {
+                return new System.Drawing.SolidBrush(color);
+            }
+        }
+    }
+}
