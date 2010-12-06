60471950435336665b44822666c3f36d20f3f9a7
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-06 06:45:05 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-120/BioLink.Client.Maps/LayersWindow.xaml.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SharpMap.Layers;
+using System.Collections.ObjectModel;
+using BioLink.Client.Extensibility;
+using SharpMap.Styles;
+using SharpMap;
+using BioLink.Client.Utilities;
+using Microsoft.Win32;
+
+namespace BioLink.Client.Maps {
+    /// <summary>
+    /// Interaction logic for LayersWindow.xaml
+    /// </summary>
+    public partial class LayersWindow : Window {
+
+        #region Design Constructor
+        public LayersWindow() {
+            InitializeComponent();
+        }
+        #endregion
+
+        private MapControl _mapControl;
+        private ObservableCollection<VectorLayerViewModel> _model;
+        private List<StyleViewModel> _styleModel;
+
+        public LayersWindow(MapControl mapControl) {
+            InitializeComponent();
+            _mapControl = mapControl;            
+            _model = new ObservableCollection<VectorLayerViewModel>();
+            foreach (ILayer layer in mapControl.mapBox.Map.Layers) {
+                if (layer is VectorLayer) {
+                    _model.Insert(0, new VectorLayerViewModel(layer as VectorLayer));
+                }
+            }
+
+            lstLayers.ItemsSource = _model;
+
+            _styleModel = new List<StyleViewModel>();
+            _styleModel.Add(new StyleViewModel(null));
+
+            foreach (System.Drawing.Drawing2D.HatchStyle item in Enum.GetValues(typeof(System.Drawing.Drawing2D.HatchStyle))) {
+                _styleModel.Add(new StyleViewModel(item));
+            }
+
+            this.MapBackColor = mapControl.mapBox.BackColor;
+
+            backgroundColorPicker.DataContext = this;
+
+            cmbStyle.ItemsSource = _styleModel;
+        }
+
+        public System.Drawing.Color MapBackColor { get; set; }
+
+        private void lstLayers_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selected = lstLayers.SelectedItem as VectorLayerViewModel;
+            grpLayer.IsEnabled = (selected != null);
+            grpLayer.DataContext = selected;
+            if (selected != null) {
+                var selectedStyle = _styleModel.Find((vm) => {
+                    return vm.HatchStyle == selected.HatchStyle;
+                });
+                cmbStyle.SelectedItem = selectedStyle;
+            } else {
+                cmbStyle.SelectedItem = null;                
+            }
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e) {
+            ApplyChanges();
+        }
+
+        private void ApplyChanges() {
+
+            var map = _mapControl.mapBox.Map;
+
+            map.Layers.Clear();
+
+            for (int i = _model.Count -1; i >=0; i--) {
+                var m = _model[i];
+                var layer = LayerFileLoader.LoadLayer(m.ConnectionID);
+                if (layer != null) {
+                    map.Layers.Add(layer);
+                    if (layer is VectorLayer) {
+                        var vl = layer as VectorLayer;
+                        vl.Style.Fill = m.FillBrush();
+                        vl.Style.EnableOutline = m.DrawOutline;
+                    }
+                } else {
+                    throw new Exception("Could not load layer " + m.ConnectionID + "!");
+                }
+            }
+
+            _mapControl.mapBox.BackColor = MapBackColor;
+
+            _mapControl.mapBox.Refresh();
+
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e) {
+            ApplyChanges();
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void cmbStyle_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var layer = grpLayer.DataContext as VectorLayerViewModel;
+            if (layer != null) {
+                var selected = cmbStyle.SelectedItem as StyleViewModel;
+                layer.HatchStyle = selected.HatchStyle;
+            }
+        }
+
+        private void AddNewLayer() {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Shape Files (*.shp)|*.shp|All files (*.*)|*.*";
+            if (dlg.ShowDialog().ValueOrFalse()) {
+                ILayer layer = LayerFileLoader.LoadLayer(dlg.FileName);
+                if (layer != null && layer is VectorLayer) {
+                    var viewModel = new VectorLayerViewModel(layer as VectorLayer);
+                    _model.Add(viewModel);
+                }
+            }
+        }
+
+        private void RemoveSelectedLayer() {
+            var vm = lstLayers.SelectedItem as VectorLayerViewModel;
+            if (vm != null) {
+                _model.Remove(vm);
+            }
+        }
+
+        private void MoveLayerUp() {
+            var vm = lstLayers.SelectedItem as VectorLayerViewModel;
+            if (vm != null) {
+                int index = _model.IndexOf(vm);
+                if (_model.IndexOf(vm) > 0) {
+                    _model.RemoveAt(index);
+                    _model.Insert(index - 1, vm);
+                }
+                lstLayers.SelectedItem = vm;
+            }
+        }
+
+        private void MoveLayerDown() {
+            var vm = lstLayers.SelectedItem as VectorLayerViewModel;
+            if (vm != null) {
+                int index = _model.IndexOf(vm);
+                if (_model.IndexOf(vm) < _model.Count - 1) {
+                    _model.RemoveAt(index);
+                    _model.Insert(index + 1, vm);
+                }
+                lstLayers.SelectedItem = vm;
+            }
+
+        }
+
+        private void btnAddLayer_Click(object sender, RoutedEventArgs e) {
+            AddNewLayer();
+        }
+
+        private void btnDeleteLayer_Click(object sender, RoutedEventArgs e) {
+            RemoveSelectedLayer();
+        }
+
+        private void btnLayerUp_Click(object sender, RoutedEventArgs e) {
+            MoveLayerUp();
+        }
+
+        private void btnLayerDown_Click(object sender, RoutedEventArgs e) {
+            MoveLayerDown();
+        }
+
+    }
+
+    internal class StyleViewModel : ViewModelBase {
+
+        protected int _iconSize = 15;
+
+        public StyleViewModel(System.Drawing.Drawing2D.HatchStyle? hatchStyle) {
+            this.HatchStyle = hatchStyle;
+            this.Icon = MakeHatchIcon(hatchStyle);
+        }
+
+        private BitmapSource MakeHatchIcon(System.Drawing.Drawing2D.HatchStyle? hatchStyle) {
+
+            System.Drawing.Color foreColor = System.Drawing.Color.Black;
+            System.Drawing.Color backColor = System.Drawing.Color.White;
+            System.Drawing.Bitmap bm = new System.Drawing.Bitmap(_iconSize, _iconSize);
+            System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bm);
+
+            var rect = new System.Drawing.Rectangle(0, 0, _iconSize - 1, _iconSize - 1);
+            if (hatchStyle != null && hatchStyle.HasValue) {
+                g.FillRectangle(new System.Drawing.Drawing2D.HatchBrush(hatchStyle.Value, foreColor, backColor), rect);
+                g.DrawRectangle(new System.Drawing.Pen(foreColor), rect);
+            } else {
+                g.FillRectangle(new System.Drawing.SolidBrush(foreColor), rect);
+            }
+
+            return GraphicsUtils.SystemDrawingImageToBitmapSource(bm);
+        }
+
+        public System.Drawing.Drawing2D.HatchStyle? HatchStyle { get; set;  }
+    }
+
+    internal class VectorLayerViewModel : ViewModelBase {
+
+        private VectorLayer _model;
+        
+        private System.Drawing.Pen _Line;
+        private bool _drawOutline;
+        private System.Drawing.Image _symbol;
+        private System.Drawing.Color _fillColor;
+        private System.Drawing.Drawing2D.HatchStyle? _hatchStyle;
+        
+
+        public VectorLayerViewModel(VectorLayer model) {
+            _model = model;
+            ConnectionID = model.DataSource.ConnectionID;
+
+            _fillColor = GraphicsUtils.GetColorFromBrush(model.Style.Fill);
+            _hatchStyle = GraphicsUtils.GetHatchStyleFromBrush(model.Style.Fill);
+            _Line = model.Style.Line;
+            _drawOutline = model.Style.EnableOutline;
+            _symbol = model.Style.Symbol;
+        }
+
+        public string Name {
+            get { return _model.LayerName; }
+        }
+
+        public System.Drawing.Color FillColor {
+            get { return _fillColor; }
+            set { SetProperty("FillColor", ref _fillColor, value); }
+        }
+
+        public System.Drawing.Drawing2D.HatchStyle? HatchStyle {
+            get { return _hatchStyle; }
+            set { SetProperty("HatchStyle", ref _hatchStyle, value); }
+        }
+
+        public bool DrawOutline {
+            get { return _drawOutline; }
+            set { SetProperty("DrawOutline", ref _drawOutline, value); }
+        }
+
+        public string ConnectionID { get; private set; }
+
+
+        internal System.Drawing.Brush FillBrush() {
+            return GraphicsUtils.CreateBrush(FillColor, HatchStyle);
+        }
+    }
+
+    [ValueConversion(typeof(StyleViewModel), typeof(System.Drawing.Drawing2D.HatchStyle))]
+    internal class HatchStyleConverter : IMultiValueConverter {
+
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            var list = values[1] as List<StyleViewModel>;
+            var style = values[0] as System.Drawing.Drawing2D.HatchStyle?;
+            if (list != null) {
+                foreach (StyleViewModel m in list) {
+                    if (m.HatchStyle == style) {
+                        return m;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) {
+            var vm = value as StyleViewModel;
+            if (vm != null) {
+                return new object[] {vm.HatchStyle, null};
+            }  
+
+            return null;
+        }
+    }
+}
---
+++ tags/build-120/BioLinkDAL/MaterialService.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data.Model;
+
+namespace BioLink.Data {
+
+    public class MaterialService : BioLinkService {
+
+        public MaterialService(User user)
+            : base(user) {
+        }
+
+        #region Site Explorer
+
+        public List<SiteExplorerNode> GetTopLevelExplorerItems() {
+            return GetExplorerElementsForParent(0, SiteExplorerNodeType.Region);
+        }
+
+        public List<SiteExplorerNode> GetExplorerElementsForParent(int parentID, SiteExplorerNodeType parentElemType) {
+            return GetExplorerElementsForParent(parentID, parentElemType.ToString());
+        }
+
+        public List<SiteExplorerNode> GetExplorerElementsForParent(int parentID, string parentElemType) {
+            var mapper = new GenericMapperBuilder<SiteExplorerNode>().build();
+            return StoredProcToList<SiteExplorerNode>("spSiteExplorerList", 
+                mapper, 
+                _P("intParentID", parentID), 
+                _P("vchrParentType", parentElemType)
+            );
+        }
+
+        #endregion
+
+        #region Region
+
+        public Region GetRegion(int regionID) {
+            var mapper = new GenericMapperBuilder<Region>().build();
+            Region result = null;
+            StoredProcReaderFirst("spRegionGet", (reader) => {
+                result = mapper.Map(reader);
+            }, _P("intRegionID", regionID));
+            return result;
+        }
+
+        public void UpdateRegion(int regionID, string name, string rank) {
+            StoredProcUpdate("spRegionUpdate",
+                _P("intRegionID", regionID),
+                _P("vchrName", name),
+                _P("vchrRank", rank)
+            );
+        }
+
+        public void DeleteRegion(int regionID) {
+            StoredProcUpdate("spRegionDelete", _P("intRegionID", regionID));
+        }
+
+        public void RenameRegion(int regionID, string name) {
+            StoredProcUpdate("spRegionRename",
+                _P("intRegionID", regionID),
+                _P("vchrName", name)
+            );
+        }
+
+        public int InsertRegion(string name, int parentID) {
+            var retval = ReturnParam("NewRegionID", System.Data.SqlDbType.Int);
+            StoredProcUpdate("spRegionInsert",
+                _P("vchrName", name),
+                _P("intParentID", parentID),
+                retval
+            );
+            return (int) retval.Value;
+        }
+
+        public List<RegionSearchResult> FindRegions(string searchTerm) {
+            searchTerm = searchTerm.Replace('*', '%') + '%';
+            var mapper = new GenericMapperBuilder<RegionSearchResult>().build();
+            return StoredProcToList("spRegionFind", mapper, _P("vchrRegionToFind", searchTerm));
+        }
+
+        #endregion
+
+        #region Site Group
+
+        public void RenameSiteGroup(int siteGroupID, string name) {
+            StoredProcUpdate("spSiteGroupRename", _P("intSiteGroupID",siteGroupID), _P("vchrSiteGroupName", name));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">The name of the new Site Group</param>
+        /// <param name="parentType">The "type" of the parent: 1 = Region, 2 = Site Group</param>
+        /// <param name="parentID">The parent ID</param>
+        /// <param name="regionID">The region id underwhich this group is placed. Note that even if this is a child of another site group it still needs a region id</param>
+        /// <returns></returns>
+        public int InsertSiteGroup(string name, int parentType, int parentID, int regionID) {
+            var retval = ReturnParam("NewRegionID");
+
+            StoredProcUpdate("spSiteGroupInsert",
+                _P("vchrName", name),
+                _P("sintParentType", parentType),
+                _P("intParentID", parentID),
+                _P("intPoliticalRegionID", regionID),
+                retval
+            );
+
+            return (int)retval.Value;
+        }
+
+        public void DeleteSiteGroup(int siteGroupID) {
+            StoredProcUpdate("spSiteGroupDelete", _P("intSiteGroupID", siteGroupID));
+        }
+
+        public void MergeSiteGroups(int sourceID, int targetID) {
+            StoredProcUpdate("spSiteGroupMerge",
+                _P("intOldSiteGroupID", sourceID),
+                _P("intNewSiteGroupID", targetID)
+            );
+        }
+
+        public void MoveSiteGroup(int siteGroupID, int newParentType, int newParentID, int newRegionID) {
+            StoredProcUpdate("spSiteGroupMove",
+                _P("intSiteGroupID", siteGroupID),
+                _P("sintParentType", newParentType),
+                _P("intParentID", newParentID),
+                _P("intPoliticalRegionID", newRegionID)
+            );
+        }
+
+        public List<int> GetSiteGroupSiteIDList(int siteGroupID) {            
+            var results = new List<int>();
+            StoredProcReaderForEach("spSiteGroupGetSiteIDList",
+                (reader) => { results.Add(reader.GetInt32(0)); },
+                _P("intSiteGroupID", siteGroupID)
+            );
+
+            return results;
+        }
+
+        #endregion
+
+        #region Site
+
+        public Site GetSite(int siteID) {
+            Site result = null;
+            var mapper = new GenericMapperBuilder<Site>().Override(new TintToBoolConvertingMapper("tintTemplate")).build();
+            StoredProcReaderFirst("spSiteGet", (reader) => {
+                result = mapper.Map(reader);
+            }, _P("intSiteID", siteID));
+            return result;
+        }
+
+        public void RenameSite(int siteID, string name) {
+            StoredProcUpdate("spSiteUpdateName",
+                _P("intSiteID", siteID),
+                _P("vchrSiteName", name)
+            );
+        }
+
+        public void DeleteSite(int siteID) {
+            StoredProcUpdate("spSiteDelete", _P("intSiteID", siteID));
+        }
+
+        public int InsertSite(int politicalRegion, int siteGroupId, int basedOnSiteId = -1) {
+            var retval = ReturnParam("NewSiteID", System.Data.SqlDbType.Int);
+            StoredProcUpdate("spSiteInsert",
+                _P("intPoliticalRegionID", politicalRegion),
+                _P("intSiteGroupID", siteGroupId),
+                _P("intBasedOnSiteID", basedOnSiteId),
+                retval);
+            return (int)retval.Value;
+        }
+
+        public void UpdateSite(Site site) {
+            StoredProcUpdate("spSiteUpdate",
+                _P("intSiteID", site.SiteID),
+                _P("vchrSiteName", site.SiteName),
+                _P("intPoliticalRegionID", site.PoliticalRegionID),
+                _P("tintLocalType", site.LocalityType),
+                _P("vchrLocal", site.Locality),
+                _P("vchrDistanceFromPlace", site.DistanceFromPlace),
+                _P("vchrDirFromPlace", site.DirFromPlace),
+                _P("vchrInformalLocal", site.InformalLocal),
+                _P("tintPosCoordinates", site.PosCoordinates),
+                _P("tintPosAreaType", site.PosAreaType),
+                _P("fltPosX1", site.PosX1),
+                _P("fltPosY1", site.PosY1),
+                _P("fltPosX2", site.PosX2),
+                _P("fltPosY2", site.PosY2),
+                _P("tintPosXYDisplayFormat", site.PosXYDisplayFormat),
+                _P("vchrPosSource", site.PosSource),
+                _P("vchrPosError", site.PosError),
+                _P("vchrPosWho", site.PosWho),
+                _P("vchrPosDate", site.PosDate),
+                _P("vchrPosOriginal", site.PosOriginal),
+                _P("vchrPosUTMSource", site.PosUTMSource),
+                _P("vchrPosUTMMapProj", site.PosUTMMapProj),
+                _P("vchrPosUTMMapName", site.PosUTMMapName),
+                _P("vchrPosUTMMapVer", site.PosUTMMapVer),
+                _P("tintElevType", site.ElevType),
+                _P("fltElevUpper", site.ElevUpper),
+                _P("fltElevLower", site.ElevLower),
+                _P("fltElevDepth", site.ElevDepth),
+                _P("vchrElevUnits", site.ElevUnits),
+                _P("vchrElevSource", site.ElevSource),
+                _P("vchrElevError", site.ElevError),
+                _P("vchrGeoEra", site.GeoEra),
+                _P("vchrGeoState", site.GeoState),
+                _P("vchrGeoPlate", site.GeoPlate),
+                _P("vchrGeoFormation", site.GeoFormation),
+                _P("vchrGeoMember", site.GeoMember),
+                _P("vchrGeoBed", site.GeoBed),
+                _P("vchrGeoName", site.GeoName),
+                _P("vchrGeoAgeBottom", site.GeoAgeBottom),
+                _P("vchrGeoAgeTop", site.GeoAgeTop),
+                _P("vchrGeoNotes", site.GeoNotes)
+            );
+
+        }
+
+        #endregion
+
+        #region Site Visit
+
+        public void RenameSiteVisit(int siteVisitID, string name) {
+            StoredProcUpdate("spSiteVisitUpdateName", _P("intSiteVisitID", siteVisitID), _P("vchrName", name));
+        }
+
+        public void DeleteSiteVisit(int siteVisitID) {
+            StoredProcUpdate("spSiteVisitDelete", _P("intSiteVisitID", siteVisitID));
+        }
+
+        public int InsertSiteVisit(int parentID, int basedOnSiteVisitID = -1) {
+            var retval = ReturnParam("NewSiteID", System.Data.SqlDbType.Int);
+            StoredProcUpdate("spSiteVisitInsert",
+                _P("intParentID", parentID),
+                _P("intBasedOnSiteVisitID", basedOnSiteVisitID),
+                retval);
+
+            return (int)retval.Value;
+        }
+
+        public void UpdateSiteVisit(SiteVisit siteVisit) {
+            StoredProcUpdate("spSiteVisitUpdate",
+                _P("intSiteVisitID", siteVisit.SiteVisitID),
+                _P("vchrSiteVisitName", siteVisit.SiteVisitName),
+                _P("vchrFieldNumber", siteVisit.FieldNumber),
+                _P("vchrCollector", siteVisit.Collector),
+                _P("tintDateType", siteVisit.DateType),
+                _P("intDateStart", siteVisit.DateStart),
+                _P("intDateEnd", siteVisit.DateEnd),
+                _P("intTimeStart", siteVisit.TimeStart),
+                _P("intTimeEnd", siteVisit.TimeEnd),
+                _P("vchrCasualTime", siteVisit.CasualTime));
+        }
+
+        public SiteVisit GetSiteVisit(int siteVisitID) {
+            var mapper = new GenericMapperBuilder<SiteVisit>().Override(new TintToBoolConvertingMapper("tintTemplate")).build();
+            SiteVisit result = null;
+            StoredProcReaderFirst("spSiteVisitGet", (reader) => {
+                result = mapper.Map(reader);
+            }, _P("intSiteVisitID", siteVisitID));
+
+            return result;
+        }
+
+        public List<string> GetDistinctCollectors() {
+            var list = new List<string>();
+            StoredProcReaderForEach("spCollectorListDistinct", (reader) => {
+                list.Add(reader[1] as string);
+            });
+            return list;
+        }
+
+        #endregion
+
+        #region Trap
+
+        public void RenameTrap(int trapID, string name) {
+            StoredProcUpdate("spTrapUpdateName", _P("intTrapID", trapID), _P("vchrTrapName", name));
+        }
+
+        #endregion
+
+        #region Material
+
+        public void RenameMaterial(int materialID, string name) {
+            StoredProcUpdate("spMaterialUpdateName", _P("intMaterialID", materialID), _P("vchrName", name));
+        }
+
+        #endregion
+
+    }
+}
