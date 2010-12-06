ffe3ba643ad056a9eb9dfc0b9d513ed4576c6afb
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-06 11:47:40 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-122/BioLink.Client.Extensibility/LookupControl.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for LookupControl.xaml
+    /// </summary>
+    public partial class LookupControl : UserControl {
+
+        #region Designer Constructor
+        public LookupControl() {
+            InitializeComponent();
+
+            this.GotFocus += new RoutedEventHandler((source, e) => {
+                txt.Focus();
+            });
+        }
+        #endregion
+
+        public void BindUser(User user, LookupType lookupType) {
+            User = user;
+            LookupType = lookupType;
+        }
+
+        private void btnLookup_Click(object sender, RoutedEventArgs e) {
+            LaunchLookup();
+        }
+
+        private void LaunchLookup() {
+            Type t = null;
+            switch (LookupType) {
+                case LookupType.Reference:
+                    t = typeof(ReferenceSearchResult);
+                    break;
+                case LookupType.Region:
+                    t = typeof(Region);
+                    break;
+                default:
+                    throw new Exception("Unhandled Lookup type: " + LookupType.ToString());
+            }
+
+            if (t != null) {
+                PluginManager.Instance.StartSelect(t, (result) => {
+                    this.Text = result.Description;
+                    this.ObjectID = result.ObjectID;
+                    this.InvokeIfRequired(() => {
+                        txt.Focus();
+                    });
+                });
+            }
+        }
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(LookupControl), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnTextChanged)));
+
+        private static void OnTextChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args) {
+            var control = (LookupControl)obj;
+            control.txt.Text = args.NewValue as String;
+            control.FireValueChanged(control.txt.Text);
+        }
+
+        protected void FireValueChanged(string text) {
+            if (this.ValueChanged != null) {
+                ValueChanged(this, text);
+            }
+        }
+
+        public String Text {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ObjectIDProperty = DependencyProperty.Register("ObjectID", typeof(int?), typeof(LookupControl), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnObjectIDChanged)));
+
+        private static void OnObjectIDChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args) {
+            //var control = (LookupControl) color;
+            //control.txt.Text = args.NewValue as String;
+            //control.FireValueChanged(control.txt.Text);
+        }
+
+        public int? ObjectID {
+            get { return (int?)GetValue(ObjectIDProperty); }
+            set { SetValue(ObjectIDProperty, value); }
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        public User User { get; private set; }
+
+        public LookupType LookupType { get; private set; }
+
+        #endregion
+
+        #region Events
+
+        public event TextChangedHandler ValueChanged;
+
+        #endregion
+
+    }
+
+    public enum LookupType {
+        Taxon,
+        Region,
+        Material,
+        Site,
+        SiteVisit,
+        Trap,
+        Reference
+    }
+
+}
---
+++ tags/build-122/BioLink.Client.Material/MaterialExplorer.xaml.cs
@@ -0,0 +1,466 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+using System.Collections.ObjectModel;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class MaterialExplorer : DatabaseActionControl {
+
+        #region Designer constructor
+
+        public MaterialExplorer() {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        public MaterialExplorer(MaterialPlugin owner)
+            : base(owner.User, "MaterialExplorer") {
+
+            InitializeComponent();
+
+            this.ChangeRegistered += new Action<IList<DatabaseAction>>((list) => {
+                btnApply.IsEnabled = true;
+                btnCancel.IsEnabled = true;
+            });
+
+            this.ChangesCommitted += new PendingChangesCommittedHandler((list) => {
+                ReloadModel();
+                btnApply.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+            });
+
+            this.Owner = owner;
+
+            this.RememberExpanded = Config.GetGlobal<bool>("Material.RememberExpandedNodes", true);
+
+            var findScopes = new List<MaterialFindScope>();
+            findScopes.Add(new MaterialFindScope("Find in all", ""));
+            findScopes.Add(new MaterialFindScope("Site name", "site"));
+            findScopes.Add(new MaterialFindScope("Trap name", "trap"));
+            findScopes.Add(new MaterialFindScope("Visit name", "visit"));
+            findScopes.Add(new MaterialFindScope("Material name", "material"));
+            findScopes.Add(new MaterialFindScope("Accession No.", "accessionno"));
+            findScopes.Add(new MaterialFindScope("Registraton No.", "regno"));            
+            findScopes.Add(new MaterialFindScope("Region name", "region"));
+            findScopes.Add(new MaterialFindScope("Site group name", "group"));
+            findScopes.Add(new MaterialFindScope("Collector #", "collector"));
+
+            cmbFindScope.ItemsSource = findScopes;
+            cmbFindScope.DisplayMemberPath = "Label";
+
+        }
+
+        public void InitializeMaterialExplorer() {
+            this.InvokeIfRequired(() => {
+                LoadExplorerModel();
+                if (RegionsModel != null && RememberExpanded) {
+                    // Now see if we can auto-expand from the last session...                    
+                    var expanded = Config.GetProfile<List<String>>(Owner.User, "Material.Explorer.ExpandedNodes.Region", null);
+                    ExpandParentages(RegionsModel, expanded);                    
+                }
+                
+            });
+        }
+
+        private void LoadExplorerModel() {
+            var service = new MaterialService(User);
+
+            // Region explorer...
+            var list = service.GetTopLevelExplorerItems();
+            RegionsModel = BuildExplorerModel(list);
+            regionsNode.ItemsSource = RegionsModel;
+            regionsNode.IsExpanded = true;
+
+            // Unplaced sites (Sites with no region)...
+            list = service.GetTopLevelExplorerItems(SiteExplorerNodeType.Unplaced);
+            UnplacedModel = BuildExplorerModel(list);
+            unplacedNode.ItemsSource = UnplacedModel;            
+        }
+
+        private void ReloadModel() {
+
+            List<String> expanded = null;
+            if (RememberExpanded) {
+                expanded = GetExpandedParentages(RegionsModel);
+            }
+
+            LoadExplorerModel();
+
+            if (expanded != null && expanded.Count > 0) {
+                ExpandParentages(RegionsModel, expanded);
+            }
+
+            ClearPendingChanges();
+        }
+
+        private ObservableCollection<HierarchicalViewModelBase> BuildExplorerModel(List<SiteExplorerNode> list) {
+
+            list.Sort((item1, item2) => {
+                int compare = item1.ElemType.CompareTo(item2.ElemType);
+                if (compare == 0) {
+                    return item1.Name.CompareTo(item2.Name);
+                }
+                return compare;
+            });
+
+
+            var regionsModel = new ObservableCollection<HierarchicalViewModelBase>(list.ConvertAll((model) => {
+                var viewModel = new SiteExplorerNodeViewModel(model);
+
+                if (model.NumChildren > 0) {
+                    viewModel.Children.Add(new ViewModelPlaceholder("Loading..."));
+                    viewModel.LazyLoadChildren += new HierarchicalViewModelAction(viewModel_LazyLoadChildren);
+                }
+                return viewModel;
+            }));
+            return regionsModel;
+        }
+
+        void viewModel_LazyLoadChildren(HierarchicalViewModelBase item) {
+            var parent = item as SiteExplorerNodeViewModel;
+            if (parent != null) {
+                parent.Children.Clear();
+                var service = new MaterialService(User);
+                var list = service.GetExplorerElementsForParent(parent.ElemID, parent.ElemType);
+                var viewModel = BuildExplorerModel(list);
+                foreach (HierarchicalViewModelBase childViewModel in viewModel) {
+                    childViewModel.Parent = parent;
+                    parent.Children.Add(childViewModel);
+                }                
+            }
+        }
+
+        private void TreeViewItem_MouseRightButtonDown(object sender, MouseEventArgs e) {
+        }
+
+        private void EditableTextBlock_EditingComplete(object sender, string text) {
+            var selected = tvwMaterial.SelectedItem as SiteExplorerNodeViewModel;
+            if (selected != null) {
+                selected.Name = text;
+                switch (selected.NodeType) {
+                    case SiteExplorerNodeType.Region:
+                        RegisterPendingChange(new RenameRegionAction(selected));
+                        break;
+                    case SiteExplorerNodeType.SiteGroup:
+                        RegisterPendingChange(new RenameSiteGroupAction(selected));
+                        break;
+                    case SiteExplorerNodeType.Site:
+                        RegisterPendingChange(new RenameSiteAction(selected));
+                        break;
+                    case SiteExplorerNodeType.SiteVisit:
+                        RegisterPendingChange(new RenameSiteVisitAction(selected));
+                        break;                
+                    case SiteExplorerNodeType.Trap:
+                        RegisterPendingChange(new RenameTrapAction(selected));
+                        break;
+                    case SiteExplorerNodeType.Material:
+                        RegisterPendingChange(new RenameMaterialAction(selected));
+                        break;
+                    default:
+                        throw new NotImplementedException(selected.NodeType.ToString());
+                }                
+            }
+        }
+
+        private void EditableTextBlock_EditingCancelled(object sender, string oldtext) {
+        }
+
+        private void treeview_MouseRightButtonUp(object sender, MouseButtonEventArgs e) {
+            ShowContextMenu(sender as TreeView);
+        }
+
+        private void ShowContextMenu(TreeView tvw) {
+            var selected = tvw.SelectedItem as SiteExplorerNodeViewModel;
+            if (selected != null) {
+                var menu  = SiteExplorerMenuBuilder.Build(selected, this);
+                if (menu != null) {
+                    tvw.ContextMenu = menu;
+                }
+            }
+        }
+
+        public List<string> GetExpandedParentages(ObservableCollection<HierarchicalViewModelBase> model) {
+            List<string> list = new List<string>();
+            CollectExpandedParentages(model, list);
+            return list;
+        }
+
+        private void CollectExpandedParentages(ObservableCollection<HierarchicalViewModelBase> model, List<string> list) {
+            foreach (SiteExplorerNodeViewModel tvm in model) {
+                if (tvm.IsExpanded) {
+                    list.Add(tvm.GetParentage());
+                    if (tvm.Children != null && tvm.Children.Count > 0) {
+                        CollectExpandedParentages(tvm.Children, list);
+                    }
+                }
+            }
+        }
+
+        public void ExpandParentages(ObservableCollection<HierarchicalViewModelBase> model, List<string> expanded) {
+            if (expanded != null && expanded.Count > 0) {
+                var todo = new Stack<HierarchicalViewModelBase>(model);
+                while (todo.Count > 0) {
+                    var vm = todo.Pop();
+                    if (vm is SiteExplorerNodeViewModel) {
+                        var tvm = vm as SiteExplorerNodeViewModel;
+                        string parentage = tvm.GetParentage();
+                        if (expanded.Contains(parentage)) {
+                            tvm.IsExpanded = true;
+                            expanded.Remove(parentage);
+                            tvm.Children.ForEach(child => todo.Push(child));
+                        }
+                    }
+                }
+            }
+        }
+
+        internal SiteExplorerNodeViewModel AddNewNode(SiteExplorerNodeViewModel parent, SiteExplorerNodeType nodeType, Func<SiteExplorerNodeViewModel, DatabaseAction> actionFactory) {
+
+            parent.IsExpanded = true;
+
+            var model = new SiteExplorerNode();
+            model.Name = string.Format("<New {0}>", nodeType.ToString());
+            model.ParentID = parent.ElemID;
+            model.ElemType = nodeType.ToString();
+            model.ElemID = -1;
+            model.RegionID = -1;
+
+            var viewModel = new SiteExplorerNodeViewModel(model);
+            viewModel.Parent = parent;
+            parent.Children.Add(viewModel);
+            viewModel.IsSelected = true;
+            viewModel.IsRenaming = true;
+
+            if (actionFactory != null) {
+                RegisterPendingChange(actionFactory(viewModel));
+            }
+
+            return viewModel;
+        }
+
+        internal void AddRegion(SiteExplorerNodeViewModel parent) {
+            AddNewNode(parent, SiteExplorerNodeType.Region, (viewModel) => { return new InsertRegionAction(viewModel); });
+        }
+
+        internal void AddSiteGroup(SiteExplorerNodeViewModel parent) {
+            AddNewNode(parent, SiteExplorerNodeType.SiteGroup, (viewModel) => { return new InsertSiteGroupAction(viewModel); });
+        }
+
+        internal void AddSite(SiteExplorerNodeViewModel parent) {
+            AddNewNode(parent, SiteExplorerNodeType.Site, (viewModel) => { return new InsertSiteAction(viewModel); });
+        }
+
+        internal void AddSiteVisit(SiteExplorerNodeViewModel parent) {
+            AddNewNode(parent, SiteExplorerNodeType.SiteVisit, (viewModel) => { return new InsertSiteVisitAction(viewModel); });
+        }
+
+        internal void AddTrap(SiteExplorerNodeViewModel parent) {
+            AddNewNode(parent, SiteExplorerNodeType.Trap, (viewModel) => { return new InsertTrapAction(viewModel); });
+        }
+
+        private void EditNode(SiteExplorerNodeViewModel node, Func<DatabaseActionControl> editorFactory) {
+            if (node.ElemID < 0) {
+                ErrorMessage.Show("You must first apply the changes before editing the details of this item!");
+                return;
+            } else {
+                var editor = editorFactory();                
+                var caption = string.Format("{0} Detail {1} [{2}]", node.NodeType.ToString(), node.Name, node.ElemID);
+                PluginManager.Instance.AddNonDockableContent(Owner, editor, caption, SizeToContent.WidthAndHeight);
+            }
+        }
+
+        public void EditNode(SiteExplorerNodeViewModel node) {
+            switch (node.NodeType) {
+                case SiteExplorerNodeType.Site:
+                    EditSite(node);
+                    break;
+                case SiteExplorerNodeType.Region:
+                    EditRegion(node);
+                    break;
+                case SiteExplorerNodeType.SiteGroup:
+                    break;
+                case SiteExplorerNodeType.SiteVisit:
+                    EditSiteVisit(node);
+                    break;
+                case SiteExplorerNodeType.Material:
+                    EditMaterial(node);
+                    break;
+                case SiteExplorerNodeType.Trap:
+                    EditTrap(node);
+                    break;
+            }
+        }
+
+        internal void EditSiteVisit(SiteExplorerNodeViewModel sitevisit) {
+            EditNode(sitevisit, () => { return new SiteVisitDetails(User, sitevisit.ElemID); });
+        }
+
+        internal void EditMaterial(SiteExplorerNodeViewModel material) {
+            throw new NotImplementedException();
+        }
+
+        internal void EditTrap(SiteExplorerNodeViewModel trap) {
+            EditNode(trap, () => { return new TrapDetails(User, trap.ElemID); });
+        }
+
+        internal void EditRegion(SiteExplorerNodeViewModel region) {
+            EditNode(region, () => { return new RegionDetails(User, region.ElemID ); });
+        }
+
+        internal void EditSite(SiteExplorerNodeViewModel site) {
+            EditNode(site, () => { return new SiteDetails(User, site.ElemID); });
+        }
+
+        internal void DeleteNode(SiteExplorerNodeViewModel node, Func<DatabaseAction> actionFactory) {
+
+            if (!node.IsDeleted) {
+                node.Traverse((child) => {
+                    child.IsDeleted = true;
+                });
+
+                if (actionFactory != null) {
+                    RegisterPendingChange(actionFactory());
+                }
+            }
+        }
+
+        internal void DeleteRegion(SiteExplorerNodeViewModel region) {
+            DeleteNode(region, () => { return new DeleteRegionAction(region.ElemID); });
+        }
+
+        internal void DeleteSiteGroup(SiteExplorerNodeViewModel group) {
+            DeleteNode(group, () => { return new DeleteSiteGroupAction(group.ElemID); });
+        }
+
+        internal void DeleteSite(SiteExplorerNodeViewModel group) {
+            DeleteNode(group, () => { return new DeleteSiteAction(group.ElemID); });
+        }
+
+        internal void DeleteSiteVisit(SiteExplorerNodeViewModel group) {
+            DeleteNode(group, () => { return new DeleteSiteVisitAction(group.ElemID); });
+        }
+
+        internal void DeleteTrap(SiteExplorerNodeViewModel trap) {
+            DeleteNode(trap, () => { return new DeleteTrapAction(trap.ElemID); });
+        }
+
+        private void tvwMaterial_MouseRightButtonDown(object sender, MouseButtonEventArgs e) {
+            TreeViewItem item = sender as TreeViewItem;
+            if (item != null) {
+                item.Focus();
+                e.Handled = true;
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+            ReloadModel();            
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e) {
+            ApplyChanges();
+        }
+
+        private void ApplyChanges() {
+            CommitPendingChanges();
+        }
+
+        private Action<SelectionResult> _selectionCallback;
+
+        public void BindSelectCallback(Action<SelectionResult> selectionFunc) {
+            if (selectionFunc != null) {
+                btnSelect.Visibility = Visibility.Visible;
+                btnSelect.IsEnabled = true;
+                _selectionCallback = selectionFunc;
+            } else {
+                ClearSelectCallback();
+            }
+
+        }
+
+        public void ClearSelectCallback() {
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e) {
+            var selected = tvwMaterial.SelectedItem as SiteExplorerNodeViewModel;
+            if (selected != null && _selectionCallback != null) {
+                var result = new SelectionResult();
+                result.ObjectID = selected.ElemID;
+                result.Description = selected.Name;
+                _selectionCallback(result);
+            }
+        }
+
+
+        internal void Refresh() {
+            if (HasPendingChanges) {            
+                if (this.Question("You have unsaved changes. Refreshing will cause those changes to be discarded. Are you sure you want to discard unsaved changes?", "Discard unsaved changes?")) {
+                    ReloadModel();
+                }
+            } else {
+                ReloadModel();
+            }            
+        }
+
+        #region Properties
+
+        public MaterialPlugin Owner { get; private set; }
+
+        public ObservableCollection<HierarchicalViewModelBase> RegionsModel { get; private set; }
+
+        public ObservableCollection<HierarchicalViewModelBase> UnplacedModel { get; private set; }
+
+        internal bool RememberExpanded { get; private set; }
+
+        #endregion
+
+        internal PinnableObject CreatePinnable(SiteExplorerNodeViewModel node) {
+            return new PinnableObject(MaterialPlugin.MATERIAL_PLUGIN_NAME, string.Format("{0}:{1}", node.NodeType.ToString(), node.ElemID));            
+        }
+
+        private void btnFind_Click(object sender, RoutedEventArgs e) {
+            DoFind();
+        }
+
+        private void DoFind() {
+            var service = new MaterialService(User);
+            var scope = cmbFindScope.SelectedItem as MaterialFindScope;
+            string limitations = "";
+            if (scope != null) {
+                limitations = scope.Value;
+            }
+            var list = service.FindNodesByName(txtFind.Text, limitations);
+            var findModel = BuildExplorerModel(list);
+            tvwFind.ItemsSource = findModel;
+        }
+    }
+
+    internal class MaterialFindScope {
+
+        public MaterialFindScope(string label, string value) {
+            this.Label = label;
+            this.Value = value;
+        }
+
+        public String Label { get; private set; }
+        public String Value { get; private set; }
+    }
+
+}
