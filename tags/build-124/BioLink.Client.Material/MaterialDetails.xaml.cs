1a7502c4588e8ccd9980aa373bfe41cb2f784159
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-08 09:50:06 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-124/BioLink.Client.Material/MaterialDetails.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for MaterialDetails.xaml
+    /// </summary>
+    public partial class MaterialDetails : DatabaseActionControl {
+        #region Designer Constructor
+        public MaterialDetails() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public MaterialDetails(User user, int materialID) : base(user, "Material:" + materialID) {
+            InitializeComponent();
+            var service = new MaterialService(user);
+            var model = service.GetMaterial(materialID);
+            var viewModel = new MaterialViewModel(model);
+            viewModel.DataChanged += new DataChangedHandler(viewModel_DataChanged);
+            this.DataContext = viewModel;
+
+            // General tab
+            txtAccessionNumber.BindUser(User, "MaterialAccessionNo", "tblMaterial", "vchrAccessionNo");            
+            txtRegistrationNumber.BindUser(User, "MaterialRegNo", "tblMaterial", "vchrRegNo" );
+            txtCollectorNo.BindUser(User, "MaterialCollectorNo", "tblMaterial", "vchrCollectorNo");
+
+            txtAbundance.BindUser(user, PickListType.Phrase, "Material Abundance", TraitCategoryType.Material);
+            txtSource.BindUser(user, PickListType.Phrase, "Material Source", TraitCategoryType.Material);
+            txtInstitution.BindUser(user, PickListType.Phrase, "Institution", TraitCategoryType.Material);
+            txtCollectionMethod.BindUser(user, PickListType.Phrase, "Collection Method", TraitCategoryType.Material);
+            txtMacroHabitat.BindUser(user, PickListType.Phrase, "Macro Habitat", TraitCategoryType.Material);
+            txtMicroHabitat.BindUser(user, PickListType.Phrase, "Micro Habitat", TraitCategoryType.Material);
+
+            txtTrap.BindUser(User, LookupType.Trap);
+
+            // Identification tab
+            txtIdentification.BindUser(User, LookupType.Taxon);
+            txtIdentifiedBy.BindUser(User, "tblMaterial", "vchrIDBy");
+            txtReference.BindUser(User, LookupType.Reference);
+            txtAccuracy.BindUser(User, PickListType.Phrase, "Identification Accuracy", TraitCategoryType.Material);
+            txtMethod.BindUser(User, PickListType.Phrase, "Identification Method", TraitCategoryType.Material);
+            txtNameQual.BindUser(User, PickListType.Phrase, "Identification Qualifier", TraitCategoryType.Material);
+
+            var idhistory = new IdentificationHistoryControl(user, materialID);
+            idhistory.Margin = new Thickness(0);
+            tabIDHistory.Content = idhistory;
+        }
+
+        void viewModel_DataChanged(ChangeableModelBase viewmodel) {
+            var mvm = viewmodel as MaterialViewModel;
+            if (mvm != null) {
+                RegisterUniquePendingChange(new UpdateMaterialAction(mvm));
+            }
+
+        }
+    }
+}
---
+++ tags/build-124/BioLink.Client.Material/RegionDatabaseAction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+
+    public class RenameRegionAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public RenameRegionAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.RenameRegion(Model.ElemID, Model.Name);
+        }
+    }
+
+    public class InsertRegionAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public InsertRegionAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.ElemID = service.InsertRegion(Model.Name, Model.ParentID);
+            Model.RegionID = Model.ElemID;
+            foreach (SiteExplorerNodeViewModel child in Model.Children) {
+                child.ParentID = Model.ElemID;
+            }
+        }
+
+    }
+
+    public class DeleteRegionAction : DatabaseAction {
+
+        public DeleteRegionAction(int regionID) {
+            this.RegionID = regionID;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteRegion(RegionID);
+        }
+
+        public int RegionID { get; private set; }
+    }
+
+}
