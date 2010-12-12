641ff4a2381463fdbc445233927abc6f1ba14cc6
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-12 12:03:25 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-129/BioLink.Client.Material/SiteVisitDatabaseAction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Material {
+
+    public class RenameSiteVisitAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public RenameSiteVisitAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.RenameSiteVisit(Model.ElemID, Model.Name);
+        }
+
+    }
+
+    public class DeleteSiteVisitAction : DatabaseAction {
+
+        public DeleteSiteVisitAction(int siteVisitID) {
+            this.SiteVisitID = siteVisitID;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteSiteVisit(SiteVisitID);
+        }
+
+        public int SiteVisitID { get; private set; }
+    }
+
+    public class InsertSiteVisitAction : AbstractSiteExplorerAction {
+
+        public InsertSiteVisitAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.ElemID = service.InsertSiteVisit(Model.ParentID);
+            UpdateChildrenParentID();
+        }
+    }
+
+    public class UpdateSiteVisitAction : GenericDatabaseAction<SiteVisit> {
+        public UpdateSiteVisitAction(SiteVisit model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            if (Model.DateStart.GetValueOrDefault(-1) < 0 && Model.DateEnd.GetValueOrDefault(-1) < 0) {
+                Model.DateType = 2;
+            } else {
+                Model.DateType = 1;
+            }
+            service.UpdateSiteVisit(Model);
+        }
+
+    }
+
+}
