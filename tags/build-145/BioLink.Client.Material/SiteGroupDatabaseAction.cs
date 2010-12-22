1b6025472464697d00f7d1fa99d841a65b9aece2
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-22 07:07:50 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-145/BioLink.Client.Material/SiteGroupDatabaseAction.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Material {
+
+    public class RenameSiteGroupAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public RenameSiteGroupAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.RenameSiteGroup(Model.ElemID, Model.Name);
+        }
+
+    }
+
+    public abstract class AbstractSiteExplorerAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public AbstractSiteExplorerAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+            this.Parent = model.Parent as SiteExplorerNodeViewModel;
+        }
+
+        protected int FindRegionID(SiteExplorerNodeViewModel node) {
+            return FindIDOfParentType(node, SiteExplorerNodeType.Region);
+        }
+
+        protected int FindIDOfParentType(SiteExplorerNodeViewModel node, SiteExplorerNodeType nodeType, int defaultId = -1) {
+            HierarchicalViewModelBase p = node;
+
+            while (p != null && (p as SiteExplorerNodeViewModel).NodeType != nodeType) {
+                p = p.Parent;
+            }
+            if (p != null) {
+                return (p as SiteExplorerNodeViewModel).ElemID;
+            }
+
+            return defaultId;
+        }
+
+        protected void UpdateChildrenParentID() {
+            foreach (SiteExplorerNodeViewModel child in Model.Children) {
+                child.ParentID = Model.ElemID;
+            }
+
+        }
+
+        internal SiteExplorerNodeViewModel Parent { get; private set; }
+
+    }
+
+    public class InsertSiteGroupAction : AbstractSiteExplorerAction {
+
+        public InsertSiteGroupAction(SiteExplorerNodeViewModel model)
+            : base(model) {            
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            var parentID = Parent.ElemID;
+            var parentType = 2;
+            if (Parent.NodeType == SiteExplorerNodeType.Region) {
+                // Weird! Need to do this otherwise stored proc crashes
+                parentID = 0;
+                parentType = 1;
+            }
+            var regionID = FindRegionID(Model);
+            Model.ElemID = service.InsertSiteGroup(Model.Name, parentType, parentID, regionID);
+            base.UpdateChildrenParentID();
+        }
+
+    }
+
+    public class DeleteSiteGroupAction : DatabaseAction {
+
+        public DeleteSiteGroupAction(int siteGroupID) {
+            this.SiteGroupID = siteGroupID;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteSiteGroup(SiteGroupID);
+        }
+
+        public int SiteGroupID { get; private set; }
+    }
+
+    public class MergeSiteGroupAction : GenericDatabaseAction<SiteExplorerNode> {
+
+        public MergeSiteGroupAction(SiteExplorerNode source, SiteExplorerNode dest) 
+            : base(source) {
+            Dest = dest;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.MergeSiteGroup(Model.ElemID, Dest.ElemID);
+        }
+
+        public SiteExplorerNode Dest { get; private set; }
+    }
+
+    public class MoveSiteGroupAction : GenericDatabaseAction<SiteExplorerNode> {
+
+        public MoveSiteGroupAction(SiteExplorerNode source, SiteExplorerNode newParent)
+            : base(source) {
+                NewParent = newParent;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            int parentType = 0;
+            int parentID = 0;            
+            if (NewParent.ElemType == "Region") {
+                parentType = 1;
+                parentID = 0;
+            } else {
+                parentType = 2;
+                parentID = NewParent.ElemID;
+            }
+            service.MoveSiteGroup(Model.ElemID, parentType, parentID, NewParent.RegionID);
+        }
+
+
+        public SiteExplorerNode NewParent { get; private set; }
+    }
+
+}
---
+++ tags/build-145/BioLinkDAL/Model/AutoNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class AutoNumber : GUIDObject {
+
+        public int AutoNumberCatID { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string Postfix { get; set; }
+        public int NumLeadingZeros { get; set; }
+        public int LastNumber { get; set; }
+        public bool Locked { get; set; }
+        public bool EnsureUnique { get; set; }
+    }
+
+    public class NewAutoNumber : AutoNumber {
+        public int NewNumber { get; set; }
+
+        public string FormattedNumber {
+
+            get {
+                string format = null;
+                if (NumLeadingZeros > 0) {
+                    format = string.Format("{{0}}{{1:{0}}}{{2}}", new string('0', NumLeadingZeros));
+                } else {
+                    format = "{0}{1}{2}";
+                }
+                return string.Format(format, Prefix, NewNumber, Postfix);
+            }
+
+        }
+    }
+    
+}
