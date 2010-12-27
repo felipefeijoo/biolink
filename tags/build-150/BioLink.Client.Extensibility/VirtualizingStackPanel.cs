b441d1ec2cf363df9941a577dbf995089034c7c7
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-27 02:35:08 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-150/BioLink.Client.Extensibility/VirtualizingStackPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BioLink.Client.Extensibility {
+
+    [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(VTreeViewItem))]
+    public class VTreeView : TreeView {
+        protected override DependencyObject GetContainerForItemOverride() {
+            return new VTreeViewItem();
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item) {
+            base.PrepareContainerForItemOverride(element, item);
+            if (AutoExpandTopLevel) {
+                ((TreeViewItem)element).IsExpanded = true;
+            }
+        }
+
+        public bool AutoExpandTopLevel { get; set; }
+    }
+
+    [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(VTreeViewItem))]
+    public class VTreeViewItem : TreeViewItem {
+        protected override DependencyObject GetContainerForItemOverride() {
+            return new VTreeViewItem();
+        }
+
+        protected override void PrepareContainerForItemOverride(DependencyObject element, object item) {
+            base.PrepareContainerForItemOverride(element, item);
+        }
+    }
+
+    public class BLVirtualizingStackPanel : VirtualizingStackPanel {
+        public void BringIntoView(int index) {
+            if (index < this.VisualChildrenCount) {
+                this.BringIndexIntoView(index);
+            }
+        }
+    }
+
+
+}
---
+++ tags/build-150/BioLink.Client.Material/MaterialDatabaseAction.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Material {
+
+    public class RenameMaterialAction : GenericDatabaseAction<SiteExplorerNodeViewModel> {
+
+        public RenameMaterialAction(SiteExplorerNodeViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.RenameMaterial(Model.ElemID, Model.Name);
+        }
+
+    }
+
+    public class InsertMaterialAction : AbstractSiteExplorerAction {
+
+        public InsertMaterialAction(SiteExplorerNodeViewModel model, int templateID = 0)
+            : base(model) {
+            this.TemplateID = templateID;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.ElemID = service.InsertMaterial(Model.ParentID, TemplateID);
+            UpdateChildrenParentID();
+        }
+
+        public int TemplateID { get; private set; }
+    }
+
+    public class DeleteMaterialAction : DatabaseAction {
+
+        public DeleteMaterialAction(int materialId) {
+            this.MaterialID = materialId;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteMaterial(MaterialID);
+        }
+
+        public int MaterialID { get; private set; }
+    }
+
+    public class UpdateMaterialAction : GenericDatabaseAction<MaterialViewModel> {
+
+        public UpdateMaterialAction(MaterialViewModel model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.UpdateMaterial(Model.Model);
+        }
+    }
+
+    public class MergeMaterialAction : GenericDatabaseAction<SiteExplorerNode> {
+
+        public MergeMaterialAction(SiteExplorerNode source, SiteExplorerNode dest)
+            : base(source) {
+            Dest = dest;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.MergeMaterial(Model.ElemID, Dest.ElemID);
+        }
+
+        public SiteExplorerNode Dest { get; private set; }
+    }
+
+    public class MoveMaterialAction : GenericDatabaseAction<SiteExplorerNode> {
+        public MoveMaterialAction(SiteExplorerNode model, SiteExplorerNode dest)
+            : base(model) {
+            this.Destination = dest;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.MoveMaterial(Model.ElemID, Destination.ElemID);
+        }
+
+        public SiteExplorerNode Destination { get; private set; }
+    }
+
+    public class InsertMaterialTemplateAction : GenericDatabaseAction<SiteExplorerNode> {
+        public InsertMaterialTemplateAction(SiteExplorerNode model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.ElemID = service.InsertMaterialTemplate();
+        }
+    }
+
+}
