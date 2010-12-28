c2051aed82c47fed4ee231b746e5c4640ba47e7b
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-28 03:05:34 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-152/BioLinkDAL/DatabaseAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Data {
+
+    public abstract class DatabaseAction {
+
+        public void Process(User user) {
+            ProcessImpl(user);
+        }
+
+        protected abstract void ProcessImpl(User user);
+    }
+
+    public abstract class GenericDatabaseAction<T> : DatabaseAction {
+        
+        public GenericDatabaseAction(T model) {
+            Model = model;            
+        }
+
+        public T Model { get; private set; }
+
+        public override bool Equals(object obj) {
+            GenericDatabaseAction<T> other = obj as GenericDatabaseAction<T>;
+            if (other != null) {
+                return other.Model.Equals(this.Model);
+            }
+            return false;
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode() + Model.GetHashCode();
+        }
+
+    }
+
+}
