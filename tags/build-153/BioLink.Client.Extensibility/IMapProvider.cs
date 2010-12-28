6101d531bf48f7a21646e36a58fc8da0aae30d80
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2010-12-28 06:02:17 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-153/BioLink.Client.Extensibility/IMapProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace BioLink.Client.Extensibility {
+
+    public interface IMapProvider {
+
+        void Show();
+
+        void DropAnchor(double longitude, double latitude, string caption);
+        void HideAnchor();
+
+        void PlotPoints(MapPointSet points);
+        void ClearPoints();
+    }
+
+    public class MapPointSet : List<MapPoint> {
+
+        public MapPointSet(string name) : base() {
+            this.Name = name;
+            this.PointColor = Colors.Red;
+            this.DrawOutline = true;
+            this.OutlineColor = Colors.Black;
+            this.PointShape = MapPointShape.Circle;
+            this.Size = 7;
+        }
+
+        public String Name { get; set; }
+        public Color PointColor { get; set; }
+        public MapPointShape PointShape { get; set; }
+        public bool DrawOutline { get; set; }
+        public Color OutlineColor { get; set; }
+        public int Size { get; set; }
+        public bool DrawLabels { get; set; } 
+
+    }
+
+    public enum MapPointShape {
+        Circle,
+        Square,
+        Triangle
+    }
+
+    public class MapPoint {
+
+        public MapPoint() {
+        }
+
+        public MapPoint(double lat, double lon) {
+            this.Latitude = lat;
+            this.Longitude = lon;
+        }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Label { get; set; }
+
+        public int SiteID { get; set; }
+        public int SiteVisitID { get; set; }
+        public int MaterialID { get; set; }
+
+        // TODO - extra stuff in here for click events etc...
+
+    }
+}
