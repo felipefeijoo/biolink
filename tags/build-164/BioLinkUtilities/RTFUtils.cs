79fc7000970d5a75e04c54837f7281e43ee9e09a
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-01-06 06:40:49 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-164/BioLinkUtilities/RTFUtils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BioLink.Client.Utilities {
+    public static class RTFUtils {
+
+        public static string StripMarkup(string rtf) {
+            if (string.IsNullOrEmpty(rtf)) {
+                return rtf;
+            }
+
+            var rtb = new RichTextBox();
+            rtb.Rtf = rtf;
+            return rtb.Text;
+        }
+    }
+
+}
