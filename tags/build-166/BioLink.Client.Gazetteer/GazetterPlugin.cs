178941e7f7fb785fd888a3b1e4ce3d6b3941d424
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-01-07 06:03:11 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-166/BioLink.Client.Gazetteer/GazetterPlugin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+using System.Windows;
+
+namespace BioLink.Client.Gazetteer {
+
+    public class GazetterPlugin : BiolinkPluginBase {
+
+        private ExplorerWorkspaceContribution<Gazetteer> _gazetter;
+
+        public const string GAZETTEER_PLUGIN_NAME = "Gazetteer";
+
+        public GazetterPlugin() {
+        }
+
+        public override string Name {
+            get { return GAZETTEER_PLUGIN_NAME; }
+        }
+
+        public override List<IWorkspaceContribution> GetContributions() {
+            List<IWorkspaceContribution> contrib = new List<IWorkspaceContribution>();
+            contrib.Add(new MenuWorkspaceContribution(this, "ShowGazetteer", (obj, e) => { PluginManager.EnsureVisible(this, "Gazetteer"); },
+                String.Format("{{'Name':'View', 'Header':'{0}','InsertAfter':'File'}}", _R("Gazetteer.Menu.View")),
+                String.Format("{{'Name':'ShowGazetteer', 'Header':'{0}'}}", _R("Gazetteer.Menu.ShowExplorer"))
+            ));
+
+            _gazetter = new ExplorerWorkspaceContribution<Gazetteer>(this, "Gazetteer", new Gazetteer(this), _R("Gazetteer.Title"), (explorer) => {});
+
+            contrib.Add(_gazetter);
+
+            return contrib;            
+        }
+
+        public override bool RequestShutdown() {
+            return true;
+        }
+
+        public override void Dispose() {
+            if (_gazetter != null) {
+                (_gazetter.Content as Gazetteer).Dispose();
+            }
+        }
+
+        public override List<Command> GetCommandsForObject(ViewModelBase obj) {
+            return null;
+        }
+
+        public override ViewModelBase CreatePinnableViewModel(PinnableObject pinnable) {
+            var placeName = pinnable.GetState<PlaceName>();
+            if (placeName != null) {
+                return new PlaceNameViewModel(placeName);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ tags/build-166/BioLink.Client.Material/MaterialPartAction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+
+    public class InsertMaterialPartAction : GenericDatabaseAction<MaterialPart> {
+
+        public InsertMaterialPartAction(MaterialPart model, ViewModelBase owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.MaterialID = Owner.ObjectID.Value;
+            Model.MaterialPartID = service.InsertMaterialPart(Model);
+        }
+
+        protected ViewModelBase Owner { get; private set; }
+    }
+
+    public class UpdateMaterialPartAction : GenericDatabaseAction<MaterialPart> {
+
+        public UpdateMaterialPartAction(MaterialPart model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.UpdateMaterialPart(Model);
+        }
+    }
+
+    public class DeleteMaterialPartAction : GenericDatabaseAction<MaterialPart> {
+
+        public DeleteMaterialPartAction(MaterialPart model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteMaterialPart(Model.MaterialPartID);
+        }
+    }
+
+}
