0d6c859374c9967af3bab3e18fef4649f253bddb
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-01-13 01:52:36 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-171/BioLinkDAL/Model/CurationEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class CurationEvent : GUIDObject {
+
+        public int CurationEventID { get; set; }
+        public int MaterialID { get; set; }
+        public string SubpartName { get; set; }
+        public string Who { get; set; }
+        public DateTime? When { get; set; }
+        public string EventType { get; set; }
+        public string EventDesc { get; set; }
+        public int Changes { get; set; }
+
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return () => this.CurationEventID; }
+        }
+    }
+}
