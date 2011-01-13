3f4fe792067385d907ad1932d42fa7eaa2f97fbe
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-01-13 04:32:26 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-172/BioLink.Client.Maps/LayerViewModel.cs
@@ -0,0 +1,17 @@
+﻿using BioLink.Client.Extensibility;
+using SharpMap.Layers;
+
+namespace BioLink.Client.Maps {
+
+    public class LayerViewModel : GenericViewModelBase<ILayer> {
+
+        public LayerViewModel(ILayer layer) : base(layer, null) { }
+
+        public string LayerName {
+            get { return Model.LayerName; }
+            set { SetProperty(() => Model.LayerName, value); }
+        }
+
+    }
+
+}
---
+++ tags/build-172/BioLink.Client.Material/CurationEventViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+
+    public class CurationEventViewModel : GenericViewModelBase<CurationEvent> {
+
+        public CurationEventViewModel(CurationEvent model) : base(model, ()=>model.CurationEventID) { }
+
+        public override string DisplayLabel {
+            get {
+                return String.Format("{0}  {1}  {2}", EventType, SubpartName, Who);
+            }
+        }
+
+        public int CurationEventID {
+            get { return Model.CurationEventID; }
+            set { SetProperty(() => Model.CurationEventID, value); }
+        }
+
+        public int MaterialID {
+            get { return Model.MaterialID; }
+            set { SetProperty(() => Model.MaterialID, value); }
+        }
+
+        public string SubpartName {
+            get { return Model.SubpartName; }
+            set { SetProperty(() => Model.SubpartName, value); }
+        }
+
+        public string Who {
+            get { return Model.Who; }
+            set { SetProperty(() => Model.Who, value); }
+        }
+
+        public DateTime? When {
+            get { return Model.When; }
+            set { SetProperty(() => Model.When, value); }
+        }
+
+        public string EventType {
+            get { return Model.EventType; }
+            set { SetProperty(() => Model.EventType, value); }
+        }
+
+        public string EventDesc {
+            get { return Model.EventDesc; }
+            set { SetProperty(() => Model.EventDesc, value); }
+        }
+
+    }
+}
