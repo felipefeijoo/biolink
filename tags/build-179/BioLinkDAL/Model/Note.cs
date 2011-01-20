fbee564ab99c70fb30e808a70b57109b1572cfce
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-01-20 01:05:42 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-179/BioLinkDAL/Model/Note.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class Note : GUIDObject {
+
+        public string NoteCategory { get; set; }
+
+        public int IntraCatID { get; set; }
+
+        public int NoteID { get; set; }
+
+        public string NoteType { get; set; }
+
+        [MappingInfo("Note")]
+        public string NoteRTF { get; set; }
+
+        public string Author { get; set; }
+
+        public string Comments { get; set; }
+
+        public bool UseInReports { get; set; }
+
+        public int RefID { get; set; }
+
+        public string RefCode { get; set; }
+
+        public string RefPages { get; set; }
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return ()=>this.NoteID; }
+        }
+    }
+
+}
