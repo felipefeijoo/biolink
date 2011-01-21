6d902764f4d5181786e1cdd61b6d4eb9b3776cbc
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-01-21 07:20:07 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-182/BioLink.Client.Extensibility/Associates/AssociateDatabaseActions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Extensibility {
+
+    public class InsertAssociateAction : GenericDatabaseAction<Associate> {
+
+        public InsertAssociateAction(Associate model, ViewModelBase owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service =new SupportService(user);
+            Model.FromIntraCatID = Owner.ObjectID.Value;
+
+            switch (Model.ToCatID.GetValueOrDefault(-1)) {
+                case 1:
+                    Model.ToCategory = "Material";
+                    break;
+                case 2:
+                    Model.ToCategory = "Taxon";
+                    break;
+                default:
+                    Model.ToCategory = "";
+                    break;
+            }
+
+            Model.AssociateID = service.InsertAssociate(Model);
+        }
+
+        public override string ToString() {
+            return string.Format("Insert Associate: Name={0}", Model.AssocName);
+        }
+
+        protected ViewModelBase Owner { get; private set; }
+
+    }
+
+    public class UpdateAssociateAction : GenericDatabaseAction<Associate> {
+        public UpdateAssociateAction(Associate model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+
+            switch (Model.ToCatID.GetValueOrDefault(-1)) {
+                case 1:
+                    Model.ToCategory = "Material";
+                    break;
+                case 2:
+                    Model.ToCategory = "Taxon";
+                    break;
+                default:
+                    Model.ToCategory = "";
+                    break;
+            }
+
+            service.UpdateAssociate(Model);
+        }
+
+        public override string ToString() {
+            return string.Format("Update Associate: Name={0}", Model.AssocName);
+        }
+
+    }
+
+    public class DeleteAssociateAction : GenericDatabaseAction<Associate> {
+        public DeleteAssociateAction(Associate model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            service.DeleteAssociate(Model.AssociateID);
+        }
+    }
+}
---
+++ tags/build-182/BioLinkDAL/Model/FieldDescriptor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class FieldDescriptor {
+
+        public string DisplayName { get; set; }
+        public string FieldName { get; set; }
+        public string TableName { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Format { get; set; }
+        public bool UseInRDE { get; set; }
+        public string DataType { get; set; }
+
+        public override string ToString() {
+            return string.Format("{0} ({1}.{2})", DisplayName, TableName, FieldName);
+        }
+
+    }
+
+    public class QueryCriteria {
+
+        public FieldDescriptor Field { get; set; }
+        public string Criteria { get; set; }
+        public bool Output { get; set; }
+        public string Alias { get; set; }
+        public string Sort { get; set; }
+
+    }
+
+}
