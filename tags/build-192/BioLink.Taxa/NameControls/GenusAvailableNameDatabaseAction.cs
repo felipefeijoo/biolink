26031a703c115c6c65f14a5d7f1285558f055264
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-02-16 05:52:20 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-192/BioLink.Taxa/NameControls/GenusAvailableNameDatabaseAction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Taxa {
+
+    public class UpdateGenusAvailableNameAction : GenericDatabaseAction<GenusAvailableName> {
+
+        public UpdateGenusAvailableNameAction(GenusAvailableName model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new TaxaService(user);
+            service.InsertOrUpdateGenusAvailableName(Model);
+        }
+    }
+
+    public class DeleteGANIncludedSpeciesAction : GenericDatabaseAction<GANIncludedSpecies> {
+
+        public DeleteGANIncludedSpeciesAction(GANIncludedSpecies model) 
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new TaxaService(user);
+            service.DeleteGANIncludedSpecies(Model.GANISID);
+        }
+    }
+
+    public class InsertGANIncludedSpeciesAction : GenericDatabaseAction<GANIncludedSpecies> {
+
+        public InsertGANIncludedSpeciesAction(GANIncludedSpecies model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new TaxaService(user);
+            service.InsertGANIncludedSpecies(Model);
+        }
+
+    }
+
+    public class UpdateGANIncludedSpeciesAction : GenericDatabaseAction<GANIncludedSpecies> {
+
+        public UpdateGANIncludedSpeciesAction(GANIncludedSpecies model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new TaxaService(user);
+            service.UpdateGANIncludedSpecies(Model);
+        }
+    }
+}
