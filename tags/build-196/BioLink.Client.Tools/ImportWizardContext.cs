cf8d01f22ea16b83a6d8f1f7152b5e4c5dd328e0
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-02-20 23:16:43 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-196/BioLink.Client.Tools/ImportWizardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Tools {
+
+    public class ImportWizardContext {
+
+        public TabularDataImporter Importer { get; set; }
+
+        public ImporterOptions ImporterOptions { get; set; }
+
+        public List<ImportFieldMapping> FieldMappings { get; set; }
+
+    }
+}
