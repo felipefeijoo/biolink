654a060702169275d0f06c0336a470b4fdb47d0a
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-03-22 06:22:39 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-219/BioLinkDAL/Model/AvailableName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+    public class AvailableName : GUIDObject {
+
+        public int BiotaID { get; set; }
+        public int? RefID { get; set; }
+        public string RefPage { get; set; }
+        public string RefQual { get; set; }
+        public string RefCode { get; set; }
+        public string AvailableNameStatus { get; set; }
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return null; }
+        }
+    }
+}
