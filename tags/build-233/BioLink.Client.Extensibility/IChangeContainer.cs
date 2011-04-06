5e68450c636b1bd6111a5d265523256eb7fccdcb
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-04-06 06:20:00 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-233/BioLink.Client.Extensibility/IChangeContainer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+
+namespace BioLink.Client.Extensibility {
+
+    public interface IChangeContainer {
+
+        void RegisterPendingChange(DatabaseAction change, object contributer);
+
+        bool RegisterUniquePendingChange(DatabaseAction change, object contributer);
+
+        void RegisterPendingChanges(List<DatabaseAction> actions, object contributer);
+
+        void CommitPendingChanges(Action successAction = null);
+
+        void ClearPendingChanges();
+
+        void ClearMatchingPendingChanges(Predicate<DatabaseAction> predicate);
+
+        bool HasPendingChanges { get; }
+
+        ObservableCollection<DatabaseAction> PendingChanges { get; }
+    }
+
+    public interface IChangeContainerObserver {
+
+        void OnChangesCommitted();
+
+    }
+}
