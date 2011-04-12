aef930ebd49813d6fa0e6e707bcd3abf43af4121
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-04-12 05:36:02 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-241/BioLink.Client.Material/SiteVisitDatabaseAction.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Extensibility;
+
+namespace BioLink.Client.Material {
+
+    public class RenameSiteVisitAction : GenericDatabaseAction<SiteExplorerNode> {
+
+        public RenameSiteVisitAction(SiteExplorerNode model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.RenameSiteVisit(Model.ElemID, Model.Name);
+        }
+
+    }
+
+    public class DeleteSiteVisitAction : DatabaseAction {
+
+        public DeleteSiteVisitAction(int siteVisitID) {
+            this.SiteVisitID = siteVisitID;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteSiteVisit(SiteVisitID);
+        }
+
+        public int SiteVisitID { get; private set; }
+    }
+
+    public class InsertSiteVisitAction : AbstractSiteExplorerAction {
+
+        public InsertSiteVisitAction(SiteExplorerNode model, SiteExplorerNodeViewModel viewModel, int templateId = -1) : base(model, viewModel) {
+            TemplateID = templateId;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.ElemID = service.InsertSiteVisit(Model.ParentID, TemplateID);
+            UpdateChildrenParentID();
+        }
+
+        public int TemplateID { get; private set; }
+    }
+
+    public class UpdateSiteVisitAction : GenericDatabaseAction<SiteVisit> {
+        public UpdateSiteVisitAction(SiteVisit model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            if (Model.DateStart.GetValueOrDefault(-1) < 0 && Model.DateEnd.GetValueOrDefault(-1) < 0) {
+                Model.DateType = 2;
+            } else {
+                Model.DateType = 1;
+            }
+            service.UpdateSiteVisit(Model);
+        }
+
+    }
+
+    public class MergeSiteVisitAction : GenericDatabaseAction<SiteExplorerNode> {
+
+        public MergeSiteVisitAction(SiteExplorerNode source, SiteExplorerNode dest)
+            : base(source) {
+            Dest = dest;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.MergeSiteVisit(Model.ElemID, Dest.ElemID);
+        }
+
+        public SiteExplorerNode Dest { get; private set; }
+    }
+
+    public class MoveSiteVisitAction : GenericDatabaseAction<SiteExplorerNode> {
+
+        public MoveSiteVisitAction(SiteExplorerNode source, SiteExplorerNode dest)
+            : base(source) {
+            Dest = dest;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);            
+            service.MoveSiteVisit(Model.ElemID, Dest.ElemID);
+        }
+
+        public SiteExplorerNode Dest { get; private set; }
+    }
+
+    public class InsertSiteVisitTemplateAction : GenericDatabaseAction<SiteExplorerNode> {
+        public InsertSiteVisitTemplateAction(SiteExplorerNode model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.ElemID = service.InsertSiteVisitTemplate();
+        }
+    }
+
+    public class InsertRDESiteVisitAction : GenericDatabaseAction<RDESiteVisit> {
+
+        public InsertRDESiteVisitAction(RDESiteVisit model, RDESite owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.SiteID = Owner.SiteID;
+            Model.SiteVisitID = service.InsertSiteVisit(Model.SiteID);
+        }
+
+        protected RDESite Owner { get; private set; }
+
+    }
+
+    public class UpdateRDESiteVisitAction : GenericDatabaseAction<RDESiteVisit> {
+
+        public UpdateRDESiteVisitAction(RDESiteVisit model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.UpdateSiteVisit(MapToSiteVisit(Model));
+        }
+
+        private static SiteVisit MapToSiteVisit(RDESiteVisit model) {
+            var visit = new SiteVisit();
+
+            if (string.IsNullOrEmpty(model.VisitName)) {
+                int? date = model.DateStart;
+                if (!date.HasValue || date.Value == 0) {
+                    date = model.DateEnd;
+                }
+
+                if (date.HasValue && date.Value != 0) {
+                    model.VisitName = string.Format("{0} {1}", model.Collector, DateControl.DateToStr(date.ToString()));
+                } else {
+                    model.VisitName = model.Collector;
+                }
+            }
+
+            visit.SiteVisitName = model.VisitName;
+            visit.SiteVisitID = model.SiteVisitID;
+            visit.SiteID = model.SiteID;
+
+            visit.Collector = model.Collector;
+            visit.DateStart = model.DateStart;
+            visit.DateEnd = model.DateEnd;
+
+            return visit;
+        }
+    }
+
+}
---
+++ tags/build-241/BioLink.Client.Tools/EnvironmentalLayer.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using System.IO;
+
+namespace BioLink.Client.Tools {
+
+    public interface IEnvironmentalLayer {
+        double GetValueAt(double latitude, double longitude, double @default=0.0);
+        EnvironmentalLayerRange GetRange(double percentile);
+        EnvironmentalLayerRange GetRangeForPoints(IEnumerable<MapPoint> points, double percentile);
+        string Name { get; }
+    }
+
+    public abstract class GridLayer : IEnvironmentalLayer {
+
+        private double[,] _data = null;
+
+        protected GridLayer(string filename) {
+            _data = LoadImpl(filename);
+            this.Name = filename;
+        }
+
+        protected abstract double[,] LoadImpl(string filename);
+
+        public double GetValueAt(double latitude, double longitude, double novalue = 0.0) {
+	        int lngX, lngY;
+	        double fudge = (double) ( DeltaLatitude / 2.0 );
+
+	        lngX = (int) ((longitude - ( Longitude0 - fudge) ) / DeltaLongitude);
+	        lngY = (int) ((latitude - ( Latitude0) ) / DeltaLatitude);
+
+            if ((lngX >= Width) || (lngY >= Height)) {
+                return novalue;
+            }
+
+	        if ((lngX < 0) || (lngY < 0)) return novalue;
+
+            double val = _data[lngY, lngX];
+	        return (val  == NoValueMarker ? novalue : val);
+        }
+
+        public EnvironmentalLayerRange GetRange(double percentile) {
+            double? min = null;
+            double? max = null;
+            
+            TraverseCells((x, y, value) => {
+                if (value != NoValueMarker) {
+                    if (!min.HasValue) {
+                        min = value;
+                        max = value;
+                    } else {
+                        if (value < min) {
+                            min = value;
+                        } else if (value > max) {
+                            max = value;
+                        }
+                    }
+                }
+            });
+            // Now work out the percentile....
+            if (max.HasValue && min.HasValue) {
+                var val = (max.Value - min.Value) * percentile;
+
+                var range = new EnvironmentalLayerRange { Max = max.Value - val, Min = min.Value - val, Percentile = percentile };
+                return range;
+            }
+
+            return null;
+        }
+
+        public EnvironmentalLayerRange GetRangeForPoints(IEnumerable<MapPoint> points, double percentile) {
+
+            var values = new PointValueList();
+
+            foreach (MapPoint p in points) {
+                var value = GetValueAt(p.Latitude, p.Longitude, NoValueMarker);
+                if (value == NoValueMarker) {
+                } else {                    
+                    values.AddValue(value);
+                }
+            }
+
+            return new EnvironmentalLayerRange { Max = values.GetUpper(percentile), Min = values.GetLower(percentile), Percentile = percentile };
+        }
+
+        public void SetAllCells(double value) {
+            TraverseCells((x, y, current) => {
+                _data[x, y] = value;
+            });
+        }
+
+        protected void TraverseCells(Action<int, int, double> action) {
+            for (int row = 0; row < Height; row++) {
+                for (int col = 0; col < Width; col++) {
+                    action(row, col, _data[row, col]);                    
+                }
+            }
+        }
+
+
+        #region Properties
+
+        public string Name { get; protected set; }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        protected double Latitude0 { get; set; }
+        protected double Longitude0 { get; set; }
+        protected double DeltaLatitude { get; set; }
+        protected double DeltaLongitude { get; set; }
+        protected double NoValueMarker { get; set; }
+        protected Int32 Flags { get; set; }
+
+        #endregion
+
+    }
+
+    public class GRDGridLayer : GridLayer {
+
+        private static Int32 GRD_MAGIC = 0xCAFA;
+
+        public GRDGridLayer(string filename) : base(filename) { }
+
+        protected override double[,] LoadImpl(string filename) {
+
+            double[,] data = null;
+
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read)) {
+                // Buffer the file in a memory stream for performance....
+                byte[] buffer = new byte[fs.Length];
+                fs.Read(buffer, 0, (int) fs.Length);
+                using (var stream = new MemoryStream(buffer)) {
+                    using (BinaryReader reader = new BinaryReader(stream)) {
+                        Int32 magic = reader.ReadInt32();
+                        if (magic != GRD_MAGIC) {
+                            throw new Exception("Bad magic number in GRD file!");
+                        }
+                        Int32 headsize = reader.ReadInt32();
+                        Width = reader.ReadInt32();
+                        Height = reader.ReadInt32();
+                        Longitude0 = reader.ReadDouble();
+                        Latitude0 = reader.ReadDouble();
+                        DeltaLongitude = reader.ReadDouble();
+                        DeltaLatitude = reader.ReadDouble();
+                        NoValueMarker = reader.ReadSingle();
+                        Flags = reader.ReadInt32();
+
+                        // Allocate an array for the data...
+                        data = new double[Width, Height];
+                        for (int row = 0; row < Height; row++) {
+                            for (int col = 0; col < Width; col++) {
+                                data[col, row] = reader.ReadSingle();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+
+    class PointValueList {
+        
+        private LLNode _first;
+
+        public PointValueList() {
+            Count = 0;
+            _first = null;
+        }
+
+        public bool AddValue(double value) {
+
+            var newNode = new LLNode(value);
+            if (_first == null) {
+                _first = newNode;
+                Count = 1;
+                return true;
+            }
+
+            if (value < _first.Value) {
+                newNode.Next = _first;
+                _first = newNode;
+                Count++;
+                return true;
+            }
+
+            var temp = _first;
+            var prev = temp;
+            while (temp != null && value > temp.Value) {
+                prev = temp;
+                temp = temp.Next;
+            }
+            newNode.Next = prev.Next;
+            prev.Next = newNode;
+
+            Count++;
+
+            return true;
+        }
+
+        public double GetLower(double percentile) {
+            int index = (int) ((double) (Count + 1) * percentile);
+            var p = _first;
+            int i = 0;
+
+            while (p != null && (i++ < index - 1)) {
+                p = p.Next;
+            }
+
+            if (p != null) {
+                return p.Value;
+            }
+
+            return 0;            
+        }
+
+        public double GetUpper(double percentile) {
+            int index = (int) (((double) (Count + 1) * (1-percentile)) - 1);
+            var p = _first;
+            int i = 0;
+            double last = 0;
+
+            while (p != null && (i++ < index)) {
+                last = p.Value;
+                p = p.Next;
+            }
+
+            if (p != null) {
+                return p.Value;
+            } 
+
+            return last;
+        }
+
+        public int Count { get; private set; }
+
+    }
+
+    class LLNode {
+
+        public LLNode() {
+            Next = null;
+            Value = 0;
+        }
+
+        public LLNode(double value) {
+            Next = null;
+            Value = value;
+        }
+
+        public LLNode Next { get; set; }
+        public double Value { get; set; }
+    }
+
+    public class EnvironmentalLayerRange {
+        public double Max { get; set; }
+        public double Min { get; set; }        
+        public double Percentile { get; set; }
+
+        public double Range {
+            get { return Max - Min; }
+        }
+    }
+}
