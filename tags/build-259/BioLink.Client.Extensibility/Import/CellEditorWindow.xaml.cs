ab5cb267d1651e9acdfc561dfcc88f216ab27623
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-05-03 04:38:06 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-259/BioLink.Client.Extensibility/Import/CellEditorWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using BioLink.Data;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for CellEditorWindow.xaml
+    /// </summary>
+    public partial class CellEditorWindow : Window, IDataErrorInfo {
+
+        public CellEditorWindow(ImportFieldMapping mapping, string currentValue) {
+            InitializeComponent();
+            this.Mapping = mapping;
+            this.CurrentValue = currentValue;
+            this.NewValue = currentValue;
+
+            this.DataContext = this;
+
+            Loaded += new RoutedEventHandler(CellEditorWindow_Loaded);
+            
+        }
+
+        void CellEditorWindow_Loaded(object sender, RoutedEventArgs e) {
+            txtNewValue.Focus();
+        }
+
+        public String CurrentValue { get; private set; }
+        public String NewValue { get; set; }
+        public ImportFieldMapping Mapping { get; private set; }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        public string Error {
+            get { return null; }
+        }
+
+        public string this[string columnName] {
+            get {
+                if (columnName == "NewValue") {
+                    var service = new ImportService(PluginManager.Instance.User);
+                    var result = service.ValidateImportValue(Mapping.TargetColumn, NewValue);
+                    if (!result.IsValid) {
+                        return result.Message;
+                    }
+                }
+
+                return null;
+            }
+        }
+    }
+    
+}
