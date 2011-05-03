f1f3bba81ace4e29e1f37676e66033215dd93058
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-05-03 08:35:37 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-260/BioLinkUtilities/IProgressMonitor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Client.Utilities {
+
+    public delegate bool ProgressHandler(string message, double percentComplete, ProgressEventType progressEventType);
+
+    public interface IProgressObserver {
+        void ProgressStart(string message, bool indeterminate=false);
+        void ProgressMessage(string message, double? percentComplete = null);        
+        void ProgressEnd(string message);
+    }
+
+    public enum ProgressEventType {
+        Start,
+        Update,
+        End
+    }
+
+    public class ProgressObserverAdapter {
+
+        public IProgressObserver _observer;
+
+        public ProgressObserverAdapter(IProgressObserver observer) {
+            _observer = observer;
+        }
+
+        public bool OnProgress(string message, double percentComplete, ProgressEventType progressEventType) {
+            if (_observer != null) {
+                switch (progressEventType) {
+                    case ProgressEventType.Start:
+                        _observer.ProgressStart(message);
+                        break;
+                    case ProgressEventType.Update:
+                        _observer.ProgressMessage(message, percentComplete);
+                        break;
+                    case ProgressEventType.End:
+                        _observer.ProgressEnd(message);
+                        break;
+                }
+            }
+            return true;
+        }
+
+        public static ProgressHandler Adapt(IProgressObserver observer) {
+            return new ProgressObserverAdapter(observer).OnProgress;
+        }
+
+    }
+    
+}
