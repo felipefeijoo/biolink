8de12a7e3ad524ca054ca9afe1e349db2ac3e3b5
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-05-18 07:37:44 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-274/BioLink.Client.Extensibility/FavoriteViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using BioLink.Client.Utilities;
+using System.Reflection;
+
+namespace BioLink.Client.Extensibility {
+
+    public class GenericHierarchicalViewModelBase<T> : HierarchicalViewModelBase {
+
+        private Expression<Func<int>> _objectIDExpr = null;
+
+        public GenericHierarchicalViewModelBase(T model, Expression<Func<int>> objectIDExpr) {
+            this.Model = model;
+            _objectIDExpr = objectIDExpr;
+        }
+
+        protected void SetProperty<K>(Expression<Func<K>> wrappedPropertyExpr, K value, Action doIfChanged = null, bool changeAgnostic = false) {
+            SetProperty(wrappedPropertyExpr, Model, value, doIfChanged, changeAgnostic);
+        }
+
+        public override int NumChildren { get; set; }
+
+        public T Model { get; private set; }
+
+        public override int? ObjectID {
+            get {
+                if (_objectIDExpr == null) {
+                    return null;
+                } else {
+                    var destProp = (PropertyInfo)((MemberExpression)_objectIDExpr.Body).Member;
+                    return (int)destProp.GetValue(Model, null);
+                }
+            }
+        }
+
+    
+    }
+
+    public class FavoriteViewModel<T> : GenericHierarchicalViewModelBase<T> where T : Favorite {
+
+        private ImageSource _image;
+
+        public FavoriteViewModel(T model) : base(model, ()=>model.FavoriteID) { }
+
+        public override ImageSource Icon {
+            get {
+                if (_image == null) {
+                    if (IsGroup) {
+                        _image = ImageCache.GetImage("pack://application:,,,/BioLink.Client.Extensibility;component/images/FavFolder.png");
+                    } else {
+                        _image = base.Icon;
+                    }
+                }
+                return _image;
+            }
+
+            set {
+                _image = value;
+                RaisePropertyChanged("Icon");
+            }
+        }
+
+
+        public string Username {
+            get { return Model.Username; }
+            set { SetProperty(() => Model.Username, value); }
+        }
+
+        public int FavoriteID {
+            get { return Model.FavoriteID; }
+            set { SetProperty(() => Model.FavoriteID, value); }
+        }
+
+        public int FavoriteParentID {
+            get { return Model.FavoriteParentID; }
+            set { SetProperty(() => Model.FavoriteParentID, value); }
+        }
+
+        public bool IsGroup {
+            get { return Model.IsGroup; }
+            set { SetProperty(() => Model.IsGroup, value); }
+        }
+
+        public string GroupName {
+            get { return Model.GroupName; }
+            set { SetProperty(() => Model.GroupName, value); }
+        }
+
+        public override int NumChildren {
+            get { return Model.NumChildren; }
+            set { SetProperty(() => Model.NumChildren, value); }
+        }
+
+        public bool IsGlobal {
+            get { return Model.IsGlobal; }
+            set { SetProperty(() => Model.IsGlobal, value); }
+        }
+
+    }
+
+}
---
+++ tags/build-274/BioLinkDAL/Model/XMLImportEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace BioLink.Data.Model {
+    public class XMLImportEvent : XMLImportObject {
+        public XMLImportEvent(XmlElement node) : base(node) { }
+    }
+}
