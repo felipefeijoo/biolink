a99096a42b4dbb896a336a31a11ff7ab7755d25b
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-06-09 12:39:20 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-280/BioLink.Client.Extensibility/PickListWindow.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+using System.Collections.Generic;
+
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for PickListWindow.xaml
+    /// </summary>
+    public partial class PickListWindow : Window {
+
+        #region DesignerConstructor
+        public PickListWindow() {
+            InitializeComponent();
+        }
+        #endregion
+
+        private Func<IEnumerable<object>> _itemsFunc;
+        private Func<String, bool> _addItemFunc;
+
+        private ObservableCollection<Object> _model;
+
+        public PickListWindow(User user, string caption, Func<IEnumerable<object>> itemsFunc, Func<String, bool> addItemFunc) {
+            _itemsFunc = itemsFunc;
+            _addItemFunc = addItemFunc;
+            this.User = user;
+            InitializeComponent();
+            Config.RestoreWindowPosition(user, this);
+            Title = caption;
+            LoadModel();
+
+            btnAddNew.Visibility = System.Windows.Visibility.Hidden;
+
+            if (_addItemFunc != null) {
+                btnAddNew.Visibility = Visibility.Visible;
+                btnAddNew.Click += new RoutedEventHandler((source, e) => {
+                    string prefill = txtFilter.Text;
+                    InputBox.Show(this, "Add a new value", "Enter the new value, and click OK", prefill, (text) => {
+                        if (_addItemFunc(text)) {
+                            _model.Add(text);
+                            lst.SelectedItem = text;
+                            this.DialogResult = true;
+                            this.Hide();
+                        }
+                    });
+                });
+            }
+        }
+
+        public void LoadModel() {
+            var list = _itemsFunc();
+                        
+            _model = new ObservableCollection<Object>();
+            foreach (object item in list) {
+                if (item != null) {
+                    if (item is string) {
+                        if (!String.IsNullOrWhiteSpace(item as string)) {
+                            _model.Add((item as string).Trim());
+                        }
+                    } else {
+                        _model.Add(item);
+                    }
+                }
+
+            }
+            lst.ItemsSource = _model;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+            this.Hide();
+        }
+
+        private void txtFilter_TypingPaused(string text) {
+            FilterList(text);
+        }
+
+        private void FilterList(string text) {
+            ListCollectionView dataView = CollectionViewSource.GetDefaultView(lst.ItemsSource) as ListCollectionView;
+
+            if (String.IsNullOrEmpty(text)) {
+                dataView.Filter = null;
+                dataView.Refresh();
+                return;
+            }
+
+            String searchTerm = text.ToLower();
+            
+            dataView.Filter = (obj) => {
+                string test = null;
+                if (obj is string) {
+                    test = obj as string;
+                } else if (obj is ViewModelBase) {
+                    test = (obj as ViewModelBase).DisplayLabel;
+                } else if (obj != null) {
+                    test = obj.ToString();
+                }
+                if (test != null) {
+                    return test.ToLower().Contains(searchTerm);
+                }
+                return false;
+            };
+
+            dataView.Refresh();
+        }
+
+        public object SelectedValue {
+            get { return lst.SelectedItem; }
+        }
+
+        private void btnSelect_Click(object sender, RoutedEventArgs e) {
+            if (lst.SelectedItem != null) {
+                this.DialogResult = true;
+                this.Hide();
+            }
+        }
+
+        private void txtFilter_KeyUp(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Down) {
+                lst.SelectedIndex = 0;
+                if (lst.SelectedItem != null) {
+                    ListBoxItem item = lst.ItemContainerGenerator.ContainerFromItem(lst.SelectedItem) as ListBoxItem;
+                    item.Focus();
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {            
+            lst.Focus();
+        }
+
+
+        private void Window_Deactivated(object sender, EventArgs e) {
+            Config.SaveWindowPosition(User, this);
+        }
+
+        private void lst_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            if (lst.SelectedItem != null) {
+                this.DialogResult = true;
+                this.Hide();
+            }
+        }
+
+        protected User User { get; private set; }
+
+    }
+}
---
+++ tags/build-280/BioLink.Client.Extensibility/ReportResults.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using System;
+using System.Windows.Input;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for ReportResults.xaml
+    /// </summary>
+    public partial class ReportResults : UserControl, IProgressObserver, IDisposable {
+
+        private bool _ReportExecuted = false;
+
+        public ReportResults() {
+            InitializeComponent();
+        }
+
+        public ReportResults(IBioLinkReport report) {
+            
+            InitializeComponent();
+            progressBar.Visibility = System.Windows.Visibility.Hidden;
+
+            this.Report = report;
+            this.Loaded += new RoutedEventHandler(ReportResults_Loaded);
+            
+        }
+
+        void ReportResults_Loaded(object sender, RoutedEventArgs e) {
+            if (!_ReportExecuted) {
+                _ReportExecuted = true;
+                JobExecutor.QueueJob(() => {
+                    try {
+                        this.WaitCursor();
+                        StatusMessage("Running report...");
+                        this.InvokeIfRequired(() => { DisplayLoading(); });
+                        DataMatrix data = Report.ExtractReportData(this);
+                        this.InvokeIfRequired(() => { DisplayReportResults(data); });                        
+                    } catch (Exception ex) {
+                        this.NormalCursor();
+                        this.ProgressEnd("An error occured executing the report");
+                        this.InvokeIfRequired(() => { DisplayException(ex); });
+                    } finally {                        
+                        this.NormalCursor();
+                    }
+                });
+            }
+        }
+
+        private string _R(string key, params object[] args) {
+            string format = FindResource(key) as string;
+            if (format != null) {                
+                return String.Format(format, args);
+            }
+            return string.Format(key, args);
+        }
+
+        private void DisplayLoading() {
+            string assemblyName = this.GetType().Assembly.GetName().Name;
+            string imageFile = PluginManager.Instance.ResourceTempFileManager.ProxyResource(new Uri(String.Format("pack://application:,,,/{0};component/images/loading-big.gif", assemblyName)));
+            string html = _R("ReportResults.Loading.Template", Report.Name, imageFile);
+            DisplayHTML(html);
+        }
+
+        private void DisplayHTML(String html) {
+            reportContent.Children.Clear();
+            WebBrowser browser = new WebBrowser();
+            browser.Navigating += new System.Windows.Navigation.NavigatingCancelEventHandler(browser_Navigating);                        
+            browser.NavigateToString(html);
+            reportContent.Children.Add(browser);
+        }
+
+        private void DisplayException(Exception ex) {
+            
+            string title = _R("ReportResults.Error.Title", Report.Name);
+            string stacktrace = ex.StackTrace;
+            string html = _R("ReportResults.Error.Template", title, ex.Message, stacktrace);
+            DisplayHTML(html);
+        }
+
+        void browser_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e) {
+            Logger.Debug("Navigating to {0}", e.Uri == null ? null : e.Uri.ToString());
+        }
+
+        internal void DisplayReportResults(DataMatrix data) {
+
+            StatusMessage("Preparing view...");
+
+            if (this.Report != null) {
+                reportContent.Children.Clear();
+                if (Report.Viewers.Count == 0 || Report.Viewers.Count == 1) {
+                    IReportViewerSource viewerSource = null;
+                    if (Report.Viewers.Count == 0) {
+                        viewerSource = new TabularDataViewerSource();
+                    } else {
+                        viewerSource = Report.Viewers[0];
+                    }
+                    FrameworkElement control = viewerSource.ConstructView(Report, data, this);
+                    reportContent.Children.Add(control);                    
+                } else {
+                    TabControl tab = new TabControl();
+                    foreach (IReportViewerSource viewerSource in Report.Viewers) {
+                        FrameworkElement control = viewerSource.ConstructView(Report, data, this);
+                        tab.Items.Add(control);
+                    }                    
+                    reportContent.Children.Add(tab);    
+                }
+            }
+
+            StatusMessage("{0} records retrieved.", data.Rows.Count);
+        }
+
+        internal void StatusMessage(string format, params object[] args) {
+            string message = String.Format(format, args);
+            statusMessage.InvokeIfRequired(() => {
+                statusMessage.Text = message;
+            });
+        }
+
+        #region ProgressObserver
+
+        public void ProgressStart(string message, bool indeterminate) {
+            StatusMessage(message);
+            progressBar.InvokeIfRequired(() => {
+                progressBar.Value = 0;
+                progressBar.Visibility = System.Windows.Visibility.Visible;
+                progressBar.IsIndeterminate = indeterminate;
+            });
+        }
+
+        public void ProgressMessage(string message, double? percentComplete) {
+            StatusMessage(message);
+            if (percentComplete.HasValue) {
+                progressBar.InvokeIfRequired(() => {
+                    progressBar.Value = percentComplete.Value;
+                });
+            }
+        }
+
+        public void ProgressEnd(string message) {
+            StatusMessage(message);
+            progressBar.InvokeIfRequired(() => {
+                progressBar.Value = 100;
+                progressBar.Visibility = System.Windows.Visibility.Hidden;
+            });
+        }
+
+        #endregion
+
+        #region properties
+
+        protected IBioLinkReport Report { get; private set; }
+
+        #endregion
+
+        public void Dispose() {
+            foreach (FrameworkElement elem in reportContent.Children) {
+                if (elem is IDisposable) {
+                    IDisposable disposable = elem as IDisposable;
+                    disposable.Dispose();
+                }
+            }
+        }
+
+
+        //private void mnuFileExit_Click(object sender, RoutedEventArgs e) {
+        //    PluginManager.Instance.CloseContent(this);
+        //}
+
+    }
+}
