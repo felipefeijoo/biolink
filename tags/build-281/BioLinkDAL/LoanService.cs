180384ac677cc741103e3d58152028ad90047ef3
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-06-10 07:57:23 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-281/BioLinkDAL/LoanService.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using BioLink.Data.Model;
+
+namespace BioLink.Data {
+
+    public class LoanService : BioLinkService  {
+
+        public LoanService(User user) : base(user) { }
+
+        public List<Contact> FindContacts(string filter, ContactSearchType searchType) {
+            var what = "";
+            switch (searchType) {
+                case ContactSearchType.Institution:
+                    what = "i";
+                    break;
+                case ContactSearchType.Surname:
+                    what = "s";
+                    break;
+                default:
+                    break;
+            }
+
+            var where = string.Format(" like '{0}%'", filter);
+            var mapper = GetContactMapper();
+            return StoredProcToList("spContactList", mapper, _P("vchrSearchWhere", what), _P("vchrWhereClause", where));
+        }
+
+        public List<Contact> ListContactsRange(string from, string to) {            
+            var strWhere = "left(vchrName," + from.Length + ") between '" + from + "' and '" + to + "'";
+            var mapper = new GenericMapperBuilder<Contact>().build();
+            return StoredProcToList("spContactListRange", mapper, _P("vchrWhere", strWhere));
+        }
+
+        public Contact GetContact(int contactId) {
+            var mapper = GetContactMapper();
+            return StoredProcGetOne("spContactGet", mapper, _P("intContactID", contactId));
+        }
+
+        public void UpdateContact(Contact contact) {
+            StoredProcUpdate("spContactUpdate",
+                _P("intContactID", contact.ContactID),
+                _P("vchrName", contact.Name),
+                _P("vchrTitle", contact.Title),
+                _P("vchrGivenName", contact.GivenName),
+                _P("vchrPostalAddress", contact.StreetAddress),
+                _P("vchrStreetAddress", contact.StreetAddress),
+                _P("vchrInstitution", contact.Institution),
+                _P("vchrJobTitle", contact.JobTitle),
+                _P("vchrWorkPh", contact.WorkPh),
+                _P("vchrWorkFax", contact.WorkFax),
+                _P("vchrHomePh", contact.HomePh),
+                _P("vchrEMail", contact.EMail));
+        }
+
+        public void DeleteContact(int contactID) {
+            StoredProcUpdate("spContactDelete", _P("intContactID", contactID));
+        }
+
+        protected GenericMapper<Contact> GetContactMapper() {
+            return new GenericMapperBuilder<Contact>().build();
+        }
+
+
+        public int InsertContact(Contact contact) {
+            var retval = ReturnParam("NewContactID");
+            StoredProcUpdate("spContactInsert",
+                _P("vchrName", contact.Name),
+                _P("vchrTitle", contact.Title),
+                _P("vchrGivenName", contact.GivenName),
+                _P("vchrPostalAddress", contact.StreetAddress),
+                _P("vchrStreetAddress", contact.StreetAddress),
+                _P("vchrInstitution", contact.Institution),
+                _P("vchrJobTitle", contact.JobTitle),
+                _P("vchrWorkPh", contact.WorkPh),
+                _P("vchrWorkFax", contact.WorkFax),
+                _P("vchrHomePh", contact.HomePh),
+                _P("vchrEMail", contact.EMail),
+                retval
+                );
+
+            int newContactID = (int)retval.Value;
+            return newContactID;
+        }
+
+        public List<Loan> ListLoansForContact(int contactID) {
+            var mapper = new GenericMapperBuilder<Loan>().build();
+            return StoredProcToList("spLoanListForContact", mapper, _P("intContactID", contactID));
+        }
+
+        public static string FormatName(string title, string givenName, string surname) {
+            var sb = new StringBuilder();
+            if (!string.IsNullOrWhiteSpace(title)) {
+                sb.Append(title).Append(" ");
+            }
+
+            if (!string.IsNullOrWhiteSpace(givenName)) {
+                sb.Append(givenName).Append(" ");
+            }
+
+            sb.Append(surname);
+
+            return sb.ToString();
+        }
+    }
+
+    public enum ContactSearchType {
+        All, Surname, Institution
+    }
+    
+}
---
+++ tags/build-281/BioLinkDAL/XMLExportObject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Data {
+
+    internal class XMLExportObject {
+
+        private XmlDocument _xmlDoc;
+
+        public XMLExportObject() {            
+            InitXML();
+        }
+
+        private void InitXML() {
+            _xmlDoc = new XmlDocument();
+            var XMLParent = CreateNode(_xmlDoc, "XML");
+            MetaRoot = CreateNode(XMLParent, "META");
+            var XMLDataNode = CreateNode(XMLParent, "DATA");
+            TaxaRoot = CreateNode(XMLDataNode, "TAXA");
+            MaterialRoot = CreateNode(XMLDataNode, "MATERIAL");
+            ReferenceRoot = CreateNode(XMLDataNode, "REFERENCES");
+            MultimediaRoot = CreateNode(XMLDataNode, "MULTIMEDIALIST");
+            AssociateRoot = CreateNode(XMLDataNode, "ASSOCIATES");
+            JournalRoot = CreateNode(XMLDataNode, "JOURNALS");
+            UnplacedTaxaRoot = CreateNode(TaxaRoot, "UNPLACEDTAXA");
+        }
+
+        public XmlDocument XMLDocument { 
+            get { return _xmlDoc; } 
+        }
+
+        public XmlElement CreateNode(XmlNode parent, string tag, string id = null) {
+            var element = _xmlDoc.CreateElement(tag);
+            parent.AppendChild(element);
+            if (id != null) {
+                element.AddAttribute("ID", id);
+            }
+            return element;
+        }
+
+        public XmlElement GetElementByGUID(string guid, string itemType) {
+            return _xmlDoc.SelectSingleNode(string.Format("//{0}[@ID='{1}']", itemType, guid)) as XmlElement;
+        }
+
+        public XmlElement MetaRoot { get; private set; }
+        public XmlElement TaxaRoot { get; private set; }
+        public XmlElement MaterialRoot { get; private set; }
+        public XmlElement ReferenceRoot { get; private set; }
+        public XmlElement MultimediaRoot { get; private set; }
+        public XmlElement AssociateRoot { get; private set; }
+        public XmlElement JournalRoot { get; private set; }
+        public XmlElement UnplacedTaxaRoot { get; private set; }
+
+
+        internal void Save(string filename) {
+            _xmlDoc.Save(filename);
+        }
+    }
+
+    
+
+}
