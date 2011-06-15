d26a911faf13a42ecbc79aa84d86f700b0f22801
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-06-15 07:07:48 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-282/BioLinkDAL/Model/RDESiteVisit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    [Serializable()]
+    public class RDESiteVisit : RDEObject {
+
+        public RDESiteVisit() {
+            SiteVisitID = -1;
+            SiteID = -1;
+        }
+
+        public int SiteVisitID { get; set; }
+        public int SiteID { get; set; }
+        public string VisitName { get; set; }
+        public string Collector { get; set; }
+        public int? DateStart { get; set; }
+        public int? DateEnd { get; set; }
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return () => this.SiteVisitID; }
+        }
+
+    }
+}
