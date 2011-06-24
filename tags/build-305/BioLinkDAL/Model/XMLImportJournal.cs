1e48251d2575bfbbc77fee88e33415f61fdcdabf
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-06-24 08:43:41 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-305/BioLinkDAL/Model/XMLImportJournal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace BioLink.Data.Model {
+
+    public abstract class XMLImportObject {
+
+        public XMLImportObject(XmlElement xmlNode) {
+            if (xmlNode != null && xmlNode.HasAttribute("ID")) {
+                GUID = xmlNode.Attributes["ID"].Value;
+            }
+        }
+
+        [MappingInfo("GUID", Ignore=true)]
+        public string GUID { get; set; }
+
+        public string UpdateClause { get; set; }
+        public string InsertClause { get; set; }
+        public int ID { get; set; }
+    }
+
+    public class XMLImportJournal : XMLImportObject {
+
+        public XMLImportJournal(XmlElement node) : base(node) { }
+        
+        public string FullName { get; set; }
+
+    }
+}
