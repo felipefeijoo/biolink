60dd29858f698ec5a6eb3fc2430b149bfcaf5d08
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-06-27 01:49:08 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-306/BioLink.Client.Extensibility/Export/TabularDataExporter.cs
@@ -0,0 +1,110 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using System;
+
+namespace BioLink.Client.Extensibility {
+
+    public abstract class TabularDataExporter : IBioLinkExtension {
+
+        public TabularDataExporter() {
+        }
+
+        public IProgressObserver ProgressObserver { get; set; }
+
+        public void Export(Window parentWindow, DataMatrix matrix, IProgressObserver progress) {
+            this.ProgressObserver = progress;
+            object options = GetOptions(parentWindow, matrix);
+            JobExecutor.QueueJob(() => {
+                this.ExportImpl(parentWindow, matrix, options);
+                ProgressEnd("");
+            });
+
+        }
+
+        protected string Escape(string str) {
+            return System.Security.SecurityElement.Escape(str);
+        }
+
+        protected bool FileExistsAndNotOverwrite(string filename) {
+            FileInfo file = new FileInfo(filename);
+            if (file.Exists) {
+
+                bool retval = false;
+                PluginManager.Instance.ParentWindow.InvokeIfRequired(() => {                    
+                    retval = PluginManager.Instance.ParentWindow.Question(string.Format("The file {0} already exists. Do you wish to overwrite it?", file.FullName), "Overwrite existing file?");
+                });
+
+                if (!retval) {
+                    return true;
+                }
+
+                
+                if (file.IsReadOnly) {
+                    ErrorMessage.Show("{0} is not writable. Please ensure that it is not marked as read-only and that you have sufficient priviledges to write to it before trying again", file.FullName);
+                    return true;
+                }
+
+                try {
+                    file.Delete();
+                } catch (Exception) {
+                    ErrorMessage.Show("{0} could not be deleted. Please ensure that it is not marked as read-only and that you have sufficient priviledges to write to it before trying again", file.FullName);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public abstract bool CanExport(DataMatrix matrix);
+
+        protected abstract object GetOptions(Window parentWindow, DataMatrix matrix);
+
+        public abstract void ExportImpl(Window parentWindow, DataMatrix matrix, object options);
+
+        public abstract void Dispose();
+
+        protected void ProgressStart(string message, bool indeterminate = false) {
+            if (ProgressObserver != null) {
+                ProgressObserver.ProgressStart(message, indeterminate);
+            }
+        }
+
+        protected void ProgressMessage(string message, double? percent = null) {
+            if (ProgressObserver != null) {
+                ProgressObserver.ProgressMessage(message, percent);
+            }
+        }
+
+        protected void ProgressEnd(string message) {
+            if (ProgressObserver != null) {
+                ProgressObserver.ProgressEnd(message);                
+            }
+        }
+
+        protected string PromptForFilename(string extension, string filter) {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Export"; // Default file name
+            dlg.DefaultExt = extension; // Default file extension
+            dlg.OverwritePrompt = false;
+            dlg.Filter = filter + "|All files (*.*)|*.*"; // Filter files by extension
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true) {
+                return dlg.FileName;
+            }
+
+            return null;
+        }
+
+        #region Properties
+
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract BitmapSource Icon { get; }
+
+        #endregion
+
+    }
+}
