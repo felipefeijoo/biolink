b9a27925c7b7495593735119ee7a8e942cfba6c8
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-07-05 01:33:50 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-321/BioLink.Client.Extensibility/Notes/NoteActions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Extensibility {
+
+    public class DeleteNoteAction : GenericDatabaseAction<Note> {
+    
+        public DeleteNoteAction(Note model, ViewModelBase owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            Model.IntraCatID = Owner.ObjectID.Value;
+            service.DeleteNote(Model.NoteID);
+        }
+
+        protected ViewModelBase Owner { get; set; }
+    }
+
+    public class InsertNoteAction : GenericDatabaseAction<Note> {
+
+        public InsertNoteAction(Note model, ViewModelBase owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            Model.IntraCatID = Owner.ObjectID.Value;
+            Model.NoteID = service.InsertNote(Model.NoteCategory.ToString(), Model.IntraCatID, Model.NoteType, Model.NoteRTF, Model.Author, Model.Comments, Model.UseInReports, Model.RefID, Model.RefPages);
+        }
+
+        protected ViewModelBase Owner { get; private set; }
+
+    }
+
+    public class UpdateNoteAction : GenericDatabaseAction<Note> {
+
+        public UpdateNoteAction(Note model, ViewModelBase owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            Model.IntraCatID = Owner.ObjectID.Value;            
+            service.UpdateNote(Model.NoteID, Model.NoteCategory.ToString(), Model.NoteType, Model.NoteRTF, Model.Author, Model.Comments, Model.UseInReports, Model.RefID, Model.RefPages);
+        }
+
+        protected ViewModelBase Owner { get; private set; }
+    }
+}
