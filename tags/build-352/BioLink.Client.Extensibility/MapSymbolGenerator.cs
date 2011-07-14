ee952f5be047d0f5a95075e03f1ec0a47ad3c77e
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-07-14 07:25:14 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-352/BioLink.Client.Extensibility/MapSymbolGenerator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace BioLink.Client.Extensibility {
+
+    public class MapSymbolGenerator {
+
+        public static Image GetSymbolForPointSet(MapPointSet set) {
+            return GetSymbol(set.PointShape, set.Size, set.PointColor, set.DrawOutline, set.OutlineColor);
+        }
+
+        public static Image GetSymbol(PointOptionsControl ctl) {
+            return GetSymbol(ctl.Shape, ctl.Size, ctl.Color, ctl.DrawOutline);
+        }
+
+        public static Image GetSymbol(MapPointShape shape, int size, System.Windows.Media.Color fill, bool drawOutline, System.Windows.Media.Color outline = default(System.Windows.Media.Color)) {
+            Color fillColor = Color.FromArgb(fill.A, fill.R, fill.G, fill.B);
+
+            if (outline == default(System.Windows.Media.Color)) {
+                outline = System.Windows.Media.Colors.Black;
+            }
+
+            Color outlineColor = Color.FromArgb(outline.A, outline.R, outline.G, outline.B);
+
+            Image img = null;
+
+            switch (shape) {
+                case MapPointShape.Circle:
+                    img = Circle(size, fillColor, drawOutline, outlineColor);
+                    break;
+                case MapPointShape.Square:
+                    img = Square(size, fillColor, drawOutline, outlineColor);
+                    break;
+                case MapPointShape.Triangle:
+                    img = Triangle(size, fillColor, drawOutline, outlineColor);
+                    break;
+            }
+
+            if (img != null) {
+                var info = new SymbolInfo { Size = size, Color = fillColor, DrawOutline = drawOutline, Shape = shape };
+                img.Tag = info;
+            }
+
+            return img;
+        }
+
+        public static Image Circle(int size, Color fillColor, bool drawOutline, Color outlineColor = default(Color)) {
+            Bitmap bm = new Bitmap(size, size);
+            
+            using (Graphics g = Graphics.FromImage(bm)) {
+                Brush fill = new SolidBrush(fillColor);
+                g.FillEllipse(fill, new Rectangle(0, 0, size - 1, size - 1));
+                if (drawOutline) {
+                    Pen outline = new Pen(outlineColor);
+                    g.DrawEllipse(outline, new Rectangle(0, 0, size - 1, size - 1));
+                }
+            }
+            return bm;
+        }
+
+        public static Image Square(int size, Color fillColor, bool drawOutline, Color outlineColor = default(Color)) {
+            Bitmap bm = new Bitmap(size, size);
+
+            using (Graphics g = Graphics.FromImage(bm)) {
+                Brush fill = new SolidBrush(fillColor);
+                g.FillRectangle(fill, new Rectangle(0, 0, size - 1, size - 1));
+                if (drawOutline) {
+                    Pen outline = new Pen(outlineColor);
+                    g.DrawRectangle(outline, new Rectangle(0, 0, size - 1, size - 1));
+                }
+            }
+            return bm;
+        }
+
+        public static Image Triangle(int size, Color fillColor, bool drawOutline, Color outlineColor = default(Color)) {
+            Bitmap bm = new Bitmap(size, size);
+
+            using (Graphics g = Graphics.FromImage(bm)) {
+                Brush fill = new SolidBrush(fillColor);
+                // new Point[] { new Point(0, size-1), new Point(size/2, 0), new Point(size-1, size-1) }, new byte[] { PathPointType.Start, PathPointType.Line, PathPointType.Line}
+                GraphicsPath path = new GraphicsPath();
+
+                path.AddLine(0, size - 1, (size - 1) / 2, 0);
+                path.AddLine((size - 1) / 2, 0, size - 1, size - 1);
+                path.CloseFigure();
+
+                g.FillPath(fill, path);                
+                if (drawOutline) {
+                    Pen outline = new Pen(outlineColor);
+                    g.DrawPath(outline, path);
+                }
+            }            
+            return bm;
+        }
+    }
+
+    public class SymbolInfo {
+        public int Size { get; set; }
+        public MapPointShape Shape { get; set; }
+        public Color Color { get; set; }
+        public bool DrawOutline { get; set; }
+    }
+    
+}
---
+++ tags/build-352/BioLink.Client.Tools/Loans/LoanMaterialControl.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Tools {
+    /// <summary>
+    /// Interaction logic for LoanMaterialControl.xaml
+    /// </summary>
+    public partial class LoanMaterialControl : OneToManyDetailControl {
+
+        protected Loan Loan { get; private set; }
+
+        public LoanMaterialControl(User user, Loan model) : base(user, "LoanMaterial:" + model.LoanID) {
+            InitializeComponent();
+
+            txtMaterial.BindUser(user, LookupType.Material);
+            txtTaxon.BindUser(user, LookupType.Taxon);
+            txtTaxon.EnforceLookup = false; // Any taxon name will do!
+
+            Loan = model;
+
+            lblClosed.Visibility = System.Windows.Visibility.Collapsed;
+            chkReturned.IsEnabled = true;
+            if (Loan.LoanClosed.HasValue && Loan.LoanClosed.Value) {
+                lblClosed.Visibility = System.Windows.Visibility.Visible;
+                chkReturned.IsEnabled = false;
+            }
+
+            this.DataContextChanged += new DependencyPropertyChangedEventHandler(LoanMaterialControl_DataContextChanged);
+        }
+
+        void LoanMaterialControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            var material = e.NewValue as LoanMaterialViewModel;
+            if (material != null) {
+                if (Loan.LoanClosed.ValueOrFalse() && !material.DateReturned.HasValue) {
+                    material.Model.DateReturned = Loan.DateClosed;
+                }
+            }
+        }
+
+        public override ViewModelBase AddNewItem(out DatabaseAction addAction) {            
+            var model = new LoanMaterial() { Returned = false, NumSpecimens = "1", DateAdded=DateTime.Now };
+            addAction = new InsertLoanMaterialAction(model, Loan);
+            return new LoanMaterialViewModel(model);
+        }
+
+
+        public override DatabaseAction PrepareDeleteAction(ViewModelBase viewModel) {
+            return new DeleteLoanMaterialAction((viewModel as LoanMaterialViewModel).Model);
+        }
+
+        public override List<ViewModelBase> LoadModel() {
+            var service = new LoanService(User);
+            var list = service.GetLoanMaterial(Loan.LoanID);
+            var model = new List<ViewModelBase>(list.Select((m) => {
+                return new LoanMaterialViewModel(m);
+            }));
+            return model;
+        }
+
+        public override DatabaseAction PrepareUpdateAction(ViewModelBase viewModel) {
+            return new UpdateLoanMaterialAction((viewModel as LoanMaterialViewModel).Model);
+        }
+
+        public override FrameworkElement FirstControl {
+            get { return txtMaterial; }
+        }
+
+        public override bool AcceptDroppedPinnable(PinnableObject pinnable) {
+            return pinnable.LookupType == LookupType.Material;
+        }
+
+        public override void PopulateFromPinnable(ViewModelBase viewModel, PinnableObject pinnable) {
+            var item = viewModel as LoanMaterialViewModel;
+            if (item != null && pinnable.LookupType == LookupType.Material) {
+                var service = new MaterialService(User);
+                var material = service.GetMaterial(pinnable.ObjectID);
+                if (material != null) {
+                    var vm = PluginManager.Instance.GetViewModel(pinnable);
+                    txtMaterial.ObjectID = pinnable.ObjectID;
+                    txtMaterial.Text = vm.DisplayLabel;
+                    txtTaxon.Text = material.TaxaDesc;
+                }
+            }
+        }
+
+    }
+
+    public class DeleteLoanMaterialAction : GenericDatabaseAction<LoanMaterial> {
+
+        public DeleteLoanMaterialAction(LoanMaterial model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new LoanService(user);
+            service.DeleteLoanMaterial(Model.LoanMaterialID);
+        }
+
+    }
+
+    public class InsertLoanMaterialAction : GenericDatabaseAction<LoanMaterial> {
+
+        public InsertLoanMaterialAction(LoanMaterial model, Loan loan) : base(model) {
+            Loan = loan;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new LoanService(user);
+            Model.LoanID = Loan.LoanID;
+            Model.LoanMaterialID = service.InsertLoanMaterial(Model);
+        }
+
+        private Loan Loan { get; set; }
+    }
+
+    public class UpdateLoanMaterialAction : GenericDatabaseAction<LoanMaterial> {
+        public UpdateLoanMaterialAction(LoanMaterial model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new LoanService(user);
+            service.UpdateLoanMaterial(Model);
+        }
+    }
+
+    public class LoanMaterialViewModel : GenericViewModelBase<LoanMaterial> {
+
+        public LoanMaterialViewModel(LoanMaterial model) : base(model, () => model.LoanMaterialID) { }
+
+        public int LoanMaterialID {
+            get { return Model.LoanMaterialID; }
+            set { SetProperty(() => Model.LoanMaterialID, value); } 
+        }
+
+        public int LoanID {
+            get { return Model.LoanID; }
+            set { SetProperty(() => Model.LoanID, value); }
+        }
+        
+        public int MaterialID {
+            get { return Model.MaterialID; }
+            set { SetProperty(() => Model.MaterialID, value); }
+        }
+
+        public string NumSpecimens {
+            get { return Model.NumSpecimens; }
+            set { SetProperty(() => Model.NumSpecimens, value); }
+        }
+
+        public string TaxonName { 
+            get { return Model.TaxonName; }
+            set { SetProperty(() => Model.TaxonName, value); }
+        }
+
+        public string MaterialDescription {
+            get { return Model.MaterialDescription; }
+            set { SetProperty(() => Model.MaterialDescription, value); }
+        }
+
+        public DateTime? DateAdded {
+            get { return Model.DateAdded; }
+            set { SetProperty(() => Model.DateAdded, value); }
+        }
+
+        public DateTime? DateReturned {
+            get { return Model.DateReturned; }
+            set { SetProperty(() => Model.DateReturned, value); }
+        }
+
+        public bool? Returned {
+            get { return Model.Returned; }
+            set { SetProperty(() => Model.Returned, value); }
+        }
+
+        public string MaterialName {
+            get { return Model.MaterialName; }
+            set { SetProperty(() => Model.MaterialName, value); }
+        }
+
+        public override string ToString() {
+            return string.Format("{0}  {1}  ({2} specimens)", (MaterialID == 0 ? "<No Name>" : MaterialName), TaxonName, NumSpecimens);
+        }
+
+    }
+}
