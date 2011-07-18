f7d50b99305272ee153b5afb53cae64217841e4e
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-07-18 14:28:22 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-354/BioLink.Client.Extensibility/OneToManyControl.xaml.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Data;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+using System.Collections.ObjectModel;
+using System.Threading;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for OneToManyControl.xaml
+    /// </summary>
+    public partial class OneToManyControl : DatabaseActionControl, ILazyPopulateControl {
+
+        private OneToManyDetailControl _control;
+        private ObservableCollection<ViewModelBase> _model;
+        private bool _rdeMode;        
+
+        #region Designer Constructor
+        public OneToManyControl() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public OneToManyControl(OneToManyDetailControl control, bool rdeMode = false) : base(control.User, control.ContentID) {
+
+            InitializeComponent();
+            _rdeMode = rdeMode;
+            _control = control;
+            detailsGrid.Children.Add(_control);
+            lst.SelectionChanged += new SelectionChangedEventHandler(lst_SelectionChanged);
+
+            control.Host = this;
+            detailsGrid.IsEnabled = false;
+
+            ChangesCommitted += new PendingChangesCommittedHandler(OneToManyControl_ChangesCommitted);
+
+            lst.PreviewDragEnter += new DragEventHandler(lst_PreviewDragOver);
+            lst.PreviewDragOver += new DragEventHandler(lst_PreviewDragOver);
+            lst.Drop += new DragEventHandler(lst_Drop);
+
+        }
+
+        void lst_Drop(object sender, DragEventArgs e) {
+            var pinnable = e.Data.GetData(PinnableObject.DRAG_FORMAT_NAME) as PinnableObject;            
+            if (pinnable != null) {
+                var viewModel = AddNew();
+                if (viewModel != null) {
+                    _control.PopulateFromPinnable(viewModel, pinnable);
+                }
+            }                            
+            
+        }
+
+        void lst_PreviewDragOver(object sender, DragEventArgs e) {
+            var pinnable = e.Data.GetData(PinnableObject.DRAG_FORMAT_NAME) as PinnableObject;
+            e.Effects = DragDropEffects.None;
+            if (pinnable != null) {
+                if (_control.AcceptDroppedPinnable(pinnable)) {
+                    e.Effects = DragDropEffects.Link;
+                }
+            }                            
+        }
+
+        void OneToManyControl_ChangesCommitted(object sender) {
+            int oldSelectedIndex = lst.SelectedIndex;
+            LoadModel();
+            if (oldSelectedIndex <= _model.Count && oldSelectedIndex >= 0) {
+                lst.SelectedIndex = oldSelectedIndex;
+            } else {
+                if (_model.Count > 0) {
+                    lst.SelectedIndex = 0;
+                }
+            }            
+        }
+
+        protected ViewModelBase Owner { get; set; }
+
+        public void SetModel(ViewModelBase owner, ObservableCollection<ViewModelBase> model) {
+            _model = model;
+            lst.ItemsSource = _model;
+            _control.Owner = owner;
+            if (_model.Count > 0) {
+                lst.SelectedItem = _model[0];
+            } else {
+                detailsGrid.IsEnabled = false;
+            }
+        }
+
+        public ObservableCollection<ViewModelBase> GetModel() {
+            return _model;
+        }
+
+        void lst_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            detailsGrid.IsEnabled = lst.SelectedItem != null;
+        }
+
+        private void DeleteSelected() {
+            var selected = lst.SelectedItem as ViewModelBase;
+            if (selected != null && _control != null) {
+                _model.Remove(selected);
+                var action = _control.PrepareDeleteAction(selected);
+                if (action != null) {
+                    RegisterPendingChange(action);
+                }
+            }
+        }
+
+        private ViewModelBase AddNew() {
+            if (_control != null) {
+                DatabaseAction action = null;
+                var viewModel = _control.AddNewItem(out action);
+                if (viewModel != null) {
+                    _model.Add(viewModel);
+                    if (action != null) {
+                        RegisterPendingChange(action);
+                    }
+                    lst.SelectedItem = viewModel;
+
+                    if (_control.FirstControl != null) {
+                        if (_control.FirstControl is PickListControl) {
+                            FocusHelper.Focus((_control.FirstControl as PickListControl).txt);
+                        } else {
+                            FocusHelper.Focus(_control.FirstControl);
+                        }
+                    }
+                }
+                return viewModel;
+            }
+
+            return null;
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e) {
+            DeleteSelected();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+            AddNew();
+            e.Handled = true;
+        }
+
+        private void LoadModel() {
+            if (_control != null && !_rdeMode) {
+                detailsGrid.IsEnabled = false;
+
+                var list = _control.LoadModel();
+                _model = new ObservableCollection<ViewModelBase>(list.ConvertAll((viewModel) => {
+                    viewModel.DataChanged += new DataChangedHandler(viewModel_DataChanged);
+                    return viewModel;
+                }));
+
+                lst.ItemsSource = _model;
+
+                if (_model.Count > 0) {
+                    lst.SelectedItem = _model[0];
+                }
+
+                IsPopulated = true;
+            }
+        }
+
+        void viewModel_DataChanged(ChangeableModelBase viewmodel) {
+            if (_control != null) {
+                var action = _control.PrepareUpdateAction(viewmodel as ViewModelBase);
+                if (action != null) {
+                    RegisterUniquePendingChange(action);
+                }
+            }
+        }
+
+        public bool IsPopulated { get; private set; }
+
+        public void Populate() {
+
+            if (!IsPopulated) {
+                LoadModel();
+            }
+
+        }
+
+        public void AddButtonRHS(FrameworkElement control) {
+            control.Margin = new Thickness(6, 0, 0, 0);
+            pnlButtonsRHS.Children.Add(control);
+        }
+
+        public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(OneToManyControl), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnIsReadOnlyChanged));
+
+        public bool IsReadOnly {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+        }
+
+        private static void OnIsReadOnlyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args) {
+
+            var control = obj as OneToManyControl;
+            if (control != null) {
+                bool val = (bool)args.NewValue;
+                control._control.IsEnabled = !val;
+                control.btnAdd.IsEnabled = !val;
+                control.btnDelete.IsEnabled = !val;
+            }
+
+        }
+
+    }
+
+    static class FocusHelper {
+        private delegate void MethodInvoker();
+
+        public static void Focus(UIElement element) {
+            //Focus in a callback to run on another thread, ensuring the main UI thread is initialized by the
+            //time focus is set
+            ThreadPool.QueueUserWorkItem(delegate(Object foo) {
+                UIElement elem = (UIElement)foo;
+                elem.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal,
+                    (MethodInvoker)delegate() {
+                    elem.Focus();
+                    Keyboard.Focus(elem);
+                });
+            }, element);
+        }
+    }
+}
---
+++ tags/build-354/BioLink.Client.Material/IdentificationHistoryControl.xaml.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data.Model;
+using BioLink.Data;
+using System.Collections.ObjectModel;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for IdentificationHistoryControl.xaml
+    /// </summary>
+    public partial class IdentificationHistoryControl : DatabaseActionControl {
+
+        private ObservableCollection<MaterialIdentificationViewModel> _model;
+
+        #region Designer Constructor
+        public IdentificationHistoryControl() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public IdentificationHistoryControl(User user, int materialID) : base(user, "MaterialIDHistory:" + materialID) {
+            InitializeComponent();
+            this.MaterialID = materialID;
+
+            txtIdentifiedBy.BindUser(User, "tblMaterial", "vchrIDBy");
+            txtReference.BindUser(User, LookupType.Reference);
+            txtAccuracy.BindUser(User, PickListType.Phrase, "Identification Accuracy", TraitCategoryType.Material);
+            txtMethod.BindUser(User, PickListType.Phrase, "Identification Method", TraitCategoryType.Material);
+            txtNameQual.BindUser(User, PickListType.Phrase, "Identification Qualifier", TraitCategoryType.Material);
+
+            this.ChangesCommitted += new PendingChangesCommittedHandler(IdentificationHistoryControl_ChangesCommitted);
+
+            LoadIDHistory();
+        }
+
+        void IdentificationHistoryControl_ChangesCommitted(object sender) {
+            LoadIDHistory();
+        }
+
+        public void LoadIDHistory() {
+            var service = new MaterialService(User);
+            var list = service.GetMaterialIdentification(MaterialID);
+            _model = new ObservableCollection<MaterialIdentificationViewModel>(list.ConvertAll((m) => {
+                var viewModel = new MaterialIdentificationViewModel(m);
+                viewModel.DataChanged += new DataChangedHandler(viewModel_DataChanged);
+                return viewModel;
+            }));
+
+            lst.SelectionChanged += new SelectionChangedEventHandler(lst_SelectionChanged);
+
+            detailGrid.IsEnabled = false;
+
+            lst.ItemsSource = _model;
+            if (_model.Count > 0) {
+                lst.SelectedItem = _model[0];
+            }
+        }
+
+        void lst_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            detailGrid.IsEnabled = lst.SelectedItem != null;
+        }
+
+        void viewModel_DataChanged(ChangeableModelBase viewmodel) {
+            RegisterUniquePendingChange(new UpdateMaterialIdentificationAction((viewmodel as MaterialIdentificationViewModel).Model));
+        }
+
+        public int MaterialID { get; private set; }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e) {
+            DeleteSelected();
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+            AddNew();
+        }
+
+        private void AddNew() {
+            var model = new MaterialIdentification();
+            model.MaterialID = MaterialID;
+            model.MaterialIdentID = -1;
+            model.Taxa = "<New identification>";
+            var viewmodel = new MaterialIdentificationViewModel(model);
+            _model.Add(viewmodel);
+
+            lst.SelectedItem = viewmodel;
+
+            RegisterUniquePendingChange(new InsertMaterialIdentificationAction(viewmodel.Model));
+        }
+
+        private void DeleteSelected() {
+            var selected = lst.SelectedItem as MaterialIdentificationViewModel;
+            if (selected != null) {
+                _model.Remove(selected);
+                RegisterPendingChange(new DeleteMaterialIdentificationAction(selected.Model));
+            }
+        }
+
+        public void AddHistoryFromMaterial(MaterialViewModel m) {
+            var model = new MaterialIdentification();
+
+            model.MaterialID = m.MaterialID;
+            model.MaterialIdentID = -1;
+            model.IDAccuracy = m.IdentificationAccuracy;
+            model.IDBy = m.IdentifiedBy;
+            model.IDDate = m.IdentificationDate;
+            model.IDMethod = m.IdentificationMethod;
+            model.IDNotes = m.IdentificationNotes;
+            model.IDRefID = m.IdentificationReferenceID;
+            model.IDRefPage = m.IdentificationRefPage;
+            model.Taxa = m.TaxaDesc;
+
+            var viewmodel = new MaterialIdentificationViewModel(model);
+            _model.Add(viewmodel);
+            RegisterUniquePendingChange(new InsertMaterialIdentificationAction(viewmodel.Model));            
+        }
+    }
+
+    public class MaterialIdentificationViewModel : GenericViewModelBase<MaterialIdentification> {
+
+        public MaterialIdentificationViewModel(MaterialIdentification model) : base(model, ()=>model.MaterialIdentID) { }
+
+        public override string DisplayLabel {
+            get {
+                return string.Format("{0} by {1} on {2:d MMM, yyyy}", Taxa, IDBy, IDDate);
+            }
+        }
+
+        public int MaterialIdentID {
+            get { return Model.MaterialIdentID; }
+            set { SetProperty(() => Model.MaterialIdentID, value); }
+        }
+
+        public int MaterialID {
+            get { return Model.MaterialID; }
+            set { SetProperty(() => Model.MaterialID, value); }
+        }
+
+        public string Taxa {
+            get { return Model.Taxa; }
+            set { SetProperty(() => Model.Taxa, value); }
+        }
+
+        public string IDBy {
+            get { return Model.IDBy; }
+            set { SetProperty(() => Model.IDBy, value); }
+        }
+
+        public DateTime? IDDate {
+            get { return Model.IDDate; }
+            set { SetProperty(() => Model.IDDate, value); }
+        }
+
+        public int IDRefID {
+            get { return Model.IDRefID; }
+            set { SetProperty(() => Model.IDRefID, value); }
+        }
+
+        public string IDMethod {
+            get { return Model.IDMethod; }
+            set { SetProperty(() => Model.IDMethod, value); }
+        }
+
+        public string IDAccuracy {
+            get { return Model.IDAccuracy; }
+            set { SetProperty(() => Model.IDAccuracy, value); }
+        }
+
+        public string NameQual {
+            get { return Model.NameQual; }
+            set { SetProperty(() => Model.NameQual, value); }
+        }
+
+        public string IDNotes {
+            get { return Model.IDNotes; }
+            set { SetProperty(() => Model.IDNotes, value); }
+        }
+
+        public string IDRefPage {
+            get { return Model.IDRefPage; }
+            set { SetProperty(() => Model.IDRefPage, value); }
+        }
+
+        public int? BasedOnID {
+            get { return Model.BasedOnID; }
+            set { SetProperty(() => Model.BasedOnID, value); }
+        }
+
+        public string RefCode {
+            get { return Model.RefCode; }
+            set { SetProperty(() => Model.RefCode, value); }
+        }
+
+    }
+
+    public class UpdateMaterialIdentificationAction : GenericDatabaseAction<MaterialIdentification> {
+
+        public UpdateMaterialIdentificationAction(MaterialIdentification model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.UpdateMaterialIdentification(Model);
+        }
+    }
+
+    public class InsertMaterialIdentificationAction : GenericDatabaseAction<MaterialIdentification> {
+        public InsertMaterialIdentificationAction(MaterialIdentification model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            Model.MaterialIdentID = service.InsertMaterialIdentification(Model);
+        }
+    }
+
+    public class DeleteMaterialIdentificationAction : GenericDatabaseAction<MaterialIdentification> {
+        public DeleteMaterialIdentificationAction(MaterialIdentification model) : base(model) { }
+
+        protected override void ProcessImpl(User user) {
+            var service = new MaterialService(user);
+            service.DeleteMaterialIdentification(Model.MaterialIdentID);
+        }
+    }
+
+}
