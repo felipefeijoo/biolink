0b69d6d6e75f53fcd33e372f6bc345e711c94075
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-07-22 01:54:40 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-368/BioLinkDAL/Model/XMLImportReference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace BioLink.Data.Model {
+    public class XMLImportReference : XMLImportObject {
+
+        public XMLImportReference(XmlElement node) : base(node) { }
+
+        public string Code { get; set; }
+        public string Author { get; set; }
+        public string Year { get; set; }        
+    }
+}
