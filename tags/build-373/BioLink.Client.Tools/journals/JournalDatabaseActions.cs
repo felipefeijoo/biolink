05bd70139fdb46ecb5099b411e99a18bc6a2efb3
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-07-25 07:49:45 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-373/BioLink.Client.Tools/journals/JournalDatabaseActions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+
+namespace BioLink.Client.Tools {
+
+    public class InsertJournalCommand : GenericDatabaseCommand<Journal> {
+
+        public InsertJournalCommand(Journal model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            Model.JournalID = service.InsertJournal(Model);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SUPPORT_JOURNALS, PERMISSION_MASK.INSERT);
+        }
+
+    }
+
+    public class UpdateJournalCommand : GenericDatabaseCommand<Journal> {
+
+        public UpdateJournalCommand(Journal model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            service.UpdateJournal(Model);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SUPPORT_JOURNALS, PERMISSION_MASK.UPDATE);
+        }
+
+    }
+
+    public class DeleteJournalCommand : GenericDatabaseCommand<Journal> {
+
+        public DeleteJournalCommand(Journal model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            service.DeleteJournal(Model.JournalID);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SUPPORT_JOURNALS, PERMISSION_MASK.DELETE);
+        }
+
+    }
+}
