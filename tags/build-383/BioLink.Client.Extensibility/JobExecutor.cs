248edd95badd08ae7ed3b055cae233fd975528a4
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-03 05:28:06 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-383/BioLink.Client.Extensibility/JobExecutor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace BioLink.Client.Extensibility {
+
+    /// <summary>
+    /// Facade over thread pool, just in case we want to take more control over the threading later
+    /// </summary>
+    public class JobExecutor {
+
+        public static void QueueJob(WaitCallback job, object state) {
+            ThreadPool.QueueUserWorkItem(job, state);
+        }
+
+        public static void QueueJob(Action job) {
+            ThreadPool.QueueUserWorkItem((ignored) => {
+                try {
+                    job();
+                } catch (Exception ex) {
+                    GlobalExceptionHandler.Handle(ex);
+                }
+            });
+        }
+
+    }
+}
---
+++ tags/build-383/BioLink.Client.Extensibility/MultiMedia/LinkedMultimediaItemsControl.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data.Model;
+using BioLink.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for LinkedMultimediaItemsControl.xaml
+    /// </summary>
+    public partial class LinkedMultimediaItemsControl : UserControl, IIdentifiableContent {
+
+        public LinkedMultimediaItemsControl(MultimediaLinkViewModel viewModel) {
+            InitializeComponent();
+            this.DataContext = viewModel;
+
+            this.MultimediaID = viewModel.MultimediaID;            
+            var service = new SupportService(User);
+
+            var mm = service.GetMultimedia(MultimediaID);
+
+            var items = service.ListItemsLinkedToMultimedia(MultimediaID);
+            var model = new ObservableCollection<ViewModelBase>();
+
+            foreach (MultimediaLinkedItem item in items) {
+                if ( !string.IsNullOrWhiteSpace(item.CategoryName)) {
+                    LookupType t;
+                    if (Enum.TryParse<LookupType>(item.CategoryName, out t)) {
+                        var vm = PluginManager.Instance.GetViewModel(t, item.IntraCatID);
+                        if (vm!= null) {
+                            model.Add(vm);
+                            vm.Tag = t;
+                        }
+                    }
+                }
+            }
+
+            lvw.MouseRightButtonUp += new MouseButtonEventHandler(lvw_MouseRightButtonUp);
+
+            ListViewDragHelper.Bind(lvw, (dragged) => {
+
+                if (dragged.Tag is LookupType) {
+                    var lookupType = (LookupType) dragged.Tag;
+                    string pluginName = null;
+                    PluginManager.Instance.TraversePlugins((plugin) => {
+                        if (plugin.CanEditObjectType(lookupType)) {
+                            pluginName = plugin.Name;
+                        }
+                    });
+
+                    if (!string.IsNullOrWhiteSpace(pluginName)) {
+                        var data = new DataObject("Pinnable", dragged);
+                        var pinnable = new PinnableObject(pluginName, lookupType, dragged.ObjectID.Value);
+                        data.SetData(PinnableObject.DRAG_FORMAT_NAME, pinnable);
+                        data.SetData(DataFormats.Text, dragged.DisplayLabel);
+                        return data;
+                    }
+                }
+                return null;
+            });
+
+            lvw.ItemsSource = model;
+            CollectionView myView = (CollectionView)CollectionViewSource.GetDefaultView(lvw.ItemsSource);
+
+            myView.SortDescriptions.Add(new SortDescription("Tag", ListSortDirection.Ascending));
+            myView.SortDescriptions.Add(new SortDescription("DisplayLabel", ListSortDirection.Ascending));
+
+            myView.GroupDescriptions.Add(new LinkedItemGroupDescription());
+
+        }
+
+        void lvw_MouseRightButtonUp(object sender, MouseButtonEventArgs e) {
+            var selected = lvw.SelectedItem as ViewModelBase;
+            if (selected != null) {
+                var list = new List<ViewModelBase>();
+                list.Add(selected);
+                var commands = PluginManager.Instance.SolicitCommandsForObjects(list);
+                if (commands.Count > 0) {
+                    var builder = new ContextMenuBuilder(null);
+                    foreach (Command loopvar in commands) {
+                        Command cmd = loopvar; // include this in the closure scope, loopvar is outside, hence will always point to the last item...
+                        if (cmd is CommandSeparator) {
+                            builder.Separator();
+                        } else {
+                            builder.New(cmd.Caption).Handler(() => { cmd.CommandAction(selected); }).End();   
+                        }                        
+                    }
+                    lvw.ContextMenu = builder.ContextMenu;
+                }
+            }
+        }
+
+        public User User { get { return PluginManager.Instance.User; } }
+
+        public int MultimediaID { get; private set; }
+
+        public string ContentIdentifier {
+            get { return "ItemsForMultimedia:" + MultimediaID; }
+        }
+
+        public void RefreshContent() {
+            throw new NotImplementedException();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e) {
+            var window = this.FindParentWindow();
+            if (window != null) {
+                window.Close();
+            }
+        }
+
+    }
+
+    public class LinkedItemGroupDescription : System.ComponentModel.GroupDescription {
+
+        public override object GroupNameFromItem(object item, int level, System.Globalization.CultureInfo culture) {
+            var vm = item as ViewModelBase;
+            if (vm != null && vm.Tag is LookupType) {
+                return ((LookupType) vm.Tag).ToString();
+            }
+
+            return "Other";
+        }
+    }
+
+}
