fcb6c195774d04b0b4623c4c1e3e06ea90a912f7
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-05 01:58:57 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-394/BioLink.Client.Extensibility/Traits/TraitDatabaseAction.cs
@@ -0,0 +1,70 @@
+﻿using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Extensibility {
+
+    public abstract class TraitDatabaseCommandBase : DatabaseCommand {
+
+        public TraitDatabaseCommandBase(Trait trait, ViewModelBase owner) {
+            this.Trait = trait;
+            this.Owner = owner;
+        }
+
+        public Trait Trait { get; private set; }
+        public ViewModelBase Owner { get; private set; }
+
+        
+    }
+
+    public class UpdateTraitDatabaseCommand : TraitDatabaseCommandBase {
+
+        public UpdateTraitDatabaseCommand(Trait trait, ViewModelBase owner) : base(trait, owner) { }
+
+        protected override void ProcessImpl(User user) {
+            SupportService service = new SupportService(user);
+            Trait.IntraCatID = Owner.ObjectID.Value;
+            Trait.TraitID = service.InsertOrUpdateTrait(Trait);
+        }
+
+        public override bool Equals(object obj) {
+            var other = obj as UpdateTraitDatabaseCommand;
+            if (other != null) {
+                return Trait == other.Trait;
+            }
+            return false;
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public override string ToString() {
+            return string.Format("Insert/Update Trait: ID={0} Cat={1} Value={2}", Trait.TraitID, Trait.Category, Trait.Value);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.None();
+        }
+
+    }
+
+    public class DeleteTraitDatabaseCommand : TraitDatabaseCommandBase {
+
+        public DeleteTraitDatabaseCommand(Trait trait, ViewModelBase owner) : base(trait, owner) { }
+
+        protected override void ProcessImpl(User user) {
+            SupportService service = new SupportService(user);
+            service.DeleteTrait(Trait.TraitID);
+        }
+
+        public override string ToString() {
+            return string.Format("Delete Trait: ID={0} Cat={1} Value={2}", Trait.TraitID, Trait.Category, Trait.Value);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.None();
+        }
+
+    }
+
+}
---
+++ tags/build-394/BioLink.Client.Tools/AdvancedPreferences.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using System.Reflection;
+
+namespace BioLink.Client.Tools {
+    /// <summary>
+    /// Interaction logic for AdvancedPreferences.xaml
+    /// </summary>
+    public partial class AdvancedPreferences : Window {
+
+        public AdvancedPreferences() {
+
+            InitializeComponent();
+
+            var questionFields = typeof(OptionalQuestions).GetMembers(BindingFlags.Public | BindingFlags.Static);
+            
+            for (int i = 0; i < questionFields.Length; ++i) {
+                var questionField = questionFields[i] as FieldInfo;         
+       
+                var question = questionField.GetValue(null) as OptionalQuestion;
+                if (question != null) {
+                    gridOptionalQuestions.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
+                    bool askQuestion = Config.GetUser(PluginManager.Instance.User, question.AskQuestionConfigurationKey, true);
+                    bool defaultAnswer = Config.GetUser(PluginManager.Instance.User, question.ConfigurationKey, true);
+                    var chkAskQuestion = new CheckBox { Content = question.ConfigurationText, IsChecked = askQuestion, VerticalAlignment = System.Windows.VerticalAlignment.Top };
+                    chkAskQuestion.Margin = new Thickness(6, 0, 0, 0);
+                    Grid.SetRow(chkAskQuestion, i);
+
+                    var chkDefaultAnswer = new CheckBox { Content = "If not, is the default answer yes?", IsChecked = defaultAnswer, VerticalAlignment = System.Windows.VerticalAlignment.Top };
+                    chkDefaultAnswer.Margin = new Thickness(30, 25, 0, 0);
+                    Grid.SetRow(chkDefaultAnswer, i);
+
+                    chkDefaultAnswer.Unloaded += new RoutedEventHandler((s, e) => {
+                        Config.SetUser(PluginManager.Instance.User, question.AskQuestionConfigurationKey, chkAskQuestion.IsChecked.ValueOrFalse());
+                        Config.SetUser(PluginManager.Instance.User, question.ConfigurationKey, chkDefaultAnswer.IsChecked.ValueOrFalse());
+                    });
+
+                    gridOptionalQuestions.Children.Add(chkAskQuestion);
+                    gridOptionalQuestions.Children.Add(chkDefaultAnswer);
+                }
+            }
+
+            txtMaxSearchResults.Text = Config.GetUser(User, "SearchResults.MaxSearchResults", 2000).ToString();
+
+            chkCheckDuplicateAccessionNumbers.IsChecked = Config.GetGlobal("Material.CheckUniqueAccessionNumbers", true);
+        }
+
+        public User User {
+            get { return PluginManager.Instance.User; }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e) {
+            SavePreferences();
+            this.Close();
+        }
+
+        private void SavePreferences() {
+
+            int maxResults = 0;
+            if (Int32.TryParse(txtMaxSearchResults.Text, out maxResults)) {
+                Config.SetUser(User, "SearchResults.MaxSearchResults", maxResults);
+            }
+
+            Config.SetGlobal("Material.CheckUniqueAccessionNumbers", chkCheckDuplicateAccessionNumbers.IsChecked.GetValueOrDefault(true));
+        }
+    }
+}
