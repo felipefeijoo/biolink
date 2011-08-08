f22bc06747f1da77de46bd652180dfe962a7af8f
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-08 02:32:40 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-399/BioLink.Client.Material/RDESiteVisitViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Data.Model;
+using System.Collections.ObjectModel;
+
+namespace BioLink.Client.Material {
+
+    public class RDESiteVisitViewModel : RDEViewModel<RDESiteVisit> {
+
+        public RDESiteVisitViewModel(RDESiteVisit model) : base(model, ()=>model.SiteVisitID) {
+            Material = new ObservableCollection<ViewModelBase>();
+        }
+
+        public int SiteVisitID {
+            get { return Model.SiteVisitID; }
+            set { SetProperty(() => Model.SiteVisitID, value); }
+        }
+
+        public int SiteID {
+            get { return Model.SiteID; }
+            set { SetProperty(() => Model.SiteID, value); }
+        }
+
+        public string VisitName {
+            get { return Model.VisitName; }
+            set { SetProperty(() => Model.VisitName, value); }
+        }
+
+        public string Collector {
+            get { return Model.Collector; }
+            set { SetProperty(() => Model.Collector, value); }
+        }
+
+        public int? DateStart {
+            get { return Model.DateStart; }
+            set { SetProperty(() => Model.DateStart, value); }
+        }
+
+        public int? DateEnd {
+            get { return Model.DateEnd; }
+            set { SetProperty(() => Model.DateEnd, value); }
+        }
+
+        public RDESiteViewModel Site { get; set; }
+
+        public ObservableCollection<ViewModelBase> Material { get; set; }
+
+    }
+}
