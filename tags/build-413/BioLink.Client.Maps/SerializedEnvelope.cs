ea14d295c175c4c9c15d41df0b2ad16e1ec5da08
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-11 03:39:59 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-413/BioLink.Client.Maps/SerializedEnvelope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharpMap.Geometries;
+using GeoAPI.Geometries;
+
+namespace BioLink.Client.Maps {
+
+    public class SerializedEnvelope {
+
+        public SerializedEnvelope() {
+        }
+
+        public SerializedEnvelope(BoundingBox envelope) {
+            MinX = envelope.Left;
+            MinY = envelope.Bottom;
+            MaxX = envelope.Right;
+            MaxY = envelope.Top;
+        }
+
+        public BoundingBox CreateBoundingBox() {
+            return new BoundingBox(MinX, MinY, MaxX, MaxY);            
+        }
+
+        public double MaxX { get; set; }
+        public double MaxY { get; set; }
+        public double MinX { get; set; }
+        public double MinY { get; set; }
+
+    }
+
+}
