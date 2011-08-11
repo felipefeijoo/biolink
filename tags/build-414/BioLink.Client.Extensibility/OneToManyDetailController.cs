a5e58ed3df39a2912beb019bbf9f49ad361d75e9
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-11 04:05:22 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-414/BioLink.Client.Extensibility/OneToManyDetailController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using BioLink.Data;
+
+namespace BioLink.Client.Extensibility {
+
+    public interface IOneToManyDetailController {
+
+        List<ViewModelBase> LoadModel();
+
+        ViewModelBase AddNewItem(out DatabaseCommand addAction);
+
+        DatabaseCommand PrepareDeleteAction(ViewModelBase viewModel);
+
+        DatabaseCommand PrepareUpdateAction(ViewModelBase viewModel);
+
+        User User { get; }
+
+        ViewModelBase Owner { get; set; }
+
+        OneToManyControl Host { get; set; }
+
+        bool AcceptDroppedPinnable(PinnableObject pinnable);
+
+        void PopulateFromPinnable(ViewModelBase viewModel, PinnableObject pinnable);
+
+        UIElement GetDetailEditor(ViewModelBase selectedItem);
+    }
+
+    public interface IOneToManyDetailEditor {
+        UIElement FirstControl { get; }
+    }
+
+    public class OneToManyControllerEditor : UserControl, IOneToManyDetailEditor, IOneToManyDetailController {
+
+        protected OneToManyControllerEditor(User user) {
+            this.User = user;
+        }
+
+        public virtual List<ViewModelBase> LoadModel() {
+            throw new NotImplementedException();
+        }
+
+        public virtual ViewModelBase AddNewItem(out DatabaseCommand addAction) {
+            throw new NotImplementedException();
+        }
+
+        public virtual DatabaseCommand PrepareDeleteAction(ViewModelBase viewModel) {
+            throw new NotImplementedException();
+        }
+
+        public virtual DatabaseCommand PrepareUpdateAction(ViewModelBase viewModel) {
+            throw new NotImplementedException();
+        }
+
+        public virtual UIElement FirstControl {
+            get { return null; }
+        }
+
+        public virtual bool AcceptDroppedPinnable(PinnableObject pinnable) {
+            return false;
+        }
+
+        public virtual void PopulateFromPinnable(ViewModelBase viewModel, PinnableObject pinnable) {
+            throw new NotImplementedException();
+        }
+
+        public virtual UIElement GetDetailEditor(ViewModelBase selectedItem) {
+            return this;
+        }
+
+        public User User { get; protected set; }
+
+        public ViewModelBase Owner { get; set; }
+
+        public OneToManyControl Host { get; set; }
+
+    }
+
+}
---
+++ tags/build-414/BioLinkDAL/Model/XMLImportMultimedia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace BioLink.Data.Model {
+
+    public class XMLImportMultimedia : XMLImportObject {
+
+        public XMLImportMultimedia(XmlElement node) : base(node) { }
+
+        public byte[] ImageData { get; set; }
+    }
+}
