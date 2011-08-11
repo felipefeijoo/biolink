05617c5c78cf85f98d7f095da2853328fb2f6ecf
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-11 06:47:28 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-416/BioLinkDAL/Model/SANTypeDataType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class SANTypeDataType : GUIDObject {
+
+        public string PrimaryType { get; set; }
+        public IEnumerable<String> SecondaryTypes { get; set; }
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return null; }
+        }
+    }
+}
