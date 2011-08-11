0d04118e8845dcbb42b8acc832e794de7b5f98a6
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-11 01:51:48 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-412/BioLink.Client.Extensibility/MapPointSetGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Client.Extensibility {
+
+    //public abstract class MapPointSetGenerator : IMapPointSetGenerator {
+
+    //    public abstract MapPointSet GeneratePoints();
+
+    //}
+
+    public interface IMapPointSetGenerator {
+
+        MapPointSet GeneratePoints(bool showOptions);
+
+    }
+
+    public class DelegatingPointSetGenerator<T> : IMapPointSetGenerator where T : class {
+
+        private Func<bool, T, MapPointSet> _delegate;
+        private T _arg;
+
+        public DelegatingPointSetGenerator(Func<bool, T, MapPointSet> @delegate, T arg) {
+            _delegate = @delegate;
+            _arg = arg;
+        }
+
+        public MapPointSet GeneratePoints(bool showOptions) {
+            if (_delegate != null) {
+                return _delegate(showOptions, _arg);
+            }
+            return null;
+        }
+    }
+}
