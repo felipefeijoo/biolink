50abcbb319a37dfba394a1120b4db24f431fb20a
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-15 05:41:00 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-433/BioLinkDAL/Model/MultimediaLink.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class MultimediaLink : OwnedDataObject {
+
+        public int MultimediaID { get; set; }
+
+        public int MultimediaLinkID { get; set; }
+
+        public string MultimediaType { get; set; }
+
+        public string Name { get; set; }
+
+        public string Caption { get; set; }
+
+        public string Extension { get; set; }
+
+        public int SizeInBytes { get; set; }
+
+        public int Changes { get; set; }
+
+        public int BlobChanges { get; set; }
+
+        public string TempFilename { get; set; }
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return () => this.MultimediaLinkID; }
+        }
+    }
+
+    public class MultimediaType {
+
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+
+
+}
