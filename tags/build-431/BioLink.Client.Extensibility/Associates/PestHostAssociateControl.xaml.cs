d41cf176c7d3725ff53d5548bb8fe43fecde6097
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-15 05:01:05 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-431/BioLink.Client.Extensibility/Associates/PestHostAssociateControl.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+
+namespace BioLink.Client.Extensibility {
+
+    /// <summary>
+    /// Interaction logic for PestHostAssociateControl.xaml
+    /// </summary>
+    public partial class PestHostAssociateControl : UserControl, IOneToManyDetailEditor {
+
+        private bool _manualSet;
+
+        public PestHostAssociateControl(User user, TraitCategoryType category, ViewModelBase owner) {
+            InitializeComponent();
+            this.Category = category;
+
+            txtRegion.BindUser(user, LookupType.Region);
+            txtSource.BindUser(user, "tblAssociate", "vchrSource");
+            txtReference.BindUser(user, LookupType.Reference);
+
+            txtAssociate.PreviewDragOver += new DragEventHandler(txtAssociate_PreviewDragOver);
+            txtAssociate.PreviewDragEnter += new DragEventHandler(txtAssociate_PreviewDragOver);
+
+            txtAssociate.PreviewDrop += new DragEventHandler(txtAssociate_PreviewDrop);
+
+            txtAssociate.KeyUp += new KeyEventHandler(txtAssociate_KeyUp);
+
+            this.DataContextChanged += new DependencyPropertyChangedEventHandler(PestHostAssociateControl_DataContextChanged);
+
+        }
+
+        void txtAssociate_KeyUp(object sender, KeyEventArgs e) {
+            var selected = DataContext as AssociateViewModel;
+            if (selected != null) {
+                selected.RelativeCatID = null;
+                selected.RelativeIntraCatID = null;
+                lblAssociateType.Content = "Description";
+                selected.AssocDescription = txtAssociate.Text;
+            }
+        }
+
+        void txtAssociate_PreviewDrop(object sender, DragEventArgs e) {
+            var pinnable = e.Data.GetData(PinnableObject.DRAG_FORMAT_NAME) as PinnableObject;
+            if (pinnable != null) {
+                var associate = DataContext as AssociateViewModel;
+                SetFromPinnable(associate, pinnable);
+                e.Handled = true;
+            }
+        }
+
+        void PestHostAssociateControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+            var selected = this.DataContext as AssociateViewModel;
+            if (selected != null) {
+
+                if (selected.RelativeCatID == 1 || selected.RelativeCatID == 2) {
+                    // txtAssociate.Text = selected.AssocName;
+                    var lookupType = AssociatesOneToManyController.GetLookupTypeFromCategoryID(selected.RelativeCatID.Value);
+                    lblAssociateType.Content = lookupType.ToString();
+                } else {
+                    // txtAssociate.Text = selected.AssocDescription;
+                    lblAssociateType.Content = "Description";
+                }
+
+                _manualSet = true;
+                if (selected.RelativeRelationFromTo.Equals("host", StringComparison.CurrentCultureIgnoreCase)) {
+                    optPest.IsChecked = true;
+                } else {
+                    optHost.IsChecked = true;
+                }
+                _manualSet = false;
+            }
+        }
+
+        private void GenericLookup<T>() {
+            PluginManager.Instance.StartSelect<T>((result) => {
+                var associate = this.DataContext as AssociateViewModel;
+                _manualSet = true;                
+                // txtAssociate.Text = result.Description;
+                
+                lblAssociateType.Content = result.LookupType.ToString();
+                associate.RelativeIntraCatID = result.ObjectID;
+                associate.RelativeCatID = AssociatesOneToManyController.GetCategoryIDFromLookupType(result.LookupType);
+                associate.RelativeRelationFromTo = optPest.IsChecked.ValueOrFalse() ? "Pest" : "Host";
+                associate.RelativeRelationToFrom = optPest.IsChecked.ValueOrFalse() ? "Host" : "Pest";
+                associate.NameOrDescription = result.Description;
+
+                _manualSet = false;
+            }, LookupOptions.None);
+        }
+
+        void txtAssociate_PreviewDragOver(object sender, DragEventArgs e) {
+            var pinnable = e.Data.GetData(PinnableObject.DRAG_FORMAT_NAME) as PinnableObject;
+            e.Effects = DragDropEffects.None;
+            if (pinnable != null) {
+                if (pinnable.LookupType == LookupType.Material || pinnable.LookupType == LookupType.Taxon) {
+                    e.Effects = DragDropEffects.Link;                    
+                }
+                e.Handled = true;
+            }                                        
+        }
+
+        private void SetFromPinnable(AssociateViewModel associate, PinnableObject pinnable) {            
+            if (pinnable != null && associate != null) {
+                var viewModel = PluginManager.Instance.GetViewModel(pinnable);                
+                associate.RelativeCatID = AssociatesOneToManyController.GetCategoryIDFromLookupType(pinnable.LookupType);
+                associate.RelativeIntraCatID = pinnable.ObjectID;
+                associate.NameOrDescription = viewModel.DisplayLabel;                
+                SetRelationships(associate);
+                lblAssociateType.Content = pinnable.LookupType.ToString();
+            }
+        }
+
+        #region Properties
+
+        public TraitCategoryType Category { get; private set; }
+
+        #endregion
+
+        private void btnSelectTaxon_Click(object sender, RoutedEventArgs e) {
+            GenericLookup<Taxon>();
+        }
+
+        private void btnSelectMaterial_Click(object sender, RoutedEventArgs e) {
+            GenericLookup<Material>();
+        }
+
+        private void btnEditSelected_Click(object sender, RoutedEventArgs e) {
+            var selected = DataContext as AssociateViewModel;
+            if (selected != null) {
+
+                if (selected.RelativeCatID.HasValue) {
+                    var lookupType = AssociatesOneToManyController.GetLookupTypeFromCategoryID(selected.RelativeCatID.Value);
+                    if (lookupType != LookupType.Unknown && selected.RelativeIntraCatID.HasValue) {
+                        PluginManager.Instance.EditLookupObject(lookupType, selected.RelativeIntraCatID.Value);
+                    }
+                }
+
+            }
+        }
+
+        private void optPest_Checked(object sender, RoutedEventArgs e) {
+            if (!_manualSet) {
+                var associate = DataContext as AssociateViewModel;
+                SetRelationships(associate);
+            }
+        }
+
+        private void optHost_Checked(object sender, RoutedEventArgs e) {
+            if (!_manualSet) {
+                var associate = DataContext as AssociateViewModel;
+                SetRelationships(associate);
+            }
+        }
+
+        private void SetRelationships(AssociateViewModel associate) {            
+            if (associate != null) {
+                associate.RelativeRelationFromTo = optPest.IsChecked.ValueOrFalse() ? "Host" : "Pest";
+                associate.RelativeRelationToFrom = optPest.IsChecked.ValueOrFalse() ? "Pest" : "Host";
+            }
+        }
+
+        protected User User { get; private set; }
+
+        UIElement IOneToManyDetailEditor.FirstControl {
+            get { return txtAssociate; }
+        }
+    }
+}
---
+++ tags/build-431/BioLink.Client.Extensibility/ReportResults.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using System;
+using System.Windows.Input;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for ReportResults.xaml
+    /// </summary>
+    public partial class ReportResults : UserControl, IProgressObserver, IDisposable {
+
+        private bool _ReportExecuted = false;
+
+        public ReportResults() {
+            InitializeComponent();
+        }
+
+        public ReportResults(IBioLinkReport report) {
+            
+            InitializeComponent();
+            progressBar.Visibility = System.Windows.Visibility.Hidden;
+
+            this.Report = report;
+            this.Loaded += new RoutedEventHandler(ReportResults_Loaded);
+            
+        }
+
+        void ReportResults_Loaded(object sender, RoutedEventArgs e) {
+            if (!_ReportExecuted) {
+                _ReportExecuted = true;
+                JobExecutor.QueueJob(() => {
+                    try {
+                        this.WaitCursor();
+                        StatusMessage("Running report...");
+                        this.InvokeIfRequired(() => { DisplayLoading(); });
+                        DataMatrix data = Report.ExtractReportData(this);
+                        this.InvokeIfRequired(() => { DisplayReportResults(data); });                        
+                    } catch (Exception ex) {
+                        this.NormalCursor();
+                        this.ProgressEnd("An error occured executing the report");
+                        this.InvokeIfRequired(() => { DisplayException(ex); });
+                    } finally {                        
+                        this.NormalCursor();
+                    }
+                });
+            }
+        }
+
+        private string _R(string key, params object[] args) {
+            string format = FindResource(key) as string;
+            if (format != null) {                
+                return String.Format(format, args);
+            }
+            return string.Format(key, args);
+        }
+
+        private void DisplayLoading() {
+            string assemblyName = this.GetType().Assembly.GetName().Name;
+            string imageFile = PluginManager.Instance.ResourceTempFileManager.ProxyResource(new Uri(String.Format("pack://application:,,,/{0};component/images/loading-big.gif", assemblyName)));
+            string html = _R("ReportResults.Loading.Template", Report.Name, imageFile);
+            DisplayHTML(html);
+        }
+
+        private void DisplayHTML(String html) {
+            reportContent.Children.Clear();
+            WebBrowser browser = new WebBrowser();
+            browser.Navigating += new System.Windows.Navigation.NavigatingCancelEventHandler(browser_Navigating);                        
+            browser.NavigateToString(html);
+            reportContent.Children.Add(browser);
+        }
+
+        private void DisplayException(Exception ex) {
+            
+            string title = _R("ReportResults.Error.Title", Report.Name);
+            string stacktrace = ex.StackTrace;
+            string html = _R("ReportResults.Error.Template", title, ex.Message, stacktrace);
+            DisplayHTML(html);
+        }
+
+        void browser_Navigating(object sender, System.Windows.Navigation.NavigatingCancelEventArgs e) {
+            Logger.Debug("Navigating to {0}", e.Uri == null ? null : e.Uri.ToString());
+        }
+
+        internal void DisplayReportResults(DataMatrix data) {
+
+            StatusMessage("Preparing view...");
+
+            if (this.Report != null) {
+                reportContent.Children.Clear();
+                if (Report.Viewers.Count == 0 || Report.Viewers.Count == 1) {
+                    IReportViewerSource viewerSource = null;
+                    if (Report.Viewers.Count == 0) {
+                        viewerSource = new TabularDataViewerSource();
+                    } else {
+                        viewerSource = Report.Viewers[0];
+                    }
+                    FrameworkElement control = viewerSource.ConstructView(Report, data, this);
+                    reportContent.Children.Add(control);                    
+                } else {
+                    TabControl tab = new TabControl();
+                    int count = 0;
+                    foreach (IReportViewerSource tmpViewerSource in Report.Viewers) {
+                        var viewerSource = tmpViewerSource; // need to capture a local because of the closure below
+                        var tabItem = new TabItem { Header = viewerSource.Name };
+
+                        // Create the first view straight away...
+                        if (count == 0) {
+                            tabItem.Content = viewerSource.ConstructView(Report, data, this);
+                        } else {
+                            tabItem.RequestBringIntoView += new RequestBringIntoViewEventHandler((s, e) => {
+                                if (tabItem.Content == null) {
+                                    tabItem.Content = viewerSource.ConstructView(Report, data, this);
+                                }
+                            });
+                        }
+                        tab.Items.Add(tabItem);
+                        count++;
+                    }                    
+                    reportContent.Children.Add(tab);
+                }
+            }
+
+            StatusMessage("{0} records retrieved.", data.Rows.Count);
+        }
+
+        internal void StatusMessage(string format, params object[] args) {
+            string message = format;
+            if (args != null && args.Length > 0) {
+                message = String.Format(format, args);
+            }            
+            statusMessage.InvokeIfRequired(() => {
+                statusMessage.Text = message;
+            });
+        }
+
+        #region ProgressObserver
+
+        public void ProgressStart(string message, bool indeterminate) {
+            StatusMessage(message);
+            progressBar.InvokeIfRequired(() => {
+                progressBar.Value = 0;
+                progressBar.Visibility = System.Windows.Visibility.Visible;
+                progressBar.IsIndeterminate = indeterminate;
+            });
+        }
+
+        public void ProgressMessage(string message, double? percentComplete) {
+            StatusMessage(message);
+            if (percentComplete.HasValue) {
+                progressBar.InvokeIfRequired(() => {
+                    progressBar.Value = percentComplete.Value;
+                });
+            }
+        }
+
+        public void ProgressEnd(string message) {
+            StatusMessage(message);
+            progressBar.InvokeIfRequired(() => {
+                progressBar.Value = 100;
+                progressBar.Visibility = System.Windows.Visibility.Hidden;
+            });
+        }
+
+        #endregion
+
+        #region properties
+
+        protected IBioLinkReport Report { get; private set; }
+
+        #endregion
+
+        public void Dispose() {
+            foreach (FrameworkElement elem in reportContent.Children) {
+                if (elem is IDisposable) {
+                    IDisposable disposable = elem as IDisposable;
+                    disposable.Dispose();
+                } else if (elem is TabControl) {
+                    var t = elem as TabControl;
+                    foreach (TabItem item in t.Items) {
+                        if (item.Content != null && item.Content is IDisposable) {
+                            (item.Content as IDisposable).Dispose();
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        //private void mnuFileExit_Click(object sender, RoutedEventArgs e) {
+        //    PluginManager.Instance.CloseContent(this);
+        //}
+
+    }
+}
