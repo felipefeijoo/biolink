c02b4ac822f6513eb8cd9cf615e3820f7049b87c
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-15 03:21:20 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-430/BioLinkDAL/Model/XMLImportSubPart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace BioLink.Data.Model {
+    public class XMLImportSubPart : XMLImportObject {
+
+        public XMLImportSubPart(XmlElement node) : base(node) { }
+
+    }
+}
