10022696fd90f07b81655b102206b43e9add3c34
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-15 05:19:21 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-432/BioLink.Client.Extensibility/FrameworkExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using BioLink.Client.Utilities;
+using System.Windows.Input;
+
+namespace BioLink.Client.Extensibility {
+
+    public static class FrameworkExtensions {
+
+        public static T FindParent<T>(this FrameworkElement control) where T : FrameworkElement {
+            var p = control.Parent as FrameworkElement;
+            while (!(p is T) && p != null) {
+                p = p.Parent as FrameworkElement;
+            }
+
+            if (p != null) {
+                return (T)p;
+            }
+            return null;
+        }
+
+        public static void WaitCursor(this Control control) {
+            control.SetCursor(Cursors.Wait);
+        }
+
+        public static void NormalCursor(this Control control) {
+            control.SetCursor(Cursors.Arrow);
+        }
+
+        public static void SetCursor(this Control control, Cursor cursor) {
+            control.InvokeIfRequired(() => {
+                control.Cursor = cursor;
+            });
+        }
+
+        public static void SetReadOnlyRecursive(this DependencyObject obj, bool readOnly) {
+            if (obj != null) {
+                foreach (object child in LogicalTreeHelper.GetChildren(obj)) {
+
+                    if (child is System.Windows.Controls.Primitives.TextBoxBase) {
+                        (child as System.Windows.Controls.Primitives.TextBoxBase).IsReadOnly = readOnly;
+                    } else if (child is CheckBox) {
+                        (child as CheckBox).IsEnabled = !readOnly;
+                    } else if (child is RadioButton || child is ComboBox) {
+                        (child as UIElement).IsEnabled = !readOnly;
+                    } else if (child is LookupControl) {
+                        (child as LookupControl).IsReadOnly = readOnly;
+                    } else {
+                        SetReadOnlyRecursive(child as DependencyObject, readOnly);
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ tags/build-432/BioLink.Taxa/TaxonDropContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Taxa {
+
+    /// <summary>
+    /// Container object for all the relevant bits of information regarding a (potential) taxon move/merge
+    /// </summary>
+    public class TaxonDropContext {
+
+        public TaxonDropContext(TaxonViewModel source, TaxonViewModel target, TaxaPlugin plugin) {
+            this.TaxaPlugin = plugin;
+            this.Source = source;
+            this.Target = target;
+            this.SourceRank = TaxaPlugin.Service.GetTaxonRank(source.ElemType);
+            this.TargetRank = TaxaPlugin.Service.GetTaxonRank(target.ElemType);
+            this.SourceChildRank = GetChildElementType(source);
+            this.TargetChildRank = GetChildElementType(target);
+        }
+
+        /// <summary>
+        /// Return the elemType of the first child that is not "unplaced", including available names, species inquirenda and incertae sedis
+        /// </summary>
+        /// <param name="current"></param>
+        /// <returns></returns>
+        public TaxonRank GetChildElementType(TaxonViewModel parent) {
+
+            if (!parent.IsExpanded) {
+                parent.IsExpanded = true; // This will load the children, if they are not already loaded...
+            }
+
+            foreach (TaxonViewModel child in parent.Children) {
+
+                if (String.IsNullOrEmpty(child.ElemType)) {
+                    continue;
+                }
+
+                if (child.AvailableName.GetValueOrDefault(false) || child.LiteratureName.GetValueOrDefault(false)) {
+                    continue;
+                }
+
+                if (child.ElemType == TaxaService.SPECIES_INQUIRENDA || child.ElemType == TaxaService.INCERTAE_SEDIS) {
+                    continue;
+                }
+
+                return TaxaPlugin.Service.GetTaxonRank(child.ElemType);
+            }
+
+            return null;
+        }
+
+
+        public TaxonViewModel Source { get; private set; }
+        public TaxonViewModel Target { get; private set; }
+        public TaxonRank SourceRank { get; private set; }
+        public TaxonRank TargetRank { get; private set; }
+        public TaxonRank SourceChildRank { get; set; }
+        public TaxonRank TargetChildRank { get; set; }        
+        public TaxaPlugin TaxaPlugin { get; private set; }
+
+    }
+}
