83789f8282884b87c086e5dcf8928cf3b203bd9e
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-16 12:27:52 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-444/BioLink.Client.Extensibility/Associates/AssociateDatabaseActions.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Extensibility {
+
+    public abstract class AssociateDatabaseCommand : GenericDatabaseCommand<Associate> {
+
+        protected AssociateDatabaseCommand(Associate model) : base(model) { }
+
+        protected string CategoryIDToString(int? catId) {
+            if (!catId.HasValue) {
+                return "";
+            }
+
+            if (catId.Value == TraitCategoryTypeHelper.GetTraitCategoryTypeID(TraitCategoryType.Material)) {
+                return "Material";
+            }
+
+            if (catId.Value == TraitCategoryTypeHelper.GetTraitCategoryTypeID(TraitCategoryType.Taxon)) {
+                return "Taxon";
+            }
+
+            return "";
+        }
+
+    }
+
+    public class InsertAssociateCommand : AssociateDatabaseCommand {
+
+        public InsertAssociateCommand(Associate model, ViewModelBase owner) : base(model) {
+            this.Owner = owner;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service =new SupportService(user);
+            Model.FromIntraCatID = Owner.ObjectID.Value;
+            Model.ToCategory = CategoryIDToString(Model.ToCatID);
+            Model.AssociateID = service.InsertAssociate(Model);
+        }
+
+        public override string ToString() {
+            return string.Format("Insert Associate: Name={0}", Model.AssocName);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SPARC_MATERIAL, PERMISSION_MASK.UPDATE);
+            required.Add(PermissionCategory.SPIN_TAXON, PERMISSION_MASK.UPDATE);
+        }
+
+        protected ViewModelBase Owner { get; private set; }
+
+    }
+
+    public class UpdateAssociateCommand : AssociateDatabaseCommand {
+
+        public UpdateAssociateCommand(Associate model) : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            Model.ToCategory = CategoryIDToString(Model.ToCatID);
+            service.UpdateAssociate(Model);
+        }
+
+        public override string ToString() {
+            return string.Format("Update Associate: Name={0}", Model.AssocName);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SPARC_MATERIAL, PERMISSION_MASK.UPDATE);
+            required.Add(PermissionCategory.SPIN_TAXON, PERMISSION_MASK.UPDATE);
+        }
+
+    }
+
+    public class DeleteAssociateCommand : GenericDatabaseCommand<Associate> {
+
+        public DeleteAssociateCommand(Associate model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            service.DeleteAssociate(Model.AssociateID);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SPARC_MATERIAL, PERMISSION_MASK.UPDATE);
+            required.Add(PermissionCategory.SPIN_TAXON, PERMISSION_MASK.UPDATE);
+        }
+
+    }
+}
---
+++ tags/build-444/BioLink.Client.Extensibility/FormattedLatLogVirtualColumn.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Client.Utilities;
+
+namespace BioLink.Client.Extensibility {
+
+    public class FormattedLatLongVirtualColumn : VirtualMatrixColumn {
+
+        public FormattedLatLongVirtualColumn(DataMatrix matrix) {
+
+            var areaTypeCol = matrix.IndexOf("AreaType");
+            var latCol = matrix.IndexOf("Lat");
+            var longCol = matrix.IndexOf("Long");
+            var latCol2 = matrix.IndexOf("Lat2");
+            var longCol2 = matrix.IndexOf("Long2");
+
+            this.Name = "FormattedLatLong";
+
+            this.ValueGenerator = (row) => {
+
+                object objLat = row[latCol];
+                object objlong = row[longCol];
+                object objAreaType = row[areaTypeCol];
+
+                if (objAreaType != null && objLat != null && objlong != null) {
+                    int areaType = int.Parse(objAreaType.ToString());
+                    double lat = (double)objLat;
+                    double @long = (double)objlong;
+                    AreaType t = (AreaType)areaType;
+                    switch (t) {
+                        case AreaType.Point:
+                            return string.Format("{0}, {1}", GeoUtils.DecDegToDMS(lat, CoordinateType.Latitude), GeoUtils.DecDegToDMS(@long, CoordinateType.Longitude));
+                        case AreaType.Line:
+                            double lat2 = (double)row[latCol2];
+                            double long2 = (double)row[longCol2];
+                            return string.Format("Line: {0}, {1} - {2}, {3}",
+                                GeoUtils.DecDegToDMS(lat, CoordinateType.Latitude), GeoUtils.DecDegToDMS(@long, CoordinateType.Longitude),
+                                GeoUtils.DecDegToDMS(lat2, CoordinateType.Latitude), GeoUtils.DecDegToDMS(long2, CoordinateType.Longitude));
+                        case AreaType.Box:
+                            lat2 = (double)row[latCol2];
+                            long2 = (double)row[longCol2];
+                            return string.Format("Box: {0}, {1} - {2}, {3}",
+                                GeoUtils.DecDegToDMS(lat, CoordinateType.Latitude), GeoUtils.DecDegToDMS(@long, CoordinateType.Longitude),
+                                GeoUtils.DecDegToDMS(lat2, CoordinateType.Latitude), GeoUtils.DecDegToDMS(long2, CoordinateType.Longitude));
+                    }
+                }
+
+                return "";
+            };
+        }        
+    }
+
+    public class FormattedDateVirtualColumn : VirtualMatrixColumn {
+
+        public FormattedDateVirtualColumn(DataMatrix matrix) {
+
+            int dateTypeCol = matrix.IndexOf("DateType");
+            int startDateCol = matrix.IndexOf("StartDate");
+            int endDateCol = matrix.IndexOf("EndDate");
+            int casualDateCol = matrix.IndexOf("CasualDate");
+
+            Name = "Dates";
+            ValueGenerator = (row) => {
+                int dateType = 0;
+                object objDateType = row[dateTypeCol];
+                if (objDateType != null) {
+                    int.TryParse(objDateType.ToString(), out dateType);
+                    return DateUtils.FormatDates(dateType, row[startDateCol] as int?, row[endDateCol] as int?, row[casualDateCol] as string);
+
+                }
+                return "";
+            };
+
+        }
+
+        private string FormatDate(int d, bool asRomanMonth = false) {
+            if (d == 0) {
+                return "";
+            }
+
+            if (asRomanMonth) {
+                return DateUtils.DateRomanMonth(d);
+            } else {
+                return DateUtils.BLDateToStr(d);
+            }
+        }
+
+
+    }
+}
