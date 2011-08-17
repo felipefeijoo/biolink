c7e9b27e1d3baa7ba505829884e6f664bcf62c06
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-08-17 02:40:18 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-447/BioLink.Client.Extensibility/Export/ExportData.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+
+namespace BioLink.Client.Extensibility {
+    /// <summary>
+    /// Interaction logic for ExportData.xaml
+    /// </summary>
+    public partial class ExportData : Window {
+
+        private DataMatrix _data;
+        private IProgressObserver _progress;
+
+        #region designer ctor
+        public ExportData() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public ExportData(DataMatrix data, IProgressObserver progress) {
+            InitializeComponent();
+            _data = data;
+            _progress = progress;
+            var candidates = PluginManager.Instance.GetExtensionsOfType<TabularDataExporter>();
+            var exporters = candidates.FindAll((exporter) => {
+                return exporter.CanExport(data);
+            });
+
+            listBox.ItemsSource = exporters;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+            this.Hide();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (listBox.SelectedItem != null) {
+                btnOk.IsEnabled = true;
+            }
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e) {            
+            ExportUsingSelectedExporter();            
+        }
+
+        private void ExportUsingSelectedExporter() {
+            TabularDataExporter exporter = listBox.SelectedItem as TabularDataExporter;
+            exporter.Export(this.Owner, _data, _progress);
+            this.DialogResult = true;
+        }
+
+        private void listBox_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            if (listBox.SelectedItem != null) {
+                ExportUsingSelectedExporter();
+            }
+        }
+
+    }
+}
---
+++ tags/build-447/BioLink.Client.Extensibility/MultiMedia/MultimediaViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data.Model;
+using BioLink.Client.Utilities;
+
+
+namespace BioLink.Client.Extensibility {
+
+    public class MultimediaViewModel : GenericOwnedViewModel<Multimedia> {
+
+        public MultimediaViewModel(Multimedia model) : base(model, ()=>model.MultimediaID) { }
+
+        public int MultimediaID {
+            get { return Model.MultimediaID; }
+            set { SetProperty(() => Model.MultimediaID, value); }
+        }
+
+        public string Name {
+            get { return Model.Name; }
+            set { SetProperty(() => Model.Name, value); }
+        }
+
+        public string Number {
+            get { return Model.Number; }
+            set { SetProperty(() => Model.Number, value); }
+        }
+
+        public string Artist {
+            get { return Model.Artist; }
+            set { SetProperty(() => Model.Artist, value); }
+        }
+
+        public string DateRecorded {
+            get { return Model.DateRecorded; }
+            set { SetProperty(() => Model.DateRecorded, value); }
+        }
+
+        public string Owner {
+            get { return Model.Owner; }
+            set { SetProperty(() => Model.Owner, value); }
+        }
+
+        public string Copyright {
+            get { return Model.Copyright; }
+            set { SetProperty(() => Model.Copyright, value); } 
+        }
+
+        public string FileExtension {
+            get { return Model.FileExtension; }
+            set { SetProperty(() => Model.FileExtension, value); }
+        }
+
+        public string Fullname {
+            get { return string.Format("{0}.{1}", Name, FileExtension); }
+        }
+
+    }
+}
