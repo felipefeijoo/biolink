8f486e4ca4d8ca80f05333baf2934e7920d2ec75
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-09-01 07:52:06 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-462/BioLink.Client.Material/TaxaForSiteReportOptions.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for TaxaForSiteReportOptions.xaml
+    /// </summary>
+    public partial class TaxaForSiteReportOptions : Window {
+
+        public TaxaForSiteReportOptions(User user, SiteExplorerNode node) {
+            InitializeComponent();
+            this.User = user;            
+
+            txtRegion.BindUser(user, LookupType.SiteOrRegion);
+            txtRegion.PreSelect(node.ObjectID, node.Name, MaterialExplorer.GetLookupTypeFromElemType(node.ElemType));
+
+            txtRegion.WatermarkText = "You must select a site or region";
+            txtTaxon.BindUser(user, LookupType.Taxon);
+            txtTaxon.WatermarkText = "All taxa";
+        }
+
+        protected User User { get; private set; }
+
+        public int? SiteRegionID {
+            get { return txtRegion.ObjectID; }
+        }
+
+        public string SiteRegionName {
+            get { return txtRegion.Text; } 
+        }
+
+        public string SiteRegionElemType { 
+            get { return (txtRegion.SelectedObject != null ? txtRegion.SelectedObject.LookupType.ToString() : null); }
+        }
+        
+
+        public int? TaxonID { 
+            get { 
+                return txtTaxon.ObjectID; 
+            } 
+        }
+
+        public string TaxonName {
+            get { return txtTaxon.Text; }
+        }
+
+        private void btnCnacel_Click(object sender, RoutedEventArgs e) {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) {
+            if (Validate()) {
+                this.DialogResult = true;
+                this.Close();
+            }
+        }
+
+        private bool Validate() {
+
+            if (txtRegion.SelectedObject == null) {
+                ErrorMessage.Show("You must select a valid site or region!");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ tags/build-462/BioLink.Client.Material/TrapDetails.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for TrapDetail.xaml
+    /// </summary>
+    public partial class TrapDetails : DatabaseCommandControl {
+
+        #region Designer Constructor
+
+        public TrapDetails() {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        public TrapDetails(User user, int trapID, bool readOnly) : base(user, "Trap:" + trapID) {
+            InitializeComponent();
+            var service = new MaterialService(user);
+            var model = service.GetTrap(trapID);
+            var viewModel = new TrapViewModel(model);
+
+            IsReadOnly = readOnly;
+
+            this.DataContext = viewModel;
+
+            viewModel.DataChanged += new DataChangedHandler(viewModel_DataChanged);
+
+            txtTrapType.BindUser(User, PickListType.Phrase, "Trap Type", TraitCategoryType.Trap);
+
+            tabTrap.AddTabItem("Traits", new TraitControl(User, TraitCategoryType.Trap, viewModel) { IsReadOnly = readOnly });
+            tabTrap.AddTabItem("Notes", new NotesControl(User, TraitCategoryType.Trap, viewModel) { IsReadOnly = readOnly });
+            tabTrap.AddTabItem("Ownership", new OwnershipDetails(model));
+        }
+
+        void viewModel_DataChanged(ChangeableModelBase viewmodel) {
+            RegisterUniquePendingChange(new UpdateTrapCommand((viewmodel as TrapViewModel).Model));
+        }
+
+        public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register("IsReadOnly", typeof(bool), typeof(TrapDetails), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnIsReadOnlyChanged)));
+
+        public bool IsReadOnly {
+            get { return (bool)GetValue(IsReadOnlyProperty); }
+            set { SetValue(IsReadOnlyProperty, value); }
+        }
+
+        private static void OnIsReadOnlyChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args) {
+            var control = (TrapDetails)obj;
+            if (control != null) {
+                var readOnly = (bool)args.NewValue;
+                control.SetReadOnlyRecursive(readOnly);
+            }
+        }
+
+
+    }
+}
