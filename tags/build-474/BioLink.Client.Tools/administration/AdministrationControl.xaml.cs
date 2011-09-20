f066d8feef8134df7ec263c67c8adf5da8b7158a
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-09-20 04:45:30 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-474/BioLink.Client.Tools/administration/AdministrationControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+
+namespace BioLink.Client.Tools {
+    /// <summary>
+    /// Interaction logic for AdministrationWindow.xaml
+    /// </summary>
+    public partial class AdministrationControl : DatabaseCommandControl {
+
+        public AdministrationControl(User user) : base(user, "AdminControl") {
+            InitializeComponent();
+            this.User = user;            
+            tabControl.AddTabItem("Trait types", new TraitTypesControl(user, "trait"));
+            tabControl.AddTabItem("Note types", new TraitTypesControl(user, "note"));
+            tabControl.AddTabItem("Ref Link types", new NameListAdminControl(user, "ref"));
+            tabControl.AddTabItem("Multimedia Link types", new NameListAdminControl(user, "mm"));
+        }
+
+    }
+}
