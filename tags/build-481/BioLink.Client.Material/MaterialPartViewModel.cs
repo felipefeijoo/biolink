d1a054b175ed014436035875066433d282eed85c
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-09-26 06:34:34 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-481/BioLink.Client.Material/MaterialPartViewModel.cs
@@ -0,0 +1,130 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+using BioLink.Data.Model;
+using BioLink.Data;
+using System.Windows;
+
+namespace BioLink.Client.Material {
+    public class MaterialPartViewModel : GenericViewModelBase<MaterialPart> {
+
+        public MaterialPartViewModel(MaterialPart model) : base(model, ()=>model.MaterialPartID) { }
+
+        public override string DisplayLabel {
+            get {
+                return String.Format("{0}", PartName, NoSpecimens);
+            }
+        }
+
+        public int MaterialID {
+            get { return Model.MaterialID; }
+            set { SetProperty(() => Model.MaterialID, value); }
+        }
+
+        public int MaterialPartID {
+            get { return Model.MaterialPartID; }
+            set { SetProperty(() => Model.MaterialPartID, value); }
+        }
+
+        public string PartName {
+            get { return Model.PartName; }
+            set { SetProperty(() => Model.PartName, value); }
+        }
+
+        public string SampleType {
+            get { return Model.SampleType; }
+            set { SetProperty(() => Model.SampleType, value); }
+        }
+
+        public int? NoSpecimens {
+            get { return Model.NoSpecimens; }
+            set { SetProperty(() => Model.NoSpecimens, value); }
+        }
+
+        public string NoSpecimensQual {
+            get { return Model.NoSpecimensQual; }
+            set { SetProperty(() => Model.NoSpecimensQual, value); }
+        }
+
+        public string Lifestage {
+            get { return Model.Lifestage; }
+            set { SetProperty(() => Model.Lifestage, value); }
+        }
+
+        public string Gender {
+            get { return Model.Gender; }
+            set { SetProperty(() => Model.Gender, value); }
+        }
+
+        public string RegNo {
+            get { return Model.RegNo; }
+            set { SetProperty(() => Model.RegNo, value); }
+        }
+
+        public string Condition {
+            get { return Model.Condition; }
+            set { SetProperty(() => Model.Condition, value); }
+        }
+
+        public string StorageSite {
+            get { return Model.StorageSite; }
+            set { SetProperty(() => Model.StorageSite, value); }
+        }
+
+        public string StorageMethod {
+            get { return Model.StorageMethod; }
+            set { SetProperty(() => Model.StorageMethod, value); }
+
+        }
+
+        public string CurationStatus {
+            get { return Model.CurationStatus; }
+            set { SetProperty(() => Model.CurationStatus, value); }
+        }
+
+        public string NoOfUnits {
+            get { return Model.NoOfUnits; } 
+            set { SetProperty(()=> Model.NoOfUnits, value); }
+        }
+        
+        public string Notes {
+            get { return Model.Notes; }
+            set { SetProperty(() => Model.Notes, value); }
+        }
+
+        public bool OnLoan {
+            get { return Model.OnLoan; }
+            set { SetProperty(() => Model.OnLoan, value); }
+        }
+
+        public int? BasedOnID {
+            get { return Model.BasedOnID; }
+            set { SetProperty(() => Model.BasedOnID, value); }
+        }
+
+        public static readonly DependencyProperty LockedProperty = DependencyProperty.Register("Locked", typeof(bool), typeof(MaterialPartViewModel), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public bool Locked {
+            get { return (bool)GetValue(LockedProperty); }
+            set { SetValue(LockedProperty, value); }
+        }
+
+    }
+}
---
+++ tags/build-481/BioLink.Client.Material/MaterialSummary.xaml.cs
@@ -0,0 +1,80 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BioLink.Client.Utilities;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Material {
+    /// <summary>
+    /// Interaction logic for MaterialSummary.xaml
+    /// </summary>
+    public partial class MaterialSummary : DatabaseCommandControl, ILazyPopulateControl {
+
+        #region Designer Ctor
+        public MaterialSummary() {
+            InitializeComponent();
+        }
+        #endregion
+
+        public MaterialSummary(User user, MaterialViewModel material)
+            : base(user, "MaterialSummary:" + material.MaterialID) {
+            InitializeComponent();
+            this.Material = material;            
+        }
+
+        private void LoadSummary() {
+            var service = new MaterialService(User);
+            String rtf = service.GetMaterialSummary(Material.Model);
+            txtSummary.RTF = rtf;
+            IsPopulated = true;
+        }
+
+        public bool IsPopulated { get; private set; }
+
+        public void Populate() {
+            if (!IsPopulated) {
+                LoadSummary();
+            }
+        }
+
+        public MaterialViewModel Material { get; private set; }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e) {
+            LoadSummary();
+        }
+
+        private void btnEditSite_Click(object sender, RoutedEventArgs e) {
+            PluginManager.Instance.EditLookupObject(LookupType.Site, Material.SiteID);
+        }
+
+        private void btnEditSiteVisit_Click(object sender, RoutedEventArgs e) {
+            PluginManager.Instance.EditLookupObject(LookupType.SiteVisit, Material.SiteVisitID);
+        }
+    }
+}
