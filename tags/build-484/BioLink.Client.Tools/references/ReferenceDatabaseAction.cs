42618bfd747d11679bcb504f0f45f8b2ec7cf75f
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2011-09-27 07:09:00 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-484/BioLink.Client.Tools/references/ReferenceDatabaseAction.cs
@@ -0,0 +1,100 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Client.Extensibility;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Tools {
+
+    public class UpdateReferenceCommand : GenericDatabaseCommand<Reference> {
+
+        public UpdateReferenceCommand(Reference model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            service.UpdateReference(Model);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SUPPORT_REFS, PERMISSION_MASK.UPDATE);
+        }
+    }
+
+    public class InsertReferenceCommand : GenericDatabaseCommand<Reference> {
+
+        public InsertReferenceCommand(Reference model)
+            : base(model) {
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            Model.RefID = service.InsertReference(Model);
+        }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SUPPORT_REFS, PERMISSION_MASK.INSERT);
+        }
+
+        public override List<string> Validate() {
+            var errors = new List<String>();
+
+            if (String.IsNullOrEmpty(Model.RefCode)) {
+                errors.Add("You must supply a Reference Code before you can continue");
+            }
+
+            return errors;
+        }
+
+    }
+
+    public class DeleteReferenceCommand : DatabaseCommand {
+
+        public DeleteReferenceCommand(int refID) {
+            this.RefID = refID;
+        }
+
+        protected override void ProcessImpl(User user) {
+            var service = new SupportService(user);
+            service.DeleteReference(RefID);
+        }
+
+        public override bool Equals(object obj) {
+            if (obj is DeleteReferenceCommand) {
+                var other = obj as DeleteReferenceCommand;
+                if (other.RefID == this.RefID) {
+                    return true;
+                }
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+
+        public int RefID { get; private set; }
+
+        protected override void BindPermissions(PermissionBuilder required) {
+            required.Add(PermissionCategory.SUPPORT_REFS, PERMISSION_MASK.DELETE);
+        }
+
+    }
+}
---
+++ tags/build-484/BioLinkDAL/Model/AutoNumber.cs
@@ -0,0 +1,57 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BioLink.Data.Model {
+
+    public class AutoNumber : GUIDObject {
+
+        public int AutoNumberCatID { get; set; }
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string Postfix { get; set; }
+        public int NumLeadingZeros { get; set; }
+        public int LastNumber { get; set; }
+        public bool Locked { get; set; }
+        public bool EnsureUnique { get; set; }
+
+        protected override System.Linq.Expressions.Expression<Func<int>> IdentityExpression {
+            get { return () => this.AutoNumberCatID; }
+        }
+    }
+
+    public class NewAutoNumber : AutoNumber {
+        public int NewNumber { get; set; }
+
+        public string FormattedNumber {
+
+            get {
+                string format = null;
+                if (NumLeadingZeros > 0) {
+                    format = string.Format("{{0}}{{1:{0}}}{{2}}", new string('0', NumLeadingZeros));
+                } else {
+                    format = "{0}{1}{2}";
+                }
+                return string.Format(format, Prefix, NewNumber, Postfix);
+            }
+
+        }
+    }
+    
+}
