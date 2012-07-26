be0d7fffa34f60726288019d6e3f5e98740d4c78
david.baird@synergi.com.au <david.baird@synergi.com.au@09e1001a-9679-c032-a9e2-1eb070a1449d>
2012-07-26 08:21:56 +0000

    Labeled automatically by TeamCity

---
+++ tags/build-531/BioLink.Client.Maps/LayersWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using SharpMap.Layers;
+using System.Collections.ObjectModel;
+using BioLink.Client.Extensibility;
+using BioLink.Client.Utilities;
+using Microsoft.Win32;
+
+namespace BioLink.Client.Maps {
+    /// <summary>
+    /// Interaction logic for LayersWindow.xaml
+    /// </summary>
+    public partial class LayersWindow : Window {
+
+        #region Design Constructor
+        public LayersWindow() {
+            InitializeComponent();
+        }
+        #endregion
+
+        private readonly MapControl _mapControl;
+        private readonly ObservableCollection<LayerViewModel> _model;
+
+        public LayersWindow(MapControl mapControl) {
+            InitializeComponent();
+            _mapControl = mapControl;            
+            _model = new ObservableCollection<LayerViewModel>();
+            if (mapControl.mapBox.Map.Layers != null && mapControl.mapBox.Map.Layers.Count > 0) {
+                foreach (ILayer layer in mapControl.mapBox.Map.Layers) {
+                    if (layer is VectorLayer) {
+                        _model.Insert(0, new VectorLayerViewModel(layer as VectorLayer));
+                    } else if (layer is MyGdalRasterLayer) {
+                        _model.Insert(0, new RasterLayerViewModel(layer as MyGdalRasterLayer));
+                    }
+                }
+            }
+
+            lstLayers.ItemsSource = _model;
+
+            if (_model.Count > 0) {
+                lstLayers.SelectedIndex = 0;
+            }
+
+            MapBackColor = mapControl.mapBox.BackColor;
+
+            backgroundColorPicker.DataContext = this;
+        }
+
+        public System.Drawing.Color MapBackColor { get; set; }
+
+        private void lstLayers_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selected = lstLayers.SelectedItem as LayerViewModel;
+            grpLayer.IsEnabled = (selected != null);
+            if (selected != null && selected is VectorLayerViewModel) {
+                var vector = selected as VectorLayerViewModel;
+                if (vector.Symbol != null) {
+                    var info = vector.Symbol.Tag as SymbolInfo;
+                    var control = new PointOptionsControl(info);                    
+                    control.ValuesChanged += ctl => {
+                                                 vector.Symbol = MapSymbolGenerator.GetSymbol(ctl);
+                                             };
+                    grpLayer.Content = control;
+                } else {                    
+                    grpLayer.Content = new VectorOptionsControl(vector);
+                }
+            } else {
+                grpLayer.Content = null;
+            }
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e) {
+            ApplyChanges();
+        }
+
+        private void ApplyChanges() {
+
+            var map = _mapControl.mapBox.Map;
+
+            map.Layers.Clear();
+
+            for (int i = _model.Count -1; i >=0; i--) {
+                var m = _model[i];
+                var layer = m.Model;          
+                if (layer != null) {
+                    map.Layers.Add(layer);
+                    if (layer is VectorLayer) {
+                        var vl = layer as VectorLayer;
+                        if (vl.Style.Symbol != null) {
+                            var vectorLayerViewModel = m as VectorLayerViewModel;
+                            if (vectorLayerViewModel != null) {
+                                vl.Style.Symbol = vectorLayerViewModel.Symbol;
+                            }
+                        } else {
+                            var vectorLayerViewModel = m as VectorLayerViewModel;
+                            if (vectorLayerViewModel != null) {
+                                vl.Style.Line = new System.Drawing.Pen(vectorLayerViewModel.FillBrush);
+                                vl.Style.Fill = vectorLayerViewModel.FillBrush;
+                                vl.Style.EnableOutline = vectorLayerViewModel.DrawOutline;
+                            }
+                        }
+                    }
+                } else {
+                    var vectorLayerViewModel = m as VectorLayerViewModel;
+                    if (vectorLayerViewModel != null) {
+                        throw new Exception("Could not load layer " + vectorLayerViewModel.ConnectionID + "!");
+                    }
+                }
+            }
+
+            // Alpha not allowed in map back color!
+            if (MapBackColor.A != 255) {
+                MapBackColor = System.Drawing.Color.FromArgb(255, MapBackColor);
+            }
+
+            _mapControl.mapBox.BackColor = MapBackColor;
+
+            _mapControl.mapBox.Refresh();
+
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e) {
+            ApplyChanges();
+            DialogResult = true;
+            Close();
+        }
+
+        private void AddNewLayer() {
+            var dlg = new OpenFileDialog {Filter = "Shape Files (*.shp)|*.shp|All files (*.*)|*.*"};
+            if (dlg.ShowDialog().ValueOrFalse()) {
+                ILayer layer = LayerFileLoader.LoadLayer(dlg.FileName);
+                if (layer != null && layer is VectorLayer) {
+                    var viewModel = new VectorLayerViewModel(layer as VectorLayer);
+                    _model.Add(viewModel);
+                }
+            }
+        }
+
+        private void RemoveSelectedLayer() {
+            var vm = lstLayers.SelectedItem as LayerViewModel;
+            if (vm != null) {
+                _model.Remove(vm);
+            }
+        }
+
+        private void MoveLayerUp() {
+            var vm = lstLayers.SelectedItem as LayerViewModel;
+            if (vm != null) {
+                int index = _model.IndexOf(vm);
+                if (_model.IndexOf(vm) > 0) {
+                    _model.RemoveAt(index);
+                    _model.Insert(index - 1, vm);
+                }
+                lstLayers.SelectedItem = vm;
+            }
+        }
+
+        private void MoveLayerDown() {
+            var vm = lstLayers.SelectedItem as LayerViewModel;
+            if (vm != null) {
+                int index = _model.IndexOf(vm);
+                if (_model.IndexOf(vm) < _model.Count - 1) {
+                    _model.RemoveAt(index);
+                    _model.Insert(index + 1, vm);
+                }
+                lstLayers.SelectedItem = vm;
+            }
+
+        }
+
+        private void btnAddLayer_Click(object sender, RoutedEventArgs e) {
+            AddNewLayer();
+        }
+
+        private void btnDeleteLayer_Click(object sender, RoutedEventArgs e) {
+            RemoveSelectedLayer();
+        }
+
+        private void btnLayerUp_Click(object sender, RoutedEventArgs e) {
+            MoveLayerUp();
+        }
+
+        private void btnLayerDown_Click(object sender, RoutedEventArgs e) {
+            MoveLayerDown();
+        }
+
+
+    }
+
+}
---
+++ tags/build-531/BioLink.Client.Tools/import/ImportReferencesProcessor.cs
@@ -0,0 +1,135 @@
+﻿/*******************************************************************************
+ * Copyright (C) 2011 Atlas of Living Australia
+ * All Rights Reserved.
+ * 
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ ******************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BioLink.Data;
+using BioLink.Data.Model;
+
+namespace BioLink.Client.Tools {
+
+    public class ImportReferencesProcessor : ImportProcessor {
+
+        protected override void InitImportImpl() {
+        }
+
+        protected override void ImportRowImpl(int rowId, System.Data.Common.DbConnection connection) {
+
+            var service = new ImportService(User);
+
+            string strJAbbrev = Get("References.Journal Abbreviated Name");
+            string strJName = Get("References.Journal name");
+            string strBookTitle = Get("References.Book title");
+            string strPublisher = Get("References.Book Publisher");
+            string strPlacePublished = Get("References.Book Place of publication");
+
+            string strRefType = null;
+
+            if (!string.IsNullOrWhiteSpace(strJAbbrev) || !string.IsNullOrWhiteSpace(strJName)) {
+                if (!string.IsNullOrWhiteSpace(strBookTitle)) {
+                    strRefType = "JS";
+                } else {
+                    strRefType = "J";
+                }
+            } else if (!string.IsNullOrWhiteSpace(strPublisher) || !string.IsNullOrWhiteSpace(strPlacePublished)) {
+                if (!string.IsNullOrWhiteSpace(strBookTitle)) {
+                    strRefType = "BS";
+                } else {
+                    strRefType = "B";
+                }
+
+            } else {
+                strRefType = "M";
+            }
+
+            int? startPage;
+            int? endPage;
+
+            ParserStartEndPages(Get("References.Total pages"), out startPage, out endPage);
+
+
+            var r = new ReferenceImport {
+                RefCode = Get("References.ReferenceCode"), 
+                Author = Get("References.Author(s)"), 
+                Title = Get("References.Article title"), 
+                BookTitle = strBookTitle,
+                Editor = Get("References.Editor"),
+                RefType = strRefType,
+                YearOfPub = Get("References.Year of publication"),
+                ActualDate = Get("References.Actual publication date"),
+                JournalAbbrevName = strJAbbrev, 
+                JournalFullName = strJName,
+                PartNo = Get("References.Part Number"),
+                Series = Get("References.Series Number"),
+                Publisher = strPublisher, 
+                Place = strPlacePublished,
+                Volume = Get("References.Volume"),
+                Pages = Get("References.Pages"), 
+                TotalPages = Get("References.Total pages"),
+                Possess = Get("References.Possession"),
+                Source = Get("References.Source"),
+                Edition = Get("References.Edition"),
+                ISBN = Get("References.ISBN"),
+                ISSN = Get("References.ISSN"),
+                Abstract = Get("References.Qualification"), 
+                DateQual = Get("References.Qualification-date"),
+                StartPage = startPage, 
+                EndPage = endPage
+            };
+
+            r.RefID = Service.ImportReference(r);
+        }
+
+        private void ParserStartEndPages(string pages, out int? startPage, out int? endPage) {
+           
+            startPage = null;
+            endPage = null;
+            if (string.IsNullOrWhiteSpace(pages)) {
+                return;
+            }
+
+            StringBuilder b = new StringBuilder();
+            foreach (char ch in pages) {
+                if (Char.IsDigit(ch)) {
+                    b.Append(ch);
+                } else if (ch == '-' || ch == 150) { // em dash?
+                    b.Append(" ");
+                }
+            }
+
+            string[] bits = b.ToString().Split(' ');
+            if (bits.Length == 1) {
+                TryParse(bits[0], endPage);
+            } else if (bits.Length > 1) {
+                TryParse(bits[0], startPage);
+                TryParse(bits[1], endPage);
+            }
+
+        }
+
+        public bool TryParse(string str, int? val) {
+            int i;
+            if (Int32.TryParse(str, out i)) {
+                val = i;
+                return true;
+            } else {
+                val = null;
+            }
+            return false;
+        }
+
+    }
+}
